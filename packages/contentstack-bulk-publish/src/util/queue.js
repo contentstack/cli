07170d0c35e92085676bf6adcfb1bc451c347cda
Abhinav Gupta <abhinav.gupta@contentstack.com>
2020-11-11 15:55:49 +0530

    Fixed the issue where multiple content-types weren't being processed

--- packages/contentstack-bulk-publish/src/util/queue.js
+++ packages/contentstack-bulk-publish/src/util/queue.js
@@ -7,8 +7,8 @@ class Queue extends EventEmitter {
     this.count = 0;
     this.store = [];
     this.config = {};
-    this.delay = 1.5
-    this.requestBatchSize = 10
+    this.delay = 3
+    this.requestBatchSize = 5
     this.on('dequeue', this.check);
   }
 
