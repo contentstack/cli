d9a10e1e02fd3c19944c57a51884a8da5baa3e1f
Rohit Mishra <rohit.mishra@contentstack.com>
2020-11-17 16:25:28 +0530

    made changes for new plugin

---
+++ packages/contentstack-clone/src/lib/util/contentstack-managment-sdk.js
@@ -0,0 +1,16 @@
+const contentstacksdk = require('@contentstack/management')
+
+
+exports.Client = function (config) {  
+
+  const option = {
+    host: config.host,
+    authtoken: config.auth_token,
+    api_key: config.target_stack,
+    maxContentLength: 100000000,
+    maxBodyLength: 1000000000,
+    logHandler: (level, data) => {}
+  }
+  const client = contentstacksdk.client(option)
+  return client
+}
