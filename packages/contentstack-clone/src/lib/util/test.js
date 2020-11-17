90edabb70a3306e04a03131e049cb7868c128ded
Rohit Mishra <rohit.mishra@contentstack.com>
2020-11-17 16:25:28 +0530

    made changes for new plugin

---
+++ packages/contentstack-clone/src/lib/util/test.js
@@ -0,0 +1,13 @@
+/*!
+* Contentstack Export
+* Copyright (c) 2019 Contentstack LLC
+* MIT Licensed
+*/
+
+let Stack = require('../util/contentstack-managment-sdk')
+
+
+exports.firstClass = function () {
+  console.log("djhjchdjchdjhcd")
+  }
+  
