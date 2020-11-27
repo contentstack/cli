11fcdcc369821400b1e52f1934fdbf15fd9b082f
ninadhatkar <ninad.hatkar@contentstack.com>
2020-11-27 16:03:53 +0530

    optimized import script

--- packages/contentstack-import/src/lib/import/labels.js
+++ packages/contentstack-import/src/lib/import/labels.js
@@ -12,7 +12,7 @@ const chalk = require('chalk');
 const _ = require('lodash')
 
 const helper = require('../util/fs');
-const {addlogs} = require('../util/log');
+const { addlogs } = require('../util/log');
 let config = require('../../config/default')
 let stack = require('../util/contentstack-management-sdk')
 
@@ -26,7 +26,7 @@ let labelFailsPath
 let client
 
 
-function importLabels () {
+function importLabels() {
   this.fails = [];
   this.success = [];
   this.labelUidMapper = {};
@@ -50,40 +50,39 @@ importLabels.prototype = {
     labelSuccessPath = path.resolve(config.data, 'labels', 'success.json');
     labelFailsPath = path.resolve(config.data, 'labels', 'fails.json');
     mkdirp.sync(labelMapperPath);
-
     return new Promise(function (resolve, reject) {
-      if(self.labels == undefined) {
+      if (self.labels == undefined) {
         addlogs(config, chalk.white('No Label Found'), 'error');
         return resolve();
       }
       self.labelUids = Object.keys(self.labels);
       return Promise.map(self.labelUids, function (labelUid) {
         let label = self.labels[labelUid];
-        if(label.parent.length != 0) {
+        if (label.parent.length != 0) {
           delete label['parent'];
-        } 
+        }
 
         if (!self.labelUidMapper.hasOwnProperty(labelUid)) {
           let requestOption = {
-              label: label
+            label: label
           };
-          
+
           // return self.createLabels(self.labels[labelUid]);
-          return client.stack({api_key: config.target_stack, management_token: config.management_token}).label().create(requestOption)
-          .then(function (response) {
-            self.labelUidMapper[labelUid] = response.uid;
-            helper.writeFile(labelUidMapperPath, self.labelUidMapper);
-            return;
-          }).catch(function (error) {
-            self.fails.push(label);
-            // addlogs(config, chalk.red('Label: \'' + label.name + '\' failed to be imported\n'), 'error');
-            if(error.errors.name) {
-            addlogs(config, chalk.red('Label: \'' + label.name + '\'  already exist'), 'error');
-            } else {
-            addlogs(config, chalk.red('Label: \'' + label.name + '\' failed to be imported\n'), 'error');
-            }
-            return;
-          });
+          return client.stack({ api_key: config.target_stack, management_token: config.management_token }).label().create(requestOption)
+            .then(function (response) {
+              self.labelUidMapper[labelUid] = response;
+              helper.writeFile(labelUidMapperPath, self.labelUidMapper);
+              return;
+            }).catch(function (error) {
+              self.fails.push(label);
+              // addlogs(config, chalk.red('Label: \'' + label.name + '\' failed to be imported\n'), 'error');
+              if (error.errors.name) {
+                addlogs(config, chalk.red('Label: \'' + label.name + '\'  already exist'), 'error');
+              } else {
+                addlogs(config, chalk.red('Label: \'' + label.name + '\' failed to be imported\n'), 'error');
+              }
+              return;
+            });
         } else {
           // the label has already been created
           addlogs(config, (chalk.white('The label: \'' + label.name +
@@ -102,7 +101,7 @@ importLabels.prototype = {
         }).catch(function (error) {
           // eslint-disable-next-line no-console
           return reject(error);
-        });      
+        });
       }).catch(function (error) {
         // error while importing labels
         helper.writeFile(labelFailsPath, self.fails);
@@ -112,34 +111,36 @@ importLabels.prototype = {
     });
   },
 
-  updateLabels: function() {
-    let self = this;    
+  updateLabels: function () {
+    let self = this;
     return new Promise(function (resolve, reject) {
       let labelsObj = helper.readFile(path.resolve(labelsFolderPath, labelConfig.fileName));
       return Promise.map(self.labelUids, function (labelUid) {
         let label = labelsObj[labelUid];
-        if(self.labelUidMapper.hasOwnProperty(labelUid)) {
+        if (self.labelUidMapper.hasOwnProperty(labelUid)) {
           let newLabelUid = self.labelUidMapper[labelUid];
-          if(label.parent.length != 0) {
+          if (label.parent.length > 0) {
             let parentUids = label.parent;
-            for(let i=0; i<parentUids.length; i++) {
-              if(self.labelUidMapper.hasOwnProperty(parentUids[i])) {
-                label.parent[i] = self.labelUidMapper[parentUids[i]];  
+            for (let i = 0; i < parentUids.length; i++) {
+              if (self.labelUidMapper.hasOwnProperty(parentUids[i])) {
+                label.parent[i] = self.labelUidMapper[parentUids[i]].uid;
               }
             }
+            return client.stack({ api_key: config.target_stack, management_token: config.management_token }).label(newLabelUid.uid).fetch()
+            .then(function (response) {
+              //Object.assign(response, _.cloneDeep(label))
+              response.parent = label.parent
+              response.update().then((result) => {
+                self.success.push(result);
+              }).catch(error => {
+                return reject(error);
+              })
+            }).catch(function (error) {
+              return reject(error);
+            });
           }
 
-          return client.stack({api_key: config.target_stack, management_token: config.management_token}).label(newLabelUid).fetch()
-          .then(function (response) {
-            Object.assign(response, _.cloneDeep(label))
-            response.update().then((result) => {              
-            self.success.push(result);
-            }).catch(error => {
-              return reject(error);
-            })
-          }).catch(function (error) {
-            return reject(error);
-          });
+          
         }
       }, {
         concurrency: reqConcurrency
