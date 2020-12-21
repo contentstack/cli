aefb7bd298afc6eab95944e03366ee3e94290c72
ninadhatkar <ninad.hatkar@contentstack.com>
2020-12-21 11:24:57 +0530

    import: adds delay on timeout or ratelimit SDK

--- packages/contentstack-import/src/lib/util/contentstack-management-sdk.js
+++ packages/contentstack-import/src/lib/util/contentstack-management-sdk.js
@@ -1,5 +1,14 @@
 const contentstacksdk = require('@contentstack/management')
 
+let delay = function (milliseconds) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      console.log(`Continuing after ${milliseconds} of delay...`)
+      return resolve()
+    }, milliseconds)
+  })
+}
+
 exports.Client = function (config) {
   const option = {
     host: config.host,
@@ -8,8 +17,10 @@ exports.Client = function (config) {
     maxContentLength: 100000000,
     maxBodyLength: 1000000000,
     logHandler: (level, data) => { },
-    retryCondition: error => {
-      console.log('I am here in custom retry condition', error)
+    retryCondition: async error => {
+      console.log('execute')
+      await delay(500)
+      console.log('I am here in custom retry condition', error.response.status)
       if (error.response && (error.response.status === 429 || error.response.status === 422)) {
         return true
       }
