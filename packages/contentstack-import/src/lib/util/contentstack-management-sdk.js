378dfeefec0987dbef8ab3414929ef79d252bd92
ninadhatkar <ninad.hatkar@contentstack.com>
2020-12-21 08:47:44 +0530

    import:added retry condition in management SDK

--- packages/contentstack-import/src/lib/util/contentstack-management-sdk.js
+++ packages/contentstack-import/src/lib/util/contentstack-management-sdk.js
@@ -1,16 +1,20 @@
 const contentstacksdk = require('@contentstack/management')
 
-
-exports.Client = function (config) {  
-
+exports.Client = function (config) {
   const option = {
     host: config.host,
     authtoken: config.auth_token,
     api_key: config.target_stack,
     maxContentLength: 100000000,
     maxBodyLength: 1000000000,
-    timeout: 240000,
-    logHandler: (level, data) => {}
+    logHandler: (level, data) => { },
+    retryCondition: error => {
+      console.log('I am here in custom retry condition', error)
+      if (error.response && (error.response.status === 429 || error.response.status === 422)) {
+        return true
+      }
+      return false
+    },
   }
   const client = contentstacksdk.client(option)
   return client
