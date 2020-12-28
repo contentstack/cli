3b58972572f1f8792f2499e33b7241034e48cb89
ninadhatkar <ninad.hatkar@contentstack.com>
2020-12-28 12:41:28 +0530

    import:more debug changes

--- packages/contentstack-import/src/lib/util/contentstack-management-sdk.js
+++ packages/contentstack-import/src/lib/util/contentstack-management-sdk.js
@@ -1,7 +1,7 @@
 const contentstacksdk = require('@contentstack/management')
 
 let delay = function (ms) {
-  return new Promise(resolve => {
+  return new Promise(function (resolve, reject) {
     setTimeout(() => {
       console.log(`waited for ${ms} milliseconds`)
       return resolve()
@@ -17,16 +17,25 @@ exports.Client = function (config) {
     maxContentLength: 100000000,
     maxBodyLength: 1000000000,
     logHandler: (level, data) => { },
+    retryCondition: async error => {
+      await delay(15000)
+      console.log('retrying...............', error.response.status)
+      if (error.response && error.response.status === 429) {
+        return true
+      }
+      return false
+    },
     retryDelayOptions: {
-      customBackoff: async function (retryCount, err) {
-        console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>', err.message)
-        await delay(15000)
-        //console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>', err.message)
-        if (retryCount < 5) {
-          return 3000
-        }
-        return -1 // returning -1 will hold next retry for request
-      },
+      base: 3000,
+      // customBackoff: async function (retryCount, err) {
+      // console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>', err.message)
+      // await delay(15000)
+      // console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>', err.message)
+      // if (retryCount < 5) {
+      // return 15000
+      // }
+      // return -1 // returning -1 will hold next retry for request
+      // },
     },
   }
   const client = contentstacksdk.client(option)
