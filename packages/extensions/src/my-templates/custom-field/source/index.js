ef10db1088463e581cf28bb2f8e92c7685310dc9
Abhinav Gupta <abhinav.gupta@contentstack.com>
2021-01-13 09:10:27 +0530

    WIP: Fixing Host Command

--- packages/extensions/src/my-templates/custom-field/source/index.js
+++ packages/extensions/src/my-templates/custom-field/source/index.js
@@ -3,9 +3,16 @@ let extensionField;
 $(document).ready(() => {
   ContentstackUIExtension.init().then((extension) => {
     extensionField = extension;
+    let existingData = extensionField.field.getData()
 
     extensionField.window.enableAutoResizing();
   });
+
+  $("#avatar").on('change', () => {
+  	let file = $("#avatar").prop('files')
+  	debugger
+  	extensionField.field.setData(file)
+  })
 
   window.extensionField = {};
 });
