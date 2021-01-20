516868add1b9dce9195a0f2d7ef7e68f76eee76f
Rohit Mishra <rohit.mishra@contentstack.com>
2021-01-21 02:41:14 +0530

    Error Handling

--- packages/contentstack-clone/src/commands/cm/stack-clone.js
+++ packages/contentstack-clone/src/commands/cm/stack-clone.js
@@ -4,32 +4,23 @@ const credStore = new Configstore('contentstack_cli')
 let config = require('../../lib/util/config/default')
 const { exec } = require("child_process")
 const {CloneHandler} = require('../../lib/util/clone-handler')
-const { resolve } = require('bluebird')
-const { reject } = require('async')
-
-
-// const export  = require('contentstack_cli')
-
 
 class StackCloneCommand extends Command { 
-  static flags = {
-    app: flags.string({required: true})
-  } 
   async run() {
-    let exportCmd  = require('@contentstack/cli-cm-export')
     let _authToken = credStore.get('authtoken')
     if (_authToken && _authToken !== undefined) {
       config.auth_token = _authToken
-      let host = this.config.userConfig.getRegion()
-      if (host.cma.includes("https://") || host.cma.includes("http://") && host.cda.includes("https://") || host.cda.includes("http://")) {
-        let cmaHost = host.cma.split('//')
-        let cdaHost = host.cda.split('//')
-        host.cma = cmaHost[1]
-        host.cda = cdaHost[1]
-      }
+      let host = this.region
+      host.cma = this.cmaHost
+      host.cda = this.cdaHost
       config.host = host
       const cloneHandler = new CloneHandler(config)
-     cloneHandler.organizationSelection("export")  
+     let orgselection = await cloneHandler.organizationSelection("export")
+     orgselection.then(() => {
+       console.log("Stack clone successfully");
+     }).catch((error) => {
+       console.log("Error: ", error);
+     })
     } else {
       console.log("AuthToken is not present in local drive, Hence use 'csdx auth:login' command for login");
     }
