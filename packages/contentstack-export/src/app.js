d10dac61b68b52921cd6c52f35d82b67f6d2d7e0
Rohit Mishra <rohit.mishra@contentstack.com>
2021-01-29 19:06:37 +0530

    Resolved the conflict issues

--- packages/contentstack-export/src/app.js
+++ packages/contentstack-export/src/app.js
@@ -5,8 +5,6 @@ var { addlogs } = require('./lib/util/log')
 const chalk = require('chalk')
 let path = require('path')
 let _ = require('lodash')
-const { config } = require('cli-ux')
-// const { reject } = require('async')
 
 exports.initial = async function (config) {
   return new Promise(function (resolve, reject) {
@@ -15,14 +13,14 @@ exports.initial = async function (config) {
     exports.getConfig = function () {
       return config
     }
-   // try {
+    // try {
     login.login(config).then(async function () {
       var types = config.modules.types
       if (config.moduleName && config.moduleName !== undefined) {
         singleExport(config.moduleName, types, config)
       } else {
         allExport(config, types).then(() => {
-        return resolve()
+          return resolve()
         })
       }
     }).catch(error => {
@@ -33,53 +31,58 @@ exports.initial = async function (config) {
         let objKey = Object.keys(error.errors)
         addlogs(config, chalk.red('Stack fail to export, ' + objKey + "" + error.errors[objKey][0]), 'error')
       }
-    }) 
+    })
   })
-  // } catch(error) {
-  //   console.log("Error me Huuuuu");
-  // } 
-  // })
 }
 
-var singleExport = (moduleName, types, config) => {
+var singleExport = async (moduleName, types, config) => {
   var types = config.modules.types
-  if (types.indexOf(moduleName) > -1) {
-    var exportedModule = require('./lib/export/' + moduleName)
-    exportedModule.start(config).then(function () {
+  try {
+    if (types.indexOf(moduleName) > -1) {
+      let iterateList = ['stack', moduleName]
+      for (let i = 0; i < iterateList.length; i++) {
+        var exportedModule = require('./lib/export/' + iterateList[i])
+        await exportedModule.start(config).then(function (result) {
+          if (iterateList[i] === 'stack') {
+            let master_locale = { master_locale: { code: result.master_locale } }
+            config = _.merge(config, master_locale)
+          }
+        })
+      }
       addlogs(config, moduleName + ' was exported successfully!', 'success')
       addlogs(config, 'The log for this is stored at ' + path.join(config.data, 'logs', 'export'), 'success')
-    }).catch(function (error) {
-      addlogs(config, 'Failed to migrate ' + moduleName, 'error')
-      addlogs(config, error, 'error')
-      addlogs(config, 'The log for this is stored at ' + path.join(config.data, 'logs', 'export'), 'error')
-    })
-  } else {
-    addlogs(config, 'Please provide valid module name.', 'error')
+    } else {
+      addlogs(config, 'Please provide valid module name.', 'error')
+    }
+  } catch (error) {
+    addlogs(config, 'Failed to migrate ' + moduleName, 'error')
+    addlogs(config, error, 'error')
+    addlogs(config, 'The log for this is stored at ' + path.join(config.data, 'logs', 'export'), 'error')
   }
 }
 
 var allExport = async (config, types) => {
-return new Promise(async (resolve, reject) => {
-  try {
-    for (let i = 0; i < types.length; i++) {
-      let type = types[i]
-      var exportedModule = require('./lib/export/' + type)
-      await exportedModule.start(config).then(result => {;
-        if (type === 'stack') {
-          let master_locale = { master_locale: { code: result.master_locale } }
-          config = _.merge(config, master_locale)
-        }
-        return
-      })
+  return new Promise(async (resolve, reject) => {
+    try {
+      for (let i = 0; i < types.length; i++) {
+        let type = types[i]
+        var exportedModule = require('./lib/export/' + type)
+        await exportedModule.start(config).then(result => {
+          if (type === 'stack') {
+            let master_locale = { master_locale: { code: result.master_locale } }
+            config = _.merge(config, master_locale)
+          }
+          return
+        })
+      }
+      addlogs(config, chalk.green('Stack: ' + config.source_stack + ' has been exported succesfully!'), 'success')
+      addlogs(config, 'The log for this is stored at ' + path.join(config.data, 'logs', 'export'), 'success')
+      return resolve()
+    } catch (error) {
+      addlogs(config, chalk.red('Failed to migrate stack: ' + config.source_stack + '. Please check error logs for more info'), 'error')
+      addlogs(config, chalk.red(error.errorMessage), 'error')
+      addlogs(config, 'The log for this is stored at ' + path.join(config.data, 'logs', 'export'), 'error')
+      return reject()
     }
-    addlogs(config, chalk.green('Stack: ' + config.source_stack + ' has been exported succesfully!'), 'success')
-    addlogs(config, 'The log for this is stored at ' + path.join(config.data, 'logs', 'export'), 'success')
-    return resolve()
-  } catch (error) {
-    addlogs(config, chalk.red('Failed to migrate stack: ' + config.source_stack + '. Please check error logs for more info'), 'error')
-    addlogs(config, chalk.red(error.errorMessage), 'error')
-    addlogs(config, 'The log for this is stored at ' + path.join(config.data, 'logs', 'export'), 'error')
-    return reject()
-  }
-})
+  })
 }
