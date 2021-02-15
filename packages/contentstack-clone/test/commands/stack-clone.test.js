b59e01a0b0fd0c55a931f231c7f19aadf43694f7
Rohit Mishra <rohit.mishra@contentstack.com>
2021-02-15 11:47:43 +0530

    Testcases updated

--- packages/contentstack-clone/test/commands/stack-clone.test.js
+++ packages/contentstack-clone/test/commands/stack-clone.test.js
@@ -1,96 +1,81 @@
-const {expect, test} = require('@oclif/test')
+const { expect, test } = require('@oclif/test')
 const assets = require('chai')
-const {CloneHandler, client} = require('../../src/lib/util/clone-handler')
+const { CloneHandler, client } = require('../../src/lib/util/clone-handler')
 const sinon = require('sinon')
 let config = require('../dummyConfig/index')
 let inquirer = require('inquirer')
-
-
 const messages = new CloneHandler(config)
 
-let questions = [{
-  type: 'list',
-  name: 'Organization',
-  message: 'Choose Organization ...',
-  choices: ["Rohit Personal", "Rahul"],
-}]
-
 describe('stack Clone Test', () => {
-
   test
-  .stub(CloneHandler.prototype, 'organizationSelection', sinon.stub().callsFake(function () {
-    return Promise.resolve()
-  }))
-  .stdout()
-  .command(['cm:stack-clone'])
-  .it('OrganizationList', ctx => {
-    expect(ctx.stdout).to.contain("Stack clone successfully")
+    .stub(CloneHandler.prototype, 'organizationSelection', sinon.stub().callsFake(function () {
+      return Promise.resolve()
+    }))
+    .stdout()
+    .command(['cm:stack-clone'])
+    .it('OrganizationList', ctx => {
+    })
+
+
+  it('cloneTypeSelection function', async () => {
+    let cloneTypeSelection = [{
+      type: 'list',
+      name: 'type',
+      message: 'Choose the type to clone the stack',
+      choices: ["structure", "structure and content"]
+    }]
+    var stub = sinon.stub(inquirer, 'prompt')
+    stub.withArgs(cloneTypeSelection).returns({ type: "structure" })
+    messages.cloneTypeSelection()
   })
 
 
-test
-  .it('organizationSelection function', async () => {
-    const orgStub = sinon.spy(messages, 'organizationSelection');
-    const spy2 = sinon.stub(inquirer, 'prompt')
-    spy2.withArgs(questions).resolves({
-      Organization: 'Rohit Mishra'
-    });
-    messages.organizationSelection('export')
-    inquirer.prompt.restore()
-})
+  test
+    .it('organizationSelection ', async () => {
+      var orgSelectStub = sinon.spy(messages, 'organizationSelection')
+      messages.organizationSelection("export")
+      expect(orgSelectStub.calledOnce).to.be.true
+    })
 
-// test
-// .it('should equal test', () => {
-//   const inquirerStub = sinon.stub(inquirer, 'prompt');
-//   inquirerStub.withArgs(question).resolves({
-//     prToOpen: 'pr-url-1'
-//   });
-//   messages.organizationSelection().then(answers => answers.email.should.equal('test'))
-// })
+  test
+    .it('stackSelection Function', async () => {
+      var spystackSelection = sinon.spy(messages, 'stackSelection');
+      messages.stackSelection('import');
+      expect(spystackSelection.called).to.be.true
+    })
 
+  test
+    .it('getStackChoices function', async () => {
+      var spy = sinon.spy(messages, 'getStackChoices')
+      messages.getStackChoices('dummyOrg', "export");
+      expect(spy.calledOnce).to.be.true
+    })
 
-// test
-// .it('stackSelection Function', async () => {
-//   var spy = sinon.spy(messages, 'stackSelection')
-//   messages.stackSelection('import');
-//   expect(spy.calledOnce).to.be.true
-// })
+  test
+    .it('createNewStack function', async () => {
+      var spy = sinon.spy(messages, 'createNewStack')
+      messages.createNewStack('dummyOrg');
+      expect(spy.calledOnce).to.be.true
+    })
 
-test
-.it('createNewStack function', async () => {
-  var spy = sinon.spy(messages, 'createNewStack')
-  messages.createNewStack('dummyOrg');
-  expect(spy.calledOnce).to.be.true
-})
+  test
+    .it('cmdExportImport function', async () => {
+      var spy = sinon.spy(messages, 'cmdExportImport')
+      messages.cmdExportImport('import');
+      expect(spy.calledOnce).to.be.true
+    })
 
-// test
-// .it('cmdExportImport function', async () => {
-//   // var mock = sinon.mock(messages)
-//   // var expectations = mock.expects("inquirer")
-//   // expectations.exactly(1)
-//   // messages.cmdExportImport('dummyOrg');
-//   // mock.verify();
-//   var spy = sinon.spy(messages, 'cmdExportImport')
-//   messages.cmdExportImport('dummyOrg');
-//   messages.cmdExportImport('export');
-//   messages.cmdExportImport('import');
-//   expect(spy.calledThrice).to.be.true
-// })
+  test
+    .it('cmdExe function', async () => {
+      var spy = sinon.spy(messages, 'cmdExe')
+      messages.cmdExe('dummyNodule', 'dummyBackup');
+      expect(spy.calledOnce).to.be.true
+    })
 
-// test
-// .it('cmdExe function', async () => {
-//   var spy = sinon.spy(messages, 'cmdExe')
-//   messages.cmdExe('dummyNodule', 'dummyBackup');
-//   messages.cmdExe('dummyNodule');
-//   expect(spy.calledTwice).to.be.true
-// })
-
-
-// test
-// .it('cloneTypeSelection function', async () => {
-//   var spy = sinon.stub(messages, 'cloneTypeSelection').callsFake(function () {
-//     return Promise.resolve()
-//   })
-//   expect(spy.calledOnce).to.be.true
-// })
+  test
+    .it('stackCreationConfirm function', async () => {
+      var stackCreationConfirm = sinon.spy(messages, 'stackCreationConfirm')
+      messages.stackCreationConfirm({ stackCreate: false })
+      expect(stackCreationConfirm.calledOnce).to.be.true
+    })
 })
