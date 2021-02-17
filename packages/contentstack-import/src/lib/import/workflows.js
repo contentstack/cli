256d10e5b6c18c2bbeffee436a84d8903e303a2c
Rohit Mishra <rohit.mishra@contentstack.com>
2021-02-18 00:50:14 +0530

    Added workflow module support in export and import plugins

--- packages/contentstack-import/src/lib/import/workflows.js
+++ packages/contentstack-import/src/lib/import/workflows.js
@@ -14,101 +14,89 @@ const _ = require('lodash')
 const helper = require('../util/fs');
 const { addlogs } = require('../util/log');
 let config = require('../../config/default')
-let stack = require('../util/contentstack-management-sdk')
+let stack = require('../util/contentstack-management-sdk');
 
 let reqConcurrency = config.concurrency;
-let labelConfig = config.modules.labels;
-let labelsFolderPath
-let labelMapperPath
-let labelUidMapperPath
-let labelSuccessPath
-let labelFailsPath
+let workflowConfig = config.modules.workflows;
+let workflowFolderPath
+let workflowMapperPath
+let workflowUidMapperPath
+let workflowSuccessPath
+let workflowFailsPath
 let client
 
 
-function importLabels() {
-  
-}
-
-exports.start = function (credentialConfig) {
+function importWorkflows() {
   this.fails = [];
   this.success = [];
-  this.labelUidMapper = {};
+  this.workflowUidMapper = {};
   this.labelUids = [];
-  if (fs.existsSync(labelUidMapperPath)) {
-    this.labelUidMapper = helper.readFile(labelUidMapperPath);
-    this.labelUidMapper = this.labelUidMapper || {};
+  if (fs.existsSync(workflowUidMapperPath)) {
+    this.workflowUidMapper = helper.readFile(workflowUidMapperPath);
+    this.workflowUidMapper = this.workflowUidMapper || {};
   }
-//   start: function (credentialConfig) {
+}
+
+importWorkflows.prototype = {
+  start: function (credentialConfig) {
     let self = this;
     config = credentialConfig
     client = stack.Client(config)
-    addlogs(config, chalk.white('Migrating labels'), 'success')
-    labelsFolderPath = path.resolve(config.data, labelConfig.dirName)
-    self.labels = helper.readFile(path.resolve(labelsFolderPath, labelConfig.fileName));
-    labelMapperPath = path.resolve(config.data, 'mapper', 'labels');
-    labelUidMapperPath = path.resolve(config.data, 'mapper', 'labels', 'uid-mapping.json');
-    labelSuccessPath = path.resolve(config.data, 'labels', 'success.json');
-    labelFailsPath = path.resolve(config.data, 'labels', 'fails.json');
-    mkdirp.sync(labelMapperPath);
+    addlogs(config, chalk.white('Migrating workflows'), 'success')
+    workflowFolderPath = path.resolve(config.data, workflowConfig.dirName)
+    self.workflows = helper.readFile(path.resolve(workflowFolderPath, workflowConfig.fileName));
+    workflowMapperPath = path.resolve(config.data, 'mapper', 'workflows');
+    workflowUidMapperPath = path.resolve(config.data, 'mapper', 'workflows', 'uid-mapping.json');
+    workflowSuccessPath = path.resolve(config.data, 'workflows', 'success.json');
+    workflowFailsPath = path.resolve(config.data, 'workflows', 'fails.json');
+    mkdirp.sync(workflowMapperPath);
     return new Promise(function (resolve, reject) {
-      if (self.labels == undefined) {
-        addlogs(config, chalk.white('No Label Found'), 'error');
+      if (self.workflows == undefined) {
+        addlogs(config, chalk.white('No workflow Found'), 'error');
         return resolve();
       }
-      self.labelUids = Object.keys(self.labels);
-      return Promise.map(self.labelUids, function (labelUid) {
-        let label = self.labels[labelUid];
-        if (label.parent.length != 0) {
-          delete label['parent'];
-        }
+      self.workflowsUids = Object.keys(self.workflows);
+      return Promise.map(self.workflowsUids, function (workflowUid) {
+        let workflow = self.workflows[workflowUid];
 
-        if (!self.labelUidMapper.hasOwnProperty(labelUid)) {
+        if (!self.workflowUidMapper.hasOwnProperty(workflowUid)) {
           let requestOption = {
-            label: label
+            workflow: workflow
           };
 
-          // return self.createLabels(self.labels[labelUid]);
-          return client.stack({ api_key: config.target_stack, management_token: config.management_token }).label().create(requestOption)
+          return client.stack({ api_key: config.target_stack, management_token: config.management_token }).workflow().create(requestOption)
             .then(function (response) {
-              self.labelUidMapper[labelUid] = response;
-              helper.writeFile(labelUidMapperPath, self.labelUidMapper);
+              self.workflowUidMapper[workflowUid] = response;
+              helper.writeFile(workflowUidMapperPath, self.workflowUidMapper);
               return;
             }).catch(function (error) {
-              self.fails.push(label);
-              // addlogs(config, chalk.red('Label: \'' + label.name + '\' failed to be imported\n'), 'error');
+              self.fails.push(workflow);
               if (error.errors.name) {
-                addlogs(config, chalk.red('Label: \'' + label.name + '\'  already exist'), 'error');
+                addlogs(config, chalk.red('workflow: \'' + workflow.name + '\'  already exist'), 'error');
               } else {
-                addlogs(config, chalk.red('Label: \'' + label.name + '\' failed to be imported\n'), 'error');
+                addlogs(config, chalk.red('workflow: \'' + workflow.name + '\' failed to be imported\n'), 'error');
               }
               return;
             });
         } else {
-          // the label has already been created
-          addlogs(config, (chalk.white('The label: \'' + label.name +
+          // the workflow has already been created
+          addlogs(config, (chalk.white('The Workflows: \'' + workflow.name +
             '\' already exists. Skipping it to avoid duplicates!')), 'success');
           return;
         }
-        // import 1 labels at a time
+        // import 1 workflows at a time
       }, {
         concurrency: reqConcurrency
       }).then(function () {
-        // eslint-disable-next-line no-undef
-        return self.updateLabels().then(function () {
-          helper.writeFile(labelSuccessPath, self.success);
-          addlogs(config, (chalk.green('Labels have been imported successfully!')), 'success');
-          return resolve();
-        }).catch(function (error) {
-          // eslint-disable-next-line no-console
-          return reject(error);
-        });
+        helper.writeFile(workflowSuccessPath, self.success);
+        addlogs(config, (chalk.green('Workflows have been imported successfully!')), 'success');
+        return resolve();
       }).catch(function (error) {
-        // error while importing labels
-        helper.writeFile(labelFailsPath, self.fails);
-        addlogs(config, chalk.red('Label import failed'), 'error');
+        helper.writeFile(workflowFailsPath, self.fails);
+        addlogs(config, chalk.red('Workflows import failed'), 'error');
         return reject(error);
       });
     });
   }
-module.exports = new importLabels();
+}
+module.exports = new importWorkflows();
