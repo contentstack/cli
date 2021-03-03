5fe1c4784f311a4ec4c5f9e658535ccc7b696d47
Rohit Mishra <rohit.mishra@contentstack.com>
2021-03-03 16:20:39 +0530

    Bug fix for EU region

--- packages/contentstack-import/src/lib/util/import-flags.js
+++ packages/contentstack-import/src/lib/util/import-flags.js
@@ -6,7 +6,7 @@
 let defaultConfig = require('../../config/default')
 let { initial } = require('../../app')
 let _ = require('lodash')
-const {cli} = require('cli-ux')
+const { cli } = require('cli-ux')
 let message = require('../../../messages/index.json')
 
 exports.configWithMToken = function (config, managementTokens, moduleName, host, _authToken) {
@@ -15,23 +15,23 @@ exports.configWithMToken = function (config, managementTokens, moduleName, host,
   if (moduleName && moduleName !== undefined) {
     defaultConfig.moduleName = moduleName
   }
-  defaultConfig.host = host.cma
-  defaultConfig.cdn = host.cda
+  defaultConfig.host = host
+
   defaultConfig.auth_token = _authToken
   defaultConfig = _.merge(defaultConfig, externalConfig)
   initial(defaultConfig)
 }
 
 exports.parameterWithMToken = function (managementTokens, data, moduleName, host, _authToken) {
-  defaultConfig.management_token = managementTokens.token  
+  defaultConfig.management_token = managementTokens.token
   defaultConfig.target_stack = managementTokens.apiKey
   defaultConfig.auth_token = _authToken
   if (moduleName && moduleName !== undefined) {
     defaultConfig.moduleName = moduleName
   }
   defaultConfig.data = data
-  defaultConfig.host = host.cma
-  defaultConfig.cdn = host.cda
+  defaultConfig.host = host
+
   initial(defaultConfig)
 }
 
@@ -45,8 +45,8 @@ exports.withoutParameterMToken = async (managementTokens, moduleName, host, _aut
     defaultConfig.moduleName = moduleName
   }
   defaultConfig.data = exporteddata
-  defaultConfig.host = host.cma
-  defaultConfig.cdn = host.cda
+  defaultConfig.host = host
+
   initial(defaultConfig)
 }
 
@@ -56,37 +56,37 @@ exports.configWithAuthToken = function (config, _authToken, moduleName, host) {
   if (moduleName && moduleName !== undefined) {
     defaultConfig.moduleName = moduleName
   }
-  defaultConfig.host = host.cma
-  defaultConfig.cdn = host.cda
+  defaultConfig.host = host
+
   if (externalConfig.modules) {
     defaultConfig.modules.types = externalConfig.modules
     delete externalConfig.modules
   }
-  
+
   defaultConfig = _.merge(defaultConfig, externalConfig)
   initial(defaultConfig)
 }
 
 exports.parametersWithAuthToken = function (_authToken, targetStack, data, moduleName, host, backupdir) {
   return new Promise(function (resolve, reject) {
-  defaultConfig.auth_token = _authToken
-  defaultConfig.target_stack = targetStack
-  if (moduleName && moduleName !== undefined && backupdir === undefined) {
-    defaultConfig.moduleName = moduleName
-  } else if (moduleName && moduleName !== undefined && backupdir !== undefined) {
-    defaultConfig.moduleName = moduleName
-    defaultConfig.useBackedupDir = backupdir
-  }
-  defaultConfig.data = data
-  defaultConfig.host = host.cma
-  defaultConfig.cdn = host.cda
-  initial(defaultConfig)
-  .then(() => {
-    return resolve()
-  }).catch((error) => {
-    return reject()
+    defaultConfig.auth_token = _authToken
+    defaultConfig.target_stack = targetStack
+    if (moduleName && moduleName !== undefined && backupdir === undefined) {
+      defaultConfig.moduleName = moduleName
+    } else if (moduleName && moduleName !== undefined && backupdir !== undefined) {
+      defaultConfig.moduleName = moduleName
+      defaultConfig.useBackedupDir = backupdir
+    }
+    defaultConfig.data = data
+    defaultConfig.host = host
+
+    initial(defaultConfig)
+      .then(() => {
+        return resolve()
+      }).catch((error) => {
+        return reject()
+      })
   })
-})
 }
 
 exports.withoutParametersWithAuthToken = async (_authToken, moduleName, host, backupdir) => {
@@ -102,7 +102,7 @@ exports.withoutParametersWithAuthToken = async (_authToken, moduleName, host, ba
     defaultConfig.useBackedupDir = backupdir
   }
 
-  defaultConfig.host = host.cma
-  defaultConfig.cdn = host.cda  
+  defaultConfig.host = host
+
   initial(defaultConfig)
 }
