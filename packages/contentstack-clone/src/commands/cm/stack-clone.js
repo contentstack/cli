dbe85a8b309f7f4e6206df4e59db3d4842175a8a
Rohit Mishra <rohit.mishra@contentstack.com>
2021-03-10 12:34:20 +0530

    Made changes for log path

--- packages/contentstack-clone/src/commands/cm/stack-clone.js
+++ packages/contentstack-clone/src/commands/cm/stack-clone.js
@@ -15,7 +15,7 @@ class StackCloneCommand extends Command {
       config.cdn  = this.cdaHost
       const cloneHandler = new CloneHandler(config)
       let result = await cloneHandler.start()
-      fs.rmdirSync(path.join(__dirname.split("src")[0], 'contents'), { recursive: true });
+     // fs.rmdirSync(path.join(__dirname.split("src")[0], 'contents'), { recursive: true });
       console.log(result);
     } else {
       console.log("AuthToken is not present in local drive, Hence use 'csdx auth:login' command for login");
