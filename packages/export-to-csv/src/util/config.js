42ae522b7961319db2dadbaf3f6ae404becad913
Abhinav Gupta <abhinav.gupta@contentstack.com>
2021-03-15 10:19:40 +0530

    Added fix for files not being created on windows

--- packages/export-to-csv/src/util/config.js
+++ packages/export-to-csv/src/util/config.js
@@ -2,5 +2,6 @@ module.exports = {
 	cancelString: 'Cancel and Exit',
 	exportEntries: 'Export entries to a .CSV file',
 	exportUsers: 'Export organization users\' data to a .CSV file',
-	adminError: 'Unable to export data. Make sure you\'re an admin or owner of this organization'
+	adminError: 'Unable to export data. Make sure you\'re an admin or owner of this organization',
+	organizationNameRegex: /\'/
 }
