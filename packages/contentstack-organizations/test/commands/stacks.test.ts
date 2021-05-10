919ad06bb407da74f97b19b709327da84b3715d0
Abhinav Gupta <abhinav.gupta@contentstack.com>
2021-05-10 16:08:29 +0530

    WIP: Updating test cases for cm:stacks

--- packages/contentstack-organizations/test/commands/stacks.test.ts
+++ packages/contentstack-organizations/test/commands/stacks.test.ts
@@ -8,6 +8,19 @@ const utilities = require('../../../contentstack-utilities/src/index.js')
 import * as mockOrgs from '../mockData/mock-organizations.json'
 import * as mockStacks from '../mockData/mock-stacks.json'
 
+const expectedOutput = `You have 3 stacks
+╔════════════╤════════════╤══════════════════╤══════════╤══════════╗
+║ Name       │ Uid        │ Api Key          │ Org Uid  │ Org Name ║
+╟────────────┼────────────┼──────────────────┼──────────┼──────────╢
+║ mockStack1 │ mockStack1 │ mockStackApiKey1 │ mockOrg1 │ org1     ║
+╟────────────┼────────────┼──────────────────┼──────────┼──────────╢
+║ mockStack2 │ mockStack2 │ mockStackApiKey2 │ mockOrg2 │ org1     ║
+╟────────────┼────────────┼──────────────────┼──────────┼──────────╢
+║ mockStack3 │ mockStack3 │ mockStackApiKey3 │ mockOrg3 │ org1     ║
+╚════════════╧════════════╧══════════════════╧══════════╧══════════╝
+
+`
+
 describe('testing cm:stacks', () => {
 	// sinon.stub(Stack.prototype, 'managementAPIClient').callsFake(() => {
 	// 	return {
@@ -65,296 +78,297 @@ describe('testing cm:stacks', () => {
 	.stdout()
 	.command(['cm:stacks'])
 	.it('displays a list of stacks', ctx => {
+		expect(ctx.stdout).to.equal(expectedOutput)
 	})
 
-	test
-	.stub(Organization.prototype, 'managementAPIClient', () => {
-		return {
-			organization: function() {
-				return {
-					fetchAll: function() {
-						return Promise.resolve(mockOrgs)
-					}
-				}
-			},
-			stack: function() {
-				return {
-					query: function() {
-						return {
-							find: function() {
-								return Promise.resolve(mockStacks)
-							}
-						}
-					}
-				}
-			}
-		}
-	})
-	.stdout()
-	.command(['cm:stacks', '--output=json'])
-	.it('displays a list of organizations', ctx => {
-		debugger
-		expect(JSON.parse(ctx.stdout)).to.be.an('array')
-	})
+	// test
+	// .stub(Organization.prototype, 'managementAPIClient', () => {
+	// 	return {
+	// 		organization: function() {
+	// 			return {
+	// 				fetchAll: function() {
+	// 					return Promise.resolve(mockOrgs)
+	// 				}
+	// 			}
+	// 		},
+	// 		stack: function() {
+	// 			return {
+	// 				query: function() {
+	// 					return {
+	// 						find: function() {
+	// 							return Promise.resolve(mockStacks)
+	// 						}
+	// 					}
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// })
+	// .stdout()
+	// .command(['cm:stacks', '--output=json'])
+	// .it('displays a list of organizations', ctx => {
+	// 	debugger
+	// 	expect(JSON.parse(ctx.stdout)).to.be.an('array')
+	// })
 
-	test
-	.stub(Organization.prototype, 'managementAPIClient', () => {
-		return {
-			organization: function() {
-				return {
-					fetchAll: function() {
-						return Promise.resolve(mockOrgs)
-					}
-				}
-			},
-			stack: function() {
-				return {
-					query: function() {
-						return {
-							find: function() {
-								return Promise.resolve(mockStacks)
-							}
-						}
-					}
-				}
-			}
-		}
-	})
-	.stdout()
-	.command(['cm:stacks', '--csv'])
-	.it('displays a list of organizations', ctx => {
-		// expect(JSON.parse(ctx.stdout)).to.be.an('array')
-	})
+	// test
+	// .stub(Organization.prototype, 'managementAPIClient', () => {
+	// 	return {
+	// 		organization: function() {
+	// 			return {
+	// 				fetchAll: function() {
+	// 					return Promise.resolve(mockOrgs)
+	// 				}
+	// 			}
+	// 		},
+	// 		stack: function() {
+	// 			return {
+	// 				query: function() {
+	// 					return {
+	// 						find: function() {
+	// 							return Promise.resolve(mockStacks)
+	// 						}
+	// 					}
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// })
+	// .stdout()
+	// .command(['cm:stacks', '--csv'])
+	// .it('displays a list of organizations', ctx => {
+	// 	// expect(JSON.parse(ctx.stdout)).to.be.an('array')
+	// })
 
-	test
-	.stub(Organization.prototype, 'managementAPIClient', () => {
-		return {
-			organization: function() {
-				return {
-					fetchAll: function() {
-						return Promise.resolve(mockOrgs)
-					}
-				}
-			},
-			stack: function() {
-				return {
-					query: function() {
-						return {
-							find: function() {
-								return Promise.resolve(mockStacks)
-							}
-						}
-					}
-				}
-			}
-		}
-	})
-	.stdout()
-	.command(['cm:stacks', '--sort=name'])
-	.it('displays a list of organizations', ctx => {
-		// expect(JSON.parse(ctx.stdout)).to.be.an('array')
-	})
+	// test
+	// .stub(Organization.prototype, 'managementAPIClient', () => {
+	// 	return {
+	// 		organization: function() {
+	// 			return {
+	// 				fetchAll: function() {
+	// 					return Promise.resolve(mockOrgs)
+	// 				}
+	// 			}
+	// 		},
+	// 		stack: function() {
+	// 			return {
+	// 				query: function() {
+	// 					return {
+	// 						find: function() {
+	// 							return Promise.resolve(mockStacks)
+	// 						}
+	// 					}
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// })
+	// .stdout()
+	// .command(['cm:stacks', '--sort=name'])
+	// .it('displays a list of organizations', ctx => {
+	// 	// expect(JSON.parse(ctx.stdout)).to.be.an('array')
+	// })
 
-	test
-	.stub(Organization.prototype, 'managementAPIClient', () => {
-		return {
-			organization: function() {
-				return {
-					fetchAll: function() {
-						return Promise.resolve(mockOrgs)
-					}
-				}
-			},
-			stack: function() {
-				return {
-					query: function() {
-						return {
-							find: function() {
-								return Promise.resolve(mockStacks)
-							}
-						}
-					}
-				}
-			}
-		}
-	})
-	.stdout()
-	.command(['cm:stacks', '--sort=-name'])
-	.it('displays a list of organizations', ctx => {
-		// expect(JSON.parse(ctx.stdout)).to.be.an('array')
-	})
+	// test
+	// .stub(Organization.prototype, 'managementAPIClient', () => {
+	// 	return {
+	// 		organization: function() {
+	// 			return {
+	// 				fetchAll: function() {
+	// 					return Promise.resolve(mockOrgs)
+	// 				}
+	// 			}
+	// 		},
+	// 		stack: function() {
+	// 			return {
+	// 				query: function() {
+	// 					return {
+	// 						find: function() {
+	// 							return Promise.resolve(mockStacks)
+	// 						}
+	// 					}
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// })
+	// .stdout()
+	// .command(['cm:stacks', '--sort=-name'])
+	// .it('displays a list of organizations', ctx => {
+	// 	// expect(JSON.parse(ctx.stdout)).to.be.an('array')
+	// })
 
-	test
-	.stub(Organization.prototype, 'managementAPIClient', () => {
-		return {
-			organization: function() {
-				return {
-					fetchAll: function() {
-						return Promise.resolve(mockOrgs)
-					}
-				}
-			},
-			stack: function() {
-				return {
-					query: function() {
-						return {
-							find: function() {
-								return Promise.resolve(mockStacks)
-							}
-						}
-					}
-				}
-			}
-		}
-	})
-	.stdout()
-	.command(['cm:stacks', '--sort=-name'])
-	.it('displays a list of organizations', ctx => {
-		// expect(JSON.parse(ctx.stdout)).to.be.an('array')
-	})
+	// test
+	// .stub(Organization.prototype, 'managementAPIClient', () => {
+	// 	return {
+	// 		organization: function() {
+	// 			return {
+	// 				fetchAll: function() {
+	// 					return Promise.resolve(mockOrgs)
+	// 				}
+	// 			}
+	// 		},
+	// 		stack: function() {
+	// 			return {
+	// 				query: function() {
+	// 					return {
+	// 						find: function() {
+	// 							return Promise.resolve(mockStacks)
+	// 						}
+	// 					}
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// })
+	// .stdout()
+	// .command(['cm:stacks', '--sort=-name'])
+	// .it('displays a list of organizations', ctx => {
+	// 	// expect(JSON.parse(ctx.stdout)).to.be.an('array')
+	// })
 
-	test
-	.stub(Organization.prototype, 'managementAPIClient', () => {
-		return {
-			organization: function() {
-				return {
-					fetchAll: function() {
-						return Promise.resolve(mockOrgs)
-					}
-				}
-			},
-			stack: function() {
-				return {
-					query: function() {
-						return {
-							find: function() {
-								return Promise.resolve(mockStacks)
-							}
-						}
-					}
-				}
-			}
-		}
-	})
-	.stdout()
-	.command(['cm:stacks', '--sort=whatever'])
-	.it('displays a list of organizations', ctx => {
-		// expect(JSON.parse(ctx.stdout)).to.be.an('array')
-	})
+	// test
+	// .stub(Organization.prototype, 'managementAPIClient', () => {
+	// 	return {
+	// 		organization: function() {
+	// 			return {
+	// 				fetchAll: function() {
+	// 					return Promise.resolve(mockOrgs)
+	// 				}
+	// 			}
+	// 		},
+	// 		stack: function() {
+	// 			return {
+	// 				query: function() {
+	// 					return {
+	// 						find: function() {
+	// 							return Promise.resolve(mockStacks)
+	// 						}
+	// 					}
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// })
+	// .stdout()
+	// .command(['cm:stacks', '--sort=whatever'])
+	// .it('displays a list of organizations', ctx => {
+	// 	// expect(JSON.parse(ctx.stdout)).to.be.an('array')
+	// })
 
-	test
-	.stub(Organization.prototype, 'managementAPIClient', () => {
-		return {
-			organization: function() {
-				return {
-					fetchAll: function() {
-						return Promise.resolve(mockOrgs)
-					}
-				}
-			},
-			stack: function() {
-				return {
-					query: function() {
-						return {
-							find: function() {
-								return Promise.resolve(mockStacks)
-							}
-						}
-					}
-				}
-			}
-		}
-	})
-	.stdout()
-	.command(['cm:stacks', '--orgUid=mockOrg1'])
-	.it('displays a list of organizations', ctx => {
-		// expect(JSON.parse(ctx.stdout)).to.be.an('array')
-	})
+	// test
+	// .stub(Organization.prototype, 'managementAPIClient', () => {
+	// 	return {
+	// 		organization: function() {
+	// 			return {
+	// 				fetchAll: function() {
+	// 					return Promise.resolve(mockOrgs)
+	// 				}
+	// 			}
+	// 		},
+	// 		stack: function() {
+	// 			return {
+	// 				query: function() {
+	// 					return {
+	// 						find: function() {
+	// 							return Promise.resolve(mockStacks)
+	// 						}
+	// 					}
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// })
+	// .stdout()
+	// .command(['cm:stacks', '--orgUid=mockOrg1'])
+	// .it('displays a list of organizations', ctx => {
+	// 	// expect(JSON.parse(ctx.stdout)).to.be.an('array')
+	// })
 
-	test
-	.stub(Organization.prototype, 'managementAPIClient', () => {
-		return {
-			organization: function() {
-				return {
-					fetchAll: function() {
-						return Promise.resolve(mockOrgs)
-					}
-				}
-			},
-			stack: function() {
-				return {
-					query: function() {
-						return {
-							find: function() {
-								return Promise.resolve(mockStacks)
-							}
-						}
-					}
-				}
-			}
-		}
-	})
-	.stdout()
-	.command(['cm:stacks', '--orgUid=whatever'])
-	.it('displays a list of organizations', ctx => {
-		// expect(JSON.parse(ctx.stdout)).to.be.an('array')
-	})
+	// test
+	// .stub(Organization.prototype, 'managementAPIClient', () => {
+	// 	return {
+	// 		organization: function() {
+	// 			return {
+	// 				fetchAll: function() {
+	// 					return Promise.resolve(mockOrgs)
+	// 				}
+	// 			}
+	// 		},
+	// 		stack: function() {
+	// 			return {
+	// 				query: function() {
+	// 					return {
+	// 						find: function() {
+	// 							return Promise.resolve(mockStacks)
+	// 						}
+	// 					}
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// })
+	// .stdout()
+	// .command(['cm:stacks', '--orgUid=whatever'])
+	// .it('displays a list of organizations', ctx => {
+	// 	// expect(JSON.parse(ctx.stdout)).to.be.an('array')
+	// })
 
-	test
-	.stub(Organization.prototype, 'managementAPIClient', () => {
-		return {
-			organization: function() {
-				return {
-					fetchAll: function() {
-						return Promise.resolve(mockOrgs)
-					}
-				}
-			},
-			stack: function() {
-				return {
-					query: function() {
-						return {
-							find: function() {
-								return Promise.resolve(mockStacks)
-							}
-						}
-					}
-				}
-			}
-		}
-	})
-	.stdout()
-	.command(['cm:stacks', '--columns=whatever'])
-	.it('displays a list of organizations', ctx => {
-		// expect(JSON.parse(ctx.stdout)).to.be.an('array')
-	})
+	// test
+	// .stub(Organization.prototype, 'managementAPIClient', () => {
+	// 	return {
+	// 		organization: function() {
+	// 			return {
+	// 				fetchAll: function() {
+	// 					return Promise.resolve(mockOrgs)
+	// 				}
+	// 			}
+	// 		},
+	// 		stack: function() {
+	// 			return {
+	// 				query: function() {
+	// 					return {
+	// 						find: function() {
+	// 							return Promise.resolve(mockStacks)
+	// 						}
+	// 					}
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// })
+	// .stdout()
+	// .command(['cm:stacks', '--columns=whatever'])
+	// .it('displays a list of organizations', ctx => {
+	// 	// expect(JSON.parse(ctx.stdout)).to.be.an('array')
+	// })
 
-	test
-	.stub(Organization.prototype, 'managementAPIClient', () => {
-		return {
-			organization: function() {
-				return {
-					fetchAll: function() {
-						return Promise.resolve(mockOrgs)
-					}
-				}
-			},
-			stack: function() {
-				return {
-					query: function() {
-						return {
-							find: function() {
-								return Promise.resolve(mockStacks)
-							}
-						}
-					}
-				}
-			}
-		}
-	})
-	.stdout()
-	.command(['cm:stacks', '--columns=name,uid'])
-	.it('displays a list of organizations', ctx => {
-		// expect(JSON.parse(ctx.stdout)).to.be.an('array')
-	})
+	// test
+	// .stub(Organization.prototype, 'managementAPIClient', () => {
+	// 	return {
+	// 		organization: function() {
+	// 			return {
+	// 				fetchAll: function() {
+	// 					return Promise.resolve(mockOrgs)
+	// 				}
+	// 			}
+	// 		},
+	// 		stack: function() {
+	// 			return {
+	// 				query: function() {
+	// 					return {
+	// 						find: function() {
+	// 							return Promise.resolve(mockStacks)
+	// 						}
+	// 					}
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// })
+	// .stdout()
+	// .command(['cm:stacks', '--columns=name,uid'])
+	// .it('displays a list of organizations', ctx => {
+	// 	// expect(JSON.parse(ctx.stdout)).to.be.an('array')
+	// })
 })
