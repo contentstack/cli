16cf9f64602a8df38907d19c309a0b66d754d241
Abhinav Gupta <abhinav.gupta@contentstack.com>
2021-05-14 18:28:18 +0530

    WIP: possible improvements

--- packages/contentstack/src/commands/config/reset.js
+++ packages/contentstack/src/commands/config/reset.js
@@ -1,11 +1,12 @@
 const {Command, flags} = require('@oclif/command')
 const Configstore = require('configstore')
-const config = new Configstore('contentstack_cli')
 const {cli} = require('cli-ux')
 const chalk = require('chalk')
 
 class ResetCommand extends Command {
   async run() {
+    // const {flags} = this.parse(ResetCommand)
+    const config = new Configstore('contentstack_cli')
     const confirmation = await cli.confirm('Do you want to reset the configuration to default? Region will be set to EU and everything else will be deleted (y/n)')
     if (confirmation) {
       const currentConfig = config.all
@@ -26,5 +27,12 @@ class ResetCommand extends Command {
 ResetCommand.description = `Reset the global cli config
 ...
 `
+
+// ResetCommand.flags = {
+//   backup: flags.boolean({char: 'b', description: 'Creates a backup of the original config'}),
+//   'reset-all': flags.boolean({char: 'r', description: 'Reset all the configuration from the global config'}),
+//   plugin: flags.string({char: 'p', description: 'Reset configuration for a particular plugin'}),
+//   key: flags.string({char: 'k', description: 'Reset configuration for a particular key'}),
+// }
 
 module.exports = ResetCommand
