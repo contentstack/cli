cfa1ce88f6d09dc9ffdd94d6a34e18f3c1d82d64
shafeeq pp <shafeeq.pp@contentstack.com>
2021-06-28 12:03:34 +0530

    fix: +- user config file name type

--- packages/contentstack-core/src/utils/index.ts
+++ packages/contentstack-core/src/utils/index.ts
@@ -2,6 +2,6 @@ export { default as CLIError } from './cli-error';
 export { default as messageHandler } from './message-handler';
 export { default as logger } from './logger';
 export { default as cliux } from './cli-ux';
-export { default as UserConfig } from './user-cofig';
+export { default as UserConfig } from './user-config';
 export { default as Analytics } from './analytics';
 export { default as getSessionId } from './generate-sessionId';
