b2166bdbbe400fa308fe1f63caf16be4cd8df156
shafeeq pp <shafeeq.pp@contentstack.com>
2021-07-07 11:52:46 +0530

    fix: typescript compile issues

--- packages/contentstack-bootstrap/src/commands/cm/bootstrap.ts
+++ packages/contentstack-bootstrap/src/commands/cm/bootstrap.ts
@@ -1,16 +1,21 @@
-import { Command, flags } from '@contentstack/cli-command'
-const ContentstackManagementSDK = require('@contentstack/management')
-import Bootstrap, { BootstrapOptions } from '../../bootstrap'
+import { Command, flags } from '@contentstack/cli-command';
+const ContentstackManagementSDK = require('@contentstack/management');
+import Bootstrap, { BootstrapOptions } from '../../bootstrap';
 import {
   inquireCloneDirectory,
   inquireApp,
   inquireGithubAccessToken,
   inquireAppType,
-} from '../../bootstrap/interactive'
-import config, { getAppLevelConfigByName, AppConfig } from '../../config'
-import messageHandler from '../../messages'
+} from '../../bootstrap/interactive';
+import config, { getAppLevelConfigByName, AppConfig } from '../../config';
+import messageHandler from '../../messages';
 export default class BootstrapCommand extends Command {
   private _managementAPIClient: any;
+  parse: Function;
+  authToken: string;
+  cmaHost: string;
+  error: any;
+  region: string;
 
   static description = 'Bootstrap contentstack apps';
 
@@ -51,53 +56,56 @@ export default class BootstrapCommand extends Command {
   };
 
   get managementAPIClient() {
-    this._managementAPIClient = ContentstackManagementSDK.client({ host: this.cmaHost, authtoken: this.authToken })
-    return this._managementAPIClient
+    this._managementAPIClient = ContentstackManagementSDK.client({ host: this.cmaHost, authtoken: this.authToken });
+    return this._managementAPIClient;
   }
 
   async run() {
-    const { flags } = this.parse(BootstrapCommand)
+    const { flags } = this.parse(BootstrapCommand);
 
     try {
       if (!this.authToken) {
-        this.error(messageHandler.parse('CLI_BOOTSTRAP_LOGIN_FAILED'), { exit: 2, suggestions: ['https://www.contentstack.com/docs/developers/cli/authentication/'] })
+        this.error(messageHandler.parse('CLI_BOOTSTRAP_LOGIN_FAILED'), {
+          exit: 2,
+          suggestions: ['https://www.contentstack.com/docs/developers/cli/authentication/'],
+        });
       }
 
       // inquire user inputs
-      let appType = flags.appType as string
+      let appType = flags.appType as string;
       if (!appType) {
-        appType = await inquireAppType()
+        appType = await inquireAppType();
       }
 
-      const selectedAppName = flags.appName as string
-      let selectedApp
+      const selectedAppName = flags.appName as string;
+      let selectedApp;
       if (!selectedAppName) {
         if (appType === 'sampleapp') {
-          selectedApp = await inquireApp(config.sampleApps)
+          selectedApp = await inquireApp(config.sampleApps);
         } else if (appType === 'starterapp') {
-          selectedApp = await inquireApp(config.starterApps)
+          selectedApp = await inquireApp(config.starterApps);
         } else {
-          this.error('Invalid app type provided ' + appType, { exit: 1 })
+          this.error('Invalid app type provided ' + appType, { exit: 1 });
         }
       }
 
       if (!selectedApp) {
-        this.error(messageHandler.parse('CLI_BOOTSTRAP_INVALID_APP_NAME'), { exit: 1 })
+        this.error(messageHandler.parse('CLI_BOOTSTRAP_INVALID_APP_NAME'), { exit: 1 });
       }
 
-      const appConfig: AppConfig = getAppLevelConfigByName(selectedApp.configKey)
+      const appConfig: AppConfig = getAppLevelConfigByName(selectedApp.configKey);
 
-      let cloneDirectory = flags.directory as string
+      let cloneDirectory = flags.directory as string;
       if (!cloneDirectory) {
-        cloneDirectory = await inquireCloneDirectory()
+        cloneDirectory = await inquireCloneDirectory();
       }
 
       // Check the access token
-      let accessToken = flags.accessToken as string
+      let accessToken = flags.accessToken as string;
       if (appConfig.private && !accessToken) {
-        accessToken = await inquireGithubAccessToken()
+        accessToken = await inquireGithubAccessToken();
       }
-      
+
       // initiate bootstrsourceap
       const options: BootstrapOptions = {
         appConfig,
@@ -105,11 +113,11 @@ export default class BootstrapCommand extends Command {
         managementAPIClient: this.managementAPIClient,
         region: this.region,
         accessToken,
-      }
-      const bootstrap = new Bootstrap(options)
-      await bootstrap.run()
+      };
+      const bootstrap = new Bootstrap(options);
+      await bootstrap.run();
     } catch (error) {
-      this.error(error, { exit: 1, suggestions: error.suggestions })
+      this.error(error, { exit: 1, suggestions: error.suggestions });
     }
   }
 }
