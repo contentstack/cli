c73ff117cef6ee192ff1e6ab49840fe4ea71fb0f
shafeeq pp <shafeeq.pp@contentstack.com>
2021-08-12 12:11:35 +0530

    build: export base code

---
+++ packages/contentstack-export/src/commands/export.ts
@@ -0,0 +1,55 @@
+import { Command, flags } from '@contentstack/cli-command';
+import { logger, cliux, messageHandler, CLIError, configHandler } from '@contentstack/cli-utilities';
+import { ModuleExporter } from '../export';
+import { setupExportConfig } from '../utils';
+
+export default class ExportCommand extends Command {
+  managementAPIClient: any;
+  private readonly parse: Function;
+  private cmaHost: string;
+  authToken: string;
+  private exit: Function;
+  static run;
+  context: any;
+
+  static description = messageHandler.parse('Export content from a stack');
+
+  static examples = [
+    'csdx cm:export -A',
+    'csdx cm:export -A -s <stack_ApiKey> -d <path/of/export/destination/dir>',
+    'csdx cm:export -A -m <single module name>',
+    'csdx cm:export -A -m <single module name> -s <stack_ApiKey> -d <path/of/export/destination/dir>',
+    'csdx cm:export -A -m <single module name> -s <stack_ApiKey> -d <path/of/export/destination/dir>',
+    'csdx cm:export -A -m <single module name> -t <content type>',
+    'csdx cm:export -A -B [optional] branch name',
+    'csdx cm:export -A -c <path/to/config/dir>',
+    'csdx cm:export -a <management_token_alias>',
+    'csdx cm:export -a <management_token_alias> -d <path/to/export/destination/dir>',
+    'csdx cm:export -a <management_token_alias> -c <path/to/config/file>',
+  ];
+
+  static flags = {
+    'external-config': flags.string({ char: 'c', description: '[optional] path of the config' }),
+    'api-key': flags.string({ char: 'k', description: 'API key of the source stack' }),
+    'export-dir': flags.string({ char: 'd', description: 'path or location to store the data' }),
+    'mtoken-alias': flags.string({ char: 'a', description: 'alias of the management token' }),
+    'auth-token': flags.boolean({ char: 'A', description: 'to use auth token' }),
+    module: flags.string({ char: 'm', description: '[optional] specific module name' }),
+    'content-type': flags.string({ char: 't', description: '[optional] content type', multiple: true }),
+    branch: flags.string({ char: 'b', description: '[optional] branch name' }),
+  };
+
+  async run(): Promise<any> {
+    // setup export config
+    // initialize the exporter
+    // start export
+    try {
+      const exportConfig = await setupExportConfig(this.context, this.parse(ExportCommand));
+      const moduleExpoter = new ModuleExporter(this.context, exportConfig);
+      await moduleExpoter.start();
+    } catch (error) {
+      logger.error('Failed to export the content', error);
+      cliux.error('Failed to export content', error.message);
+    }
+  }
+}
