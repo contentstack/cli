2ea4be199ff4ef335fa0bedad7b8655805bff543
Abhinav Gupta <abhinav.gupta@contentstack.com>
2021-08-23 08:16:06 +0530

    refactor: added constant values

---
+++ packages/contentstack-bulk-publish-typescript/src/config/commands.ts
@@ -0,0 +1,13 @@
+export default {
+  'ENTRIES': 'entries',
+  'ASSETS': 'assets',
+  'CLEAR': 'clear',
+  'CONFIGURE': 'configure',
+  'CROSS_PUBLISH': 'cross_publish',
+  'ENTRY_EDITS': 'entry_edits',
+  'NONLOCALIZED_FIELD_CHANGES': 'nonlocalized_field_changes',
+  'REVERT': 'revert',
+  'UNPUBLISH': 'unpublish',
+  'UNPUBLISHED_ENTRIES': 'unpublished_entries',
+  'ADD_FIELDS': 'add_fields'
+}
