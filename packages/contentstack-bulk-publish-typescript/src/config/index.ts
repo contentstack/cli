36bb4475dcab9cede3a914f828c8b9cb1db7a4cf
Abhinav Gupta <abhinav.gupta@contentstack.com>
2021-08-23 08:29:08 +0530

    refactor: added index to config

--- packages/contentstack-bulk-publish-typescript/src/config/index.ts
+++ packages/contentstack-bulk-publish-typescript/src/config/index.ts
@@ -1,6 +1,145 @@
+import constants from './constants'
+import commandNames from './commands'
+
+const commands = {}
+commands[commandNames.ENTRIES] = {
+	configKey: 'publish_entries',
+	flags: [
+		constants.RETRY_FAILED_CONFIRMATION,
+		constants.RETRY_FAILED,
+		constants.ALIAS,
+		constants.PUBLISH_ALL_CONTENT_TYPES,
+		constants.CONTENT_TYPES,
+		constants.BULK_PUBLISH,
+		constants.LOCALES,
+		constants.ENVIRONMENTS,
+		constants.CONFIG,
+		constants.SKIP_WORKFLOW_STAGE_CHECK,
+		constants.QUERY
+	],
+}
+commands[commandNames.ASSETS] = {
+	configKey: 'publish_assets',
+	flags: [
+		constants.ALIAS,
+		constants.RETRY_FAILED,
+		constants.ENVIRONMENTS,
+		constants.FOLDER_UID,
+		constants.BULK_PUBLISH,
+		constants.CONFIG,
+		constants.LOCALES,
+		constants.SKIP_WORKFLOW_STAGE_CHECK,
+		constants.QUERY
+	],
+}
+commands[commandNames.CLEAR] = {}
+commands[commandNames.CONFIGURE] = {}
+commands[commandNames.CROSS_PUBLISH] = {
+	configKey: 'cross_env_publish',
+	flags: [
+		constants.ALIAS,
+		constants.RETRY_FAILED,
+		constants.BULK_PUBLISH,
+		constants.CONTENT_TYPE,
+		constants.LOCALE,
+		constants.ENVIRONMENT,
+		constants.DELIVERY_TOKEN,
+		constants.DESTINATION_ENVIRONMENT,
+		constants.CONFIG,
+		constants.SKIP_WORKFLOW_STAGE_CHECK,
+		constants.QUERY
+	],
+}
+commands[commandNames.ENTRY_EDITS] = {
+	configKey: 'publish_edits_on_env',
+	flags: [
+		constants.ALIAS,
+		constants.RETRY_FAILED,
+		constants.BULK_PUBLISH,
+		constants.ENVIRONMENT,
+		constants.CONTENT_TYPES,
+		constants.LOCALES,
+		constants.ENVIRONMENTS,
+		constants.CONFIG,
+		constants.SKIP_WORKFLOW_STAGE_CHECK,
+		constants.QUERY
+	],
+}
+commands[commandNames.NONLOCALIZED_FIELD_CHANGES] = {
+	configKey: 'nonlocalized_field_changes',
+	flags: [
+		constants.ALIAS,
+		constants.RETRY_FAILED,
+		constants.BULK_PUBLISH,
+		constants.ENVIRONMENT,
+		constants.CONTENT_TYPES,
+		constants.ENVIRONMENTS,
+		constants.CONFIG,
+		constants.SKIP_WORKFLOW_STAGE_CHECK,
+		constants.QUERY
+	],
+}
+commands[commandNames.REVERT] = {
+	configKey: 'revert',
+	flags: [
+		constants.RETRY_FAILED,
+		constants.LOG_FILE
+	],
+}
+commands[commandNames.UNPUBLISH] = {
+	configKey: 'Unpublish',
+	flags: [
+		constants.ALIAS,
+		constants.RETRY_FAILED,
+		constants.BULK_UNPUBLISH,
+		constants.CONTENT_TYPE,
+		constants.LOCALE,
+		constants.ENVIRONMENT,
+		constants.DELIVERY_TOKEN,
+		constants.CONFIG,
+		constants.ONLY_ASSETS,
+		constants.ONLY_ENTRIES,
+		constants.SKIP_WORKFLOW_STAGE_CHECK,
+		constants.QUERY
+	]
+}
+commands[commandNames.UNPUBLISHED_ENTRIES] = {
+	configKey: 'publish_unpublished_env',
+	flags: [
+		constants.ALIAS,
+		constants.RETRY_FAILED,
+		constants.BULK_PUBLISH,
+		constants.ENVIRONMENT,
+		constants.CONTENT_TYPES,
+		constants.LOCALE,
+		constants.ENVIRONMENTS,
+		constants.CONFIG,
+		constants.SKIP_WORKFLOW_STAGE_CHECK,
+		constants.QUERY
+	]
+}
+commands[commandNames.ADD_FIELDS] = {
+	configKey: 'add_fields',
+	flags: [
+		constants.ALIAS,
+		constants.RETRY_FAILED,
+		constants.BULK_PUBLISH,
+		constants.CONTENT_TYPES,
+		constants.LOCALES,
+		constants.ENVIRONMENTS,
+		constants.CONFIG,
+		constants.SKIP_WORKFLOW_STAGE_CHECK,
+		constants.QUERY,
+		constants.SKIP_PUBLISH
+	]
+}
+
+
 export default {
 	logger: {
 		level: 'error',
 		silent: true,
-	}
+	},
+	commands: commands
 }
+
