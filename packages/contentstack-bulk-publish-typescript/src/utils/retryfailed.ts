3cbcffeb86c0f4998b2c8abd7d38c0c7d835b44e
Abhinav Gupta <abhinav.gupta@contentstack.com>
2021-08-24 09:18:57 +0530

    refactor: converted retryFailed to typescript

--- packages/contentstack-bulk-publish-typescript/src/utils/retryfailed.ts
+++ packages/contentstack-bulk-publish-typescript/src/utils/retryfailed.ts
@@ -1,28 +1,31 @@
 const { getAllLogs } = require('./logger');
-const {getStack} = require('./client.js')
 
-module.exports = async (filename, queue, Type) => {
+export function retryFailed (filename, queue, Type, stack): void {
   const logs = await getAllLogs(filename);
   if (logs.file.length > 0) {
     logs.file.forEach((log) => {
       if (Type === 'bulk') {
-        log.message.options.stack = getStack({alias: log.message.alias, host: log.message.host})
+        log.message.options.stack = stack
         queue.Enqueue(log.message.options);
       }
       if (Type === 'publish') {
         if (log.message.options.Type === 'entry') {
           queue.entryQueue.Enqueue({
-            content_type: log.message.options.content_type,
-            publish_details: log.message.options.publish_details,
+            'content_type': log.message.options.content_type,
+            'publish_details': log.message.options.publish_details,
             environments: log.message.options.environments,
             entryUid: log.message.options.entryUid,
             locale: log.message.options.locale,
             Type: 'entry',
-            stack: getStack({alias: log.message.alias, host: log.message.host})
+            stack: stack
           });
         } else {
           queue.assetQueue.Enqueue({
-            assetUid: log.message.options.assetUid, publish_details: log.message.options.publish_assets, environments: log.message.options.environments, Type: 'asset', stack: getStack({alias: log.message.alias, host: log.message.host})
+            assetUid: log.message.options.assetUid, 
+            'publish_details': log.message.options.publish_assets, 
+            environments: log.message.options.environments, 
+            Type: 'asset', 
+            stack: stack
           });
         }
       }
