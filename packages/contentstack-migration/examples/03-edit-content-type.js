6c360ff121299515f433941ebe09c2e63ffc17c9
ninadhatkar <ninad.hatkar@contentstack.com>
2021-09-07 18:53:09 +0530

    updates examples and bug fixes

--- packages/contentstack-migration/examples/03-edit-content-type.js
+++ packages/contentstack-migration/examples/03-edit-content-type.js
@@ -1,15 +1,14 @@
-'use strict'
-
 module.exports = ({migration}) => {
   const foo = migration.editContentType('foo3')
-  foo.description('Edited description')
-  foo.isPage(false)
 
   foo.createField('facebook_link')
   .display_name('facebook')
-  .data_type('link')
+  .data_type('text')
   .mandatory(true)
 
+  foo.createField('twitter_link')
+  .display_name('Twitter')
+  .data_type('text')
+  .mandatory(true)
   migration.addTask(foo.getTaskDefinition())
 }
-
