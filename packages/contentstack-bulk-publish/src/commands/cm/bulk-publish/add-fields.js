122734c66d43ba2e30a9659d6bbc50f5f317734d
Abhinav Gupta <abhinav.gupta@contentstack.com>
2021-09-13 18:37:05 +0530

    fix: changed branch flags to B

--- packages/contentstack-bulk-publish/src/commands/cm/bulk-publish/add-fields.js
+++ packages/contentstack-bulk-publish/src/commands/cm/bulk-publish/add-fields.js
@@ -99,7 +99,7 @@ AddFieldsCommand.flags = {
   environments: flags.string({char: 'e', description: 'Environments to which entries need to be published', multiple: true}),
   config: flags.string({char: 'c', description: 'Path to config file to be used'}),
   yes: flags.boolean({char: 'y', description: 'Agree to process the command with the current configuration'}),
-  branch: flags.string({char: 'n', default: 'master', description: 'Specify the branch to fetch the content from (default is master branch)'}),
+  branch: flags.string({char: 'B', default: 'master', description: 'Specify the branch to fetch the content from (default is master branch)'}),
 }
 
 AddFieldsCommand.examples = [
@@ -115,8 +115,8 @@ AddFieldsCommand.examples = [
   'csdx cm:bulk-publish:add-fields --retryFailed [LOG FILE NAME]',
   'csdx cm:bulk-publish:add-fields -r [LOG FILE NAME]',
   '',
-  'Using --branch or -n flag',
-  'csdx cm:bulk-publish:add-fields -t [CONTENT TYPE 1] [CONTENT TYPE 2] -e [ENVIRONMENT 1] [ENVIRONMENT 2] -l [LOCALE 1] [LOCALE 2] -a [MANAGEMENT TOKEN ALIAS] -n [BRANCH NAME]',
+  'Using --branch or -B flag',
+  'csdx cm:bulk-publish:add-fields -t [CONTENT TYPE 1] [CONTENT TYPE 2] -e [ENVIRONMENT 1] [ENVIRONMENT 2] -l [LOCALE 1] [LOCALE 2] -a [MANAGEMENT TOKEN ALIAS] -B [BRANCH NAME]',
 ]
 
 module.exports = AddFieldsCommand
