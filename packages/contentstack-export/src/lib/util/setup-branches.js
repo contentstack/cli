9195ff420294813f5fe990b6cac8080e163de805
shafeeq pp <shafeeq.pp@contentstack.com>
2021-09-15 12:46:57 +0530

    fix: CS-19372

--- packages/contentstack-export/src/lib/util/setup-branches.js
+++ packages/contentstack-export/src/lib/util/setup-branches.js
@@ -7,28 +7,28 @@ const setupBranches = async (config, branch) => {
   if (typeof config !== "object") {
     throw new Error("Invalid config to setup the branch");
   }
+  let branches = [];
 
-  try {
-    let branches = [];
-    if (typeof branch === "string") {
-      //check branch exists
-      const result = await request({
-        url: `https://${config.host}/v3/stacks/branches/${branch}`,
-        headers: {
-          api_key: config.source_stack,
-          authtoken: config.auth_token,
-        },
-      });
-      if (
-        result &&
-        typeof result.body === "object" &&
-        typeof result.body.branch === "object"
-      ) {
-        branches.push(result.body.branch);
-      } else {
-        throw new Error("No branch found with the name " + branch);
-      }
+  if (typeof branch === "string") {
+    //check branch exists
+    const result = await request({
+      url: `https://${config.host}/v3/stacks/branches/${branch}`,
+      headers: {
+        api_key: config.source_stack,
+        authtoken: config.auth_token,
+      },
+    });
+    if (
+      result &&
+      typeof result.body === "object" &&
+      typeof result.body.branch === "object"
+    ) {
+      branches.push(result.body.branch);
     } else {
+      throw new Error("No branch found with the name " + branch);
+    }
+  } else {
+    try {
       const result = await request({
         url: `https://${config.host}/v3/stacks/branches`,
         headers: {
@@ -36,6 +36,7 @@ const setupBranches = async (config, branch) => {
           authtoken: config.auth_token,
         },
       });
+
       if (
         result &&
         result.body &&
@@ -46,16 +47,16 @@ const setupBranches = async (config, branch) => {
       } else {
         branches.push("main");
       }
+    } catch (error) {
+      return;
     }
-
-    mkdirp.sync(config.data);
-    // create branch info file
-    helper.writeFile(path.join(config.data, "branches.json"), branches);
-    // add branches list in the
-    config.branches = branches;
-  } catch (error) {
-    console.log("failed to setup the branch");
   }
+
+  mkdirp.sync(config.data);
+  // create branch info file
+  helper.writeFile(path.join(config.data, "branches.json"), branches);
+  // add branches list in the
+  config.branches = branches;
 };
 
 module.exports = setupBranches;
