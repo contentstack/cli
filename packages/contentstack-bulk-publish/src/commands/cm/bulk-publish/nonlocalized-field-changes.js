e1cf3a82dd744567a0a21bece9950e08962456a2
Abhinav Gupta <abhinav.gupta@contentstack.com>
2021-09-16 00:17:55 +0530

    fix: updated examples

--- packages/contentstack-bulk-publish/src/commands/cm/bulk-publish/nonlocalized-field-changes.js
+++ packages/contentstack-bulk-publish/src/commands/cm/bulk-publish/nonlocalized-field-changes.js
@@ -103,7 +103,7 @@ NonlocalizedFieldChangesCommand.flags = {
 
 NonlocalizedFieldChangesCommand.examples = [
   'General Usage',
-  'csdx cm:bulk-publish:nonlocalized-field-changes -t [CONTENT TYPE 1] [CONTENT TYPE 2] -e [ENVIRONMENT 1] [ENVIRONMENT 2] -l [LOCALE 1] [LOCALE 2] -a [MANAGEMENT TOKEN ALIAS]',
+  'csdx cm:bulk-publish:nonlocalized-field-changes -t [CONTENT TYPE 1] [CONTENT TYPE 2] -e [ENVIRONMENT 1] [ENVIRONMENT 2] -l [LOCALE 1] [LOCALE 2] -a [MANAGEMENT TOKEN ALIAS] -s [SOURCE ENV]',
   '',
   'Using --config or -c flag',
   'Generate a config file at the current working directory using `csdx cm:bulk-publish:configure -a [ALIAS]`',
@@ -115,7 +115,7 @@ NonlocalizedFieldChangesCommand.examples = [
   'csdx cm:bulk-publish:nonlocalized-field-changes -r [LOG FILE NAME]',
   '',
   'Using --branch or -B flag',
-  'csdx cm:bulk-publish:nonlocalized-field-changes -t [CONTENT TYPE 1] [CONTENT TYPE 2] -e [ENVIRONMENT 1] [ENVIRONMENT 2] -l [LOCALE 1] [LOCALE 2] -a [MANAGEMENT TOKEN ALIAS] -B [BRANCH NAME]',
+  'csdx cm:bulk-publish:nonlocalized-field-changes -t [CONTENT TYPE 1] [CONTENT TYPE 2] -e [ENVIRONMENT 1] [ENVIRONMENT 2] -l [LOCALE 1] [LOCALE 2] -a [MANAGEMENT TOKEN ALIAS] -B [BRANCH NAME] -s [SOURCE ENV]',
 ]
 
 module.exports = NonlocalizedFieldChangesCommand
