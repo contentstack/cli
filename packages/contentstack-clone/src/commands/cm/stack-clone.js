8ae6fe2fa9f2ae930d72960b00312b69ef06a8f6
Abhinav Gupta <abhinav.gupta@contentstack.com>
2021-09-16 13:30:55 +0530

    fix: added cleanup for temp files

--- packages/contentstack-clone/src/commands/cm/stack-clone.js
+++ packages/contentstack-clone/src/commands/cm/stack-clone.js
@@ -1,28 +1,60 @@
-const { Command, flags } = require('@contentstack/cli-command')
+const {Command} = require('@contentstack/cli-command')
 const Configstore = require('configstore')
 const credStore = new Configstore('contentstack_cli')
 const {CloneHandler} = require('../../lib/util/clone-handler')
 let config = require('../../lib/util/dummyConfig.json')
 const path = require('path')
-const fs = require('fs');
 const rimraf = require('rimraf')
+let pathdir = path.join(__dirname.split('src')[0], 'contents')
 
 class StackCloneCommand extends Command { 
   async run() {
-    let _authToken = credStore.get('authtoken')
-    if (_authToken && _authToken !== undefined) {
-      config.auth_token = _authToken
-      config.host = this.cmaHost
-      config.cdn  = this.cdaHost
-      const cloneHandler = new CloneHandler(config)
-      let result = await cloneHandler.start()
-      var pathdir = path.join(__dirname.split("src")[0], 'contents')
-      rimraf(pathdir, function(err) {
-        console.log("Stack cloning process have been completed successfully");
-      })
-    } else {
-      console.log("AuthToken is not present in local drive, Hence use 'csdx auth:login' command for login");
+    try {
+      this.registerCleanupOnInterrupt(pathdir)
+      let _authToken = credStore.get('authtoken')
+      if (_authToken && _authToken !== undefined) {
+        config.auth_token = _authToken
+        config.host = this.cmaHost
+        config.cdn  = this.cdaHost
+        const cloneHandler = new CloneHandler(config)
+        let result = await cloneHandler.start()
+        let successMessage = 'Stack cloning process have been completed successfully'
+        await this.cleanUp(pathdir, successMessage)
+      } else {
+        console.log("AuthToken is not present in local drive, Hence use 'csdx auth:login' command for login");
+      }
+    } catch (error) {
+      await this.cleanUp(pathdir)
+      // eslint-disable-next-line no-console
+      console.log(error.message || error)
     }
+  }
+
+  async cleanUp(pathDir, message) {
+    return new Promise(resolve => {
+      rimraf(pathDir, function (err) {
+        if (err)
+          throw err
+        if (message) {
+          // eslint-disable-next-line no-console
+          console.log(message)
+        }
+        resolve()
+      })
+    })
+  }
+
+  registerCleanupOnInterrupt(pathDir) {
+    ['SIGINT', 'SIGQUIT', 'SIGKILL']
+    .forEach(signal => process.on(signal, async () => {
+      // eslint-disable-next-line no-console
+      console.log('\nCleaning up')
+      await this.cleanUp(pathDir)
+      // eslint-disable-next-line no-console
+      console.log('done')
+      // eslint-disable-next-line no-process-exit
+      process.exit()
+    }))
   }
 }
 
@@ -31,8 +63,7 @@ Use this plugin to automate the process of cloning a stack in a few steps.
 `
 
 StackCloneCommand.examples = [
-  'csdx cm:stack-clone'
+  'csdx cm:stack-clone',
 ]
-
 
 module.exports = StackCloneCommand
