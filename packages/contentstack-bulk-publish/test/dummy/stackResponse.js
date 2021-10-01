00887c20af9c38cc6db9a8f181689dae69a825f2
Abhinav Gupta <abhinav.gupta@contentstack.com>
2021-10-01 17:46:15 +0530

    fix: removed hardcoded ids and dates

--- packages/contentstack-bulk-publish/test/dummy/stackResponse.js
+++ packages/contentstack-bulk-publish/test/dummy/stackResponse.js
@@ -2,16 +2,16 @@ module.exports = {
   stack: {
     created_at: '2019-11-04T07:54:05.013Z',
     updated_at: '2019-11-04T07:54:05.877Z',
-    uid: 'blt88742805ce18a9c7',
+    uid: 'bltmockdata42069007',
     name: 'cross-publishing-test',
     description: 'stack for cross environment publishing test',
-    org_uid: 'bltff6ec735225b72b5',
-    api_key: '***REMOVED***',
+    org_uid: 'bltmockdata42069007',
+    api_key: 'bltmockdata42069007',
     master_locale: 'en-us',
     is_asset_download_public: true,
-    owner_uid: '***REMOVED***',
+    owner_uid: 'bltmockdata42069007',
     user_uids: [
-      '***REMOVED***',
+      'bltmockdata42069007',
     ],
     settings: {
       version: '2019-04-30',
@@ -19,7 +19,7 @@ module.exports = {
       webhook_enabled: true,
       language_fallback: false,
     },
-    master_key: '***REMOVED***',
+    master_key: 'bltmockdata42069007',
     SYS_ACL: {
       others: {
         invite: false,
@@ -32,7 +32,7 @@ module.exports = {
       },
       roles: [
         {
-          uid: 'blt96731c8116bde66d',
+          uid: 'bltmockdata42069007',
           name: 'Developer',
           invite: true,
           sub_acl: {
@@ -43,7 +43,7 @@ module.exports = {
           },
         },
         {
-          uid: 'blt4984bf8fd093b31c',
+          uid: 'bltmockdata42069007',
           name: 'Admin',
           invite: true,
           sub_acl: {
