2a6e8d2b69adce92aa16077368352b223cdecaa5
shafeeq pp <shafeeq.pp@contentstack.com>
2022-01-12 23:02:48 +0530

    ++ updated plugin

--- packages/contentstack-migration/examples/demo/02-merge-contenttype.js
+++ packages/contentstack-migration/examples/demo/02-merge-contenttype.js
@@ -9,6 +9,7 @@
  * @param {Object} managementToken
  * @param {Object} apiKey
  * @param {Object} config  external config provided by the user for the plugin
+ * Note: This could be splitted into multiple tasks as well one for create and one for update
  */
 module.exports = async ({
   migration,
@@ -18,7 +19,7 @@ module.exports = async ({
   apiKey,
   config,
 }) => {
-  const contentTypeUID = "product_teaser";
+  const { contentTypeUID } = config;
   const mergeContentTypes = {
     title: "Merge content types",
     successMessage: "Merged content types successfully",
@@ -26,7 +27,7 @@ module.exports = async ({
     task: async () => {
       // initiating source stack client based on the authentication method provided managementToken/apiKey
       let stackSDKInstanceSource;
-      if (managementToken) {
+      if (typeof managementToken === "string") {
         stackSDKInstanceSource = managementAPIClient.stack({
           management_token: managementToken.token,
           api_key: managementToken.apiKey,
@@ -45,6 +46,22 @@ module.exports = async ({
       const targetContentType = await stackSDKInstance
         .contentType(contentTypeUID)
         .fetch();
+
+      // Creating new content types
+      const newContentTypes = sourceContentType.filter(
+        ({ title }) => !targetContentType.some((x) => x.title == title)
+      );
+      if (Array.isArray(newContentTypes) && newContentTypes.length > 0) {
+        for (const contentType of newContentTypes) {
+          const entryObj = await stackSDKInstance
+            .contentType()
+            .create(contentType);
+        }
+      } else {
+        throw new Error("No new content types found to create");
+      }
+
+      // updating content type
       targetContentType.schema = sourceContentType.schema;
       await targetContentType.update();
     },
