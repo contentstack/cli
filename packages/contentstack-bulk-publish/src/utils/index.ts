e9cacead9aadd1e8e03e13d83f12e389056a7518
Abhinav Gupta <abhinav.gupta@contentstack.com>
2022-01-25 10:27:14 +0530

    WIP: branch validation

--- packages/contentstack-bulk-publish/src/utils/index.ts
+++ packages/contentstack-bulk-publish/src/utils/index.ts
@@ -75,3 +75,42 @@ export function isEmpty(file) {
     })
   })
 }
+
+export async function setupBranches(context, managementAPIClient, exportConfig): Promise<any> {
+  if (typeof exportConfig !== 'object') {
+    throw new Error('Invalid config to setup the branch');
+  }
+
+  try {
+    let branches = [];
+    if (typeof exportConfig.branchName === 'string') {
+      //check branch exists
+      const result = await managementAPIClient.axiosInstance.get({
+        url: `${context.region.cma}/${context.version || 'v3'}/stacks/branches/${exportConfig.branchName}`,
+        headers: {
+          api_key: exportConfig.apiKey,
+          authtoken: context.user.authtoken,
+        },
+      });
+      if (result && typeof result.body === 'object' && typeof result.body.branch === 'object') {
+        branches.push(result.body.branch);
+      }
+      return Promise.reject('No branch found with the name ' + exportConfig.branchName);
+    } else {
+      const result = await managementAPIClient.axiosInstance.get({
+        url: `${context.region.cma}/${context.version || 'v3'}/stacks/branches`,
+        headers: {
+          api_key: exportConfig.apiKey,
+          authtoken: context.user.authtoken,
+        },
+      });
+      if (result && result.body && Array.isArray(result.body.branches) && result.body.branches.length > 0) {
+        branches = result.body.branches;
+      }
+    }
+    // add branches list in the
+    exportConfig.branches = branches;
+  } catch (error) {
+    logger.debug('failed to setup the branch', error && error.body);
+  }
+}
