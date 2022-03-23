f890b04bb37c8059f8771faea73d9b7bb34b3081
shafeeq pp <shafeeq.pp@contentstack.com>
2022-03-23 18:09:21 +0530

    fixed asset export

--- packages/contentstack/src/hooks/prerun/print-region.js
+++ packages/contentstack/src/hooks/prerun/print-region.js
@@ -6,10 +6,13 @@ const command = new Command()
 module.exports = async function (opts) {
   if (opts.Command.id !== 'config:get:region') {
     const region = command.region
-    if (!region) {
+    if (!region && opts.Command.id !== 'config:set:region') {
       console.log(chalk.yellow("Please set a region using `csdx config:set:region <region>`"))
       command.exit(2)
     }
-    cli.log(chalk.grey(`Currently using ${region.name} region`))
+    
+    if(region) {
+      cli.log(chalk.grey(`Currently using ${region.name} region`))
+    }
   }
 }
