a219312a9152ab2e336d7dfe056296bc3fb42680
Abhinav Gupta <abhinav.gupta@contentstack.com>
2022-04-06 15:09:42 +0530

    fix: added a fix for all logs being created in a single log file

--- packages/contentstack-import/src/lib/util/log.js
+++ packages/contentstack-import/src/lib/util/log.js
@@ -38,25 +38,48 @@ var myCustomLevels = {
 };
 
 let logger
-function init (_logPath, logfileName) {
+let successTransport
+let errorTransport
+let debugTransport
+
+// removed logfileName from arguments
+function init(_logPath) {
   if (!logger) {
     var logsDir = path.resolve(_logPath, 'logs', 'import')
     // Create dir if doesn't already exist
     mkdirp.sync(logsDir)
-    var logPath = path.join(logsDir, logfileName + '.log');
 
-    var transports = [new (winston.transports.File)({
-      filename: logPath,
+    successTransport = {
+      name: 'success-file',
+      filename: path.join(logsDir, 'success.log'),
       maxFiles: 20,
       maxsize: 1000000,
       tailable: true,
-      json: true
-    })];
+      json: true,
+      level: 'info',
+    }
 
-    transports.push(new (winston.transports.Console)());
+    errorTransport = {
+      name: 'error-file',
+      filename: path.join(logsDir, 'error.log'),
+      maxFiles: 20,
+      maxsize: 1000000,
+      tailable: true,
+      json: true,
+      level: 'error',
+    }
+
+    debugTransport = {
+      name: 'debug-file',
+      filename: path.join(logsDir, 'debug.log'),
+      maxFiles: 20,
+      maxsize: 1000000,
+      tailable: true,
+      json: true,
+      level: 'debug',
+    }
 
     logger = new (winston.Logger)({
-      transports: transports,
       levels: myCustomLevels.levels
     });
   }
@@ -65,6 +88,10 @@ function init (_logPath, logfileName) {
     log: function () {
       var args = slice.call(arguments);
       var logString = returnString(args);
+      logger.clear()
+      logger
+      .add(winston.transports.File, successTransport)
+      .add(winston.transports.Console)
       if (logString) {
         logger.log('info', logString);
       }
@@ -72,6 +99,10 @@ function init (_logPath, logfileName) {
     warn: function () {
       var args = slice.call(arguments);
       var logString = returnString(args);
+      logger.clear()
+      logger
+      .add(winston.transports.File, successTransport)
+      .add(winston.transports.Console)
       if (logString) {
         logger.log('warn', logString);
       }
@@ -79,6 +110,9 @@ function init (_logPath, logfileName) {
     error: function () {
       var args = slice.call(arguments);
       var logString = returnString(args);
+      logger.clear()
+      .add(winston.transports.File, errorTransport)
+      .add(winston.transports.Console)
       if (logString) {
         logger.log('error', logString);
       }
@@ -86,6 +120,10 @@ function init (_logPath, logfileName) {
     debug: function () {
       var args = slice.call(arguments);
       var logString = returnString(args);
+      logger.clear()
+      logger
+      .add(winston.transports.File, debugTransport)
+      .add(winston.transports.Console)
       if (logString) {
         logger.log('debug', logString);
       }
@@ -96,9 +134,11 @@ function init (_logPath, logfileName) {
 exports.addlogs = async (config, message, type) => {
   var configLogPath
   config.source_stack && config.target_stack ? configLogPath = config.data : configLogPath = config.oldPath
+  // ignoring the type argument, as we are not using it to create a logfile anymore
   if (type !== 'error') {
-    init(configLogPath, type).log(message)
+    // removed type argument from init method
+    init(configLogPath).log(message)
   } else {
-    init(configLogPath, type).error(message)
+    init(configLogPath).error(message)
   }
 }
