8a645186bb3299db1dea252ca7fd6a1a75e82cd9
Abhinav Gupta <abhinav.gupta@contentstack.com>
2022-04-12 21:25:58 +0530

    feat: added entry ref resolution for json-rte

--- packages/contentstack-import/src/lib/util/supress-mandatory-fields.js
+++ packages/contentstack-import/src/lib/util/supress-mandatory-fields.js
@@ -14,6 +14,10 @@ var supress = module.exports = function (schema, flag) {
       }
     } else if (schema[i].data_type === 'reference') {
       flag.references = true;
+    } else if (schema[i].data_type === 'json' && schema[i].field_metadata.rich_text_type) {
+      flag.jsonRte = true
+      if (schema[i].field_metadata.embed_entry === true)
+        flag.jsonRteEmbeddedEntries = true;
     }
 
     if ((schema[i].hasOwnProperty('mandatory') && schema[i].mandatory) || (schema[i].hasOwnProperty('unique') &&
