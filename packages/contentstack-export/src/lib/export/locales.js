f9e00c50d2d18c9ed92295cc7a01f8bb70015f89
shafeeq pp <shafeeq.pp@contentstack.com>
2022-04-13 17:03:34 +0530

    new format

--- packages/contentstack-export/src/lib/export/locales.js
+++ packages/contentstack-export/src/lib/export/locales.js
@@ -4,18 +4,17 @@
  * MIT Licensed
  */
 
-const mkdirp = require('mkdirp')
-const path = require('path')
-const chalk = require('chalk')
+const mkdirp = require('mkdirp');
+const path = require('path');
+const chalk = require('chalk');
 
-const helper = require('../util/helper')
-const {addlogs} = require('../util/log')
-let config = require('../../config/default')
-let localeConfig = config.modules.locales
-const masterLocale = config.master_locale
-let requiredKeys = localeConfig.requiredKeys
-let stack = require('../util/contentstack-management-sdk')
-
+const helper = require('../util/helper');
+const { addlogs } = require('../util/log');
+let config = require('../../config/default');
+let localeConfig = config.modules.locales;
+const masterLocale = config.master_locale;
+let requiredKeys = localeConfig.requiredKeys;
+let stack = require('../util/contentstack-management-sdk');
 
 function ExportLocales() {
   this.qs = {
@@ -29,43 +28,48 @@ function ExportLocales() {
     only: {
       BASE: requiredKeys,
     },
-  }
-  this.locales = {}
+  };
+  this.locales = {};
 }
 
 ExportLocales.prototype.start = function (credentialConfig) {
-  this.locales = {}
-  addlogs(credentialConfig, 'Starting locale export', 'success')
-  let self = this
-  config = credentialConfig
-  let localesFolderPath = path.resolve(config.data, (config.branchName || ""), localeConfig.dirName)
-  mkdirp.sync(localesFolderPath)
+  this.locales = {};
+  addlogs(credentialConfig, 'Starting locale export', 'success');
+  let self = this;
+  config = credentialConfig;
+  let localesFolderPath = path.resolve(config.data, config.branchName || '', localeConfig.dirName);
+  mkdirp.sync(localesFolderPath);
 
-  let client = stack.Client(config)
+  let client = stack.Client(config);
   return new Promise(function (resolve, reject) {
-    client.stack({api_key: config.source_stack, management_token: config.management_token}).locale().query(self.qs).find()
-    .then(localeResponse => {
-      if (localeResponse.items.length !== 0) {
-        localeResponse.items.forEach(function (locale) {
-          addlogs(credentialConfig, locale.name + ' locale was exported successfully', 'success')
-          for (const key in locale) {
-            if (requiredKeys.indexOf(key) === -1) {
-              delete locale[key]
+    client
+      .stack({ api_key: config.source_stack, management_token: config.management_token })
+      .locale()
+      .query(self.qs)
+      .find()
+      .then((localeResponse) => {
+        if (localeResponse.items.length !== 0) {
+          localeResponse.items.forEach(function (locale) {
+            addlogs(credentialConfig, locale.name + ' locale was exported successfully', 'success');
+            for (const key in locale) {
+              if (requiredKeys.indexOf(key) === -1) {
+                delete locale[key];
+              }
             }
-          }
-          self.locales[locale.uid] = locale
-        })
-        addlogs(credentialConfig, chalk.green('All the locales have been exported successfully'), 'success')
-      } else if (localeResponse.items.length === 0) {
-        addlogs(credentialConfig, 'No languages found except the master language', 'success')
-      }
-      helper.writeFile(path.join(localesFolderPath, localeConfig.fileName), self.locales)
-      return resolve()
-    }).catch(error => {
-      addlogs(credentialConfig, error, 'error')
-      return reject()
-    })
-  })
-}
+            self.locales[locale.uid] = locale;
+          });
+          addlogs(credentialConfig, chalk.green('All the locales have been exported successfully'), 'success');
+        } else if (localeResponse.items.length === 0) {
+          addlogs(credentialConfig, 'No languages found except the master language', 'success');
+        }
+        helper.writeFile(path.join(localesFolderPath, localeConfig.fileName), self.locales);
+        return resolve();
+      })
+      .catch((error) => {
+        addlogs(credentialConfig, error, 'error');
+        return reject();
+      });
+  });
+};
 
-module.exports = new ExportLocales()
+module.exports = new ExportLocales();
--- packages/contentstack-export/src/lib/util/setup-branches.js
+++ packages/contentstack-export/src/lib/util/setup-branches.js
@@ -1,15 +1,15 @@
-const mkdirp = require("mkdirp");
-const path = require("path");
-const request = require("./request");
-const helper = require("./helper");
+const mkdirp = require('mkdirp');
+const path = require('path');
+const request = require('./request');
+const helper = require('./helper');
 
 const setupBranches = async (config, branch) => {
-  if (typeof config !== "object") {
-    throw new Error("Invalid config to setup the branch");
+  if (typeof config !== 'object') {
+    throw new Error('Invalid config to setup the branch');
   }
   let branches = [];
 
-  if (typeof branch === "string") {
+  if (typeof branch === 'string') {
     //check branch exists
     const result = await request({
       url: `https://${config.host}/v3/stacks/branches/${branch}`,
@@ -18,14 +18,10 @@ const setupBranches = async (config, branch) => {
         authtoken: config.auth_token,
       },
     });
-    if (
-      result &&
-      typeof result.body === "object" &&
-      typeof result.body.branch === "object"
-    ) {
+    if (result && typeof result.body === 'object' && typeof result.body.branch === 'object') {
       branches.push(result.body.branch);
     } else {
-      throw new Error("No branch found with the name " + branch);
+      throw new Error('No branch found with the name ' + branch);
     }
   } else {
     try {
@@ -37,15 +33,10 @@ const setupBranches = async (config, branch) => {
         },
       });
 
-      if (
-        result &&
-        result.body &&
-        Array.isArray(result.body.branches) &&
-        result.body.branches.length > 0
-      ) {
+      if (result && result.body && Array.isArray(result.body.branches) && result.body.branches.length > 0) {
         branches = result.body.branches;
       } else {
-        branches.push("main");
+        branches.push('main');
       }
     } catch (error) {
       return;
@@ -54,7 +45,7 @@ const setupBranches = async (config, branch) => {
 
   mkdirp.sync(config.data);
   // create branch info file
-  helper.writeFile(path.join(config.data, "branches.json"), branches);
+  helper.writeFile(path.join(config.data, 'branches.json'), branches);
   // add branches list in the
   config.branches = branches;
 };
