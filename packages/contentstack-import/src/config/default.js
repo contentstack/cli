f0b41bdfc53c409030e1ab4a1562c681aea01a22
Vikram Kalta <vikram.kalta@contentstack.com>
2022-04-13 21:51:48 +0530

    feat: added support for azure dc in contentstack package

--- packages/contentstack-import/src/config/default.js
+++ packages/contentstack-import/src/config/default.js
@@ -3,7 +3,7 @@ module.exports = {
   // use below hosts for eu region
   // host:'https://eu-api.contentstack.com/v3',
   // use below hosts for azure region
-  // host:'https://azure-api.contentstack.com/v3',
+  // host:'https://azure-na-api.contentstack.com/v3',
   // pass locale, only to migrate entries from that locale
   // not passing `locale` will migrate all the locales present
   // locales: ['fr-fr'],
