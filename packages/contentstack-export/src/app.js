21bb0077562f38360af258f5626c08fa69264423
shafeeq pp <shafeeq.pp@contentstack.com>
2022-04-22 14:55:07 +0530

    clone fix

--- packages/contentstack-export/src/app.js
+++ packages/contentstack-export/src/app.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-redeclare */
-var util = require("./lib/util");
-var login = require("./lib/util/login");
-var { addlogs, unlinkFileLogger } = require("./lib/util/log");
-const setupBranches = require("./lib/util/setup-branches");
-const chalk = require("chalk");
-let path = require("path");
-let _ = require("lodash");
-const cli = require("cli-ux");
+var util = require('./lib/util');
+var login = require('./lib/util/login');
+var { addlogs, unlinkFileLogger } = require('./lib/util/log');
+const setupBranches = require('./lib/util/setup-branches');
+const chalk = require('chalk');
+let path = require('path');
+let _ = require('lodash');
+const cli = require('cli-ux');
 
 exports.initial = async function (config) {
   return new Promise(function (resolve, reject) {
@@ -30,45 +30,34 @@ exports.initial = async function (config) {
                 await singleExport(config.moduleName, types, config, branch.uid);
               } else {
                 await allExport(config, types, branch.uid);
-              } 
+              }
             } catch (error) {
-              console.log('failed export branch', branch.uid, error)
+              console.log('failed export branch', branch.uid, error);
             }
-          } 
+          }
         } else {
           try {
             if (config.moduleName) {
               await singleExport(config.moduleName, types, config);
             } else {
               await allExport(config, types);
-            } 
+            }
           } catch (error) {
-            console.log('failed export contents', error)
+            console.log('failed export contents', error);
           }
         }
-        unlinkFileLogger()
-        resolve()
+        unlinkFileLogger();
+        resolve();
       })
       .catch((error) => {
-        console.log("error", error);
+        console.log('error', error);
         if (error && error.errors && error.errors.api_key) {
-          addlogs(
-            config,
-            chalk.red(
-              "Stack Api key " + error.errors.api_key[0],
-              "Please enter valid Key",
-              "error"
-            )
-          );
-          addlogs(
-            config,
-            "The log for this is stored at " + config.data + "/export/logs",
-            "success"
-          );
+          addlogs(config, chalk.red('Stack Api key ' + error.errors.api_key[0], 'Please enter valid Key', 'error'));
+          addlogs(config, 'The log for this is stored at ' + config.data + '/export/logs', 'success');
         } else {
-          console.log("Stack fail to export")
+          console.log('Stack fail to export');
         }
-        reject(error)
+        reject(error);
       });
   });
 };
@@ -80,42 +69,31 @@ var singleExport = async (moduleName, types, config, branchName) => {
       if (config.modules.dependency && config.modules.dependency[moduleName]) {
         iterateList = config.modules.dependency[moduleName];
       } else {
-        iterateList = ["stack"];
+        iterateList = ['stack'];
       }
       iterateList.push(moduleName);
 
       for (let i = 0; i < iterateList.length; i++) {
-
-        var exportedModule = require("./lib/export/" + iterateList[i]);
+        var exportedModule = require('./lib/export/' + iterateList[i]);
         const result = await exportedModule.start(config, branchName);
-        if (result && iterateList[i] === "stack") {
+        if (result && iterateList[i] === 'stack') {
           let master_locale = {
             master_locale: { code: result.master_locale },
           };
           config = _.merge(config, master_locale);
         }
       }
-      addlogs(config, moduleName + " was exported successfully!", "success");
-      addlogs(
-        config,
-        "The log for this is stored at " +
-          path.join(config.data, "logs", "export"),
-        "success"
-      );
+      addlogs(config, moduleName + ' was exported successfully!', 'success');
+      addlogs(config, 'The log for this is stored at ' + path.join(config.data, 'logs', 'export'), 'success');
     } else {
-      addlogs(config, "Please provide valid module name.", "error");
+      addlogs(config, 'Please provide valid module name.', 'error');
     }
-    return true
+    return true;
   } catch (error) {
-    addlogs(config, "Failed to migrate " + moduleName, "error");
-    addlogs(config, error, "error");
-    addlogs(
-      config,
-      "The log for this is stored at " +
-        path.join(config.data, "logs", "export"),
-      "error"
-    );
-    throw error
+    addlogs(config, 'Failed to migrate ' + moduleName, 'error');
+    addlogs(config, error, 'error');
+    addlogs(config, 'The log for this is stored at ' + path.join(config.data, 'logs', 'export'), 'error');
+    throw error;
   }
 };
 
@@ -125,34 +103,29 @@ var allExport = async (config, types, branchName) => {
     try {
       for (let i = 0; i < types.length; i++) {
         let type = types[i];
-        var exportedModule = require("./lib/export/" + type);
+        var exportedModule = require('./lib/export/' + type);
         const result = await exportedModule.start(config, branchName);
-        if (result && type === "stack") {
+        if (result && type === 'stack') {
           let master_locale = { master_locale: { code: result.code } };
           config = _.merge(config, master_locale);
         }
       }
       addlogs(
         config,
-        chalk.green(
-          "The content of the " +
-            config.source_stack +
-            " has been exported successfully!"
-        ),
-        "success"
+        chalk.green('The content of the ' + config.source_stack + ' has been exported successfully!'),
+        'success',
       );
-      addlogs(
-        config,
-        "The log for this is stored at " +
-          path.join(config.data, "logs", "export"),
-        "success"
-      );
+      addlogs(config, 'The log for this is stored at ' + path.join(config.data, 'logs', 'export'), 'success');
       return resolve();
     } catch (error) {
-      addlogs(config, chalk.red('Failed to migrate stack: ' + config.source_stack + '. Please check error logs for more info'), 'error')
-      addlogs(config, chalk.red(error.errorMessage), 'error')
-      addlogs(config, 'The log for this is stored at ' + path.join(config.data, 'logs', 'export'), 'error')
-      return reject(error)
+      addlogs(
+        config,
+        chalk.red('Failed to migrate stack: ' + config.source_stack + '. Please check error logs for more info'),
+        'error',
+      );
+      addlogs(config, chalk.red(error && error.errorMessage), 'error');
+      addlogs(config, 'The log for this is stored at ' + path.join(config.data, 'logs', 'export'), 'error');
+      return reject(error);
     }
   });
 };
