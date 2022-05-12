d8e62ac0f7334e50e76e13ee99673ef6b4cda7b9
shafeeq pp <shafeeq.pp@contentstack.com>
2022-05-12 14:11:17 +0530

    increased timeout

--- packages/contentstack-import/src/lib/util/contentstack-management-sdk.js
+++ packages/contentstack-import/src/lib/util/contentstack-management-sdk.js
@@ -1,4 +1,7 @@
 const contentstacksdk = require("@contentstack/management");
+const https = require("https");
+
+const { addlogs } = require("./log");
 
 exports.Client = function (config) {
   const option = {
@@ -8,6 +11,17 @@ exports.Client = function (config) {
     api_key: config.target_stack,
     maxContentLength: 100000000,
     maxBodyLength: 1000000000,
+    maxRequests: 10,
+    retryLimit: 5,
+    timeout: 60000,
+    httpsAgent: new https.Agent({
+      maxSockets: 100,
+      maxFreeSockets: 10,
+      keepAlive: true,
+      timeout: 60000, // active socket keepalive for 60 seconds
+      freeSocketTimeout: 30000, // free socket keepalive for 30 seconds
+    }),
+    retryDelay: Math.floor(Math.random() * (8000 - 3000 + 1) + 3000),
     logHandler: (level, data) => {},
     retryCondition: (error) => {
       // no async function should be used here
