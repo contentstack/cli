dc206db9a06308e23dd9137a2d1ce86c3cbc064e
vkalta <vikram.kalta@contentstack.com>
2022-05-20 10:50:29 +0530

    fix: replaced ocliff's command class with contentstack's command class

--- packages/contentstack-bulk-publish/src/commands/cm/bulk-publish/add-fields.js
+++ packages/contentstack-bulk-publish/src/commands/cm/bulk-publish/add-fields.js
@@ -1,4 +1,4 @@
-const { Command, flags } = require('@oclif/command');
+const { Command, flags } = require('@contentstack/cli-command');
 const { start } = require('../../../producer/add-fields');
 const store = require('../../../util/store.js');
 const { cli } = require('cli-ux');
@@ -26,7 +26,7 @@ class AddFieldsCommand extends Command {
         await this.config.runHook('validateManagementTokenAlias', { alias: updatedFlags.alias });
         config = {
           alias: updatedFlags.alias,
-          host: this.config.userConfig.getRegion().cma,
+          host: this.region.cma,
           branch: addFieldsFlags.branch,
         };
         stack = getStack(config);
