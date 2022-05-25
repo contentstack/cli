3822900f1731060438ce5451f0b9ae13f34d6bc8
Sachin Kale <sachin.kale@contentstack.com>
2022-05-25 12:12:27 +0530

    GA respecification of cm:bulk-publish:entries command

--- packages/contentstack-bulk-publish/src/commands/cm/entries/publish.js
+++ packages/contentstack-bulk-publish/src/commands/cm/entries/publish.js
@@ -7,11 +7,21 @@ const { cli } = require('cli-ux');
 const configKey = 'publish_entries';
 const { prettyPrint, formatError } = require('../../../util');
 const { getStack } = require('../../../util/client.js');
+const { printFlagDeprecation } = require('@contentstack/cli-utilities');
 let config;
 
-class EntriesCommand extends Command {
+class PublishEntriesCommand extends Command {
   async run() {
-    const entriesFlags = this.parse(EntriesCommand).flags;
+    const entriesFlags = this.parse(PublishEntriesCommand).flags;
+    entriesFlags.retryFailed = entriesFlags['retry-failed'] || entriesFlags.retryFailed;
+    entriesFlags.contentTypes = entriesFlags['content-type'] || entriesFlags.contentTypes;
+    entriesFlags.bulkPublish = entriesFlags['bulk-publish'] || entriesFlags.bulkPublish;
+    entriesFlags.publishAllContentTypes = entriesFlags['publish-all-content-types'] || entriesFlags.publishAllContentTypes;
+    delete entriesFlags['retry-failed'];
+    delete entriesFlags['content-type'];
+    delete entriesFlags['bulk-publish'];
+    delete entriesFlags['publish-all-content-types'];
+
     let updatedFlags;
     try {
       updatedFlags = entriesFlags.config ? store.updateMissing(configKey, entriesFlags) : entriesFlags;
@@ -29,7 +39,7 @@ class EntriesCommand extends Command {
         try {
           this.getToken(updatedFlags.alias);
         } catch (error) {
-          this.error(`The configured management token alias ${updatedFlags.alias} has not been added yet. Add it using 'csdx auth:tokens:add -a ${updatedFlags.alias}'`, {exit: 2})
+          this.error(`The configured management token alias ${updatedFlags.alias} has not been added yet. Add it using 'csdx auth:tokens:add -a ${updatedFlags.alias}'`, { exit: 2 })
         }
         config = {
           alias: updatedFlags.alias,
@@ -96,23 +106,28 @@ class EntriesCommand extends Command {
     if (data.yes) {
       return true;
     }
-    const confirmation = await cli.confirm('Do you want to continue with this configuration ? [yes or no]');
-    return confirmation;
+    return cli.confirm('Do you want to continue with this configuration ? [yes or no]');
   }
 }
 
-EntriesCommand.description = `Publish entries from multiple content-types to multiple environments and locales
+PublishEntriesCommand.description = `Publish entries from multiple content-types to multiple environments and locales
 The entries command is used for publishing entries from the specified content types, to the
 specified environments and locales 
 
 Content Types, Environments and Locales are required for executing the command successfully
-But, if retryFailed flag is set, then only a logfile is required
+But, if retry-failed flag is set, then only a logfile is required
 `;
 
-EntriesCommand.flags = {
+PublishEntriesCommand.flags = {
   alias: flags.string({ char: 'a', description: 'Alias for the management token to be used' }),
   retryFailed: flags.string({
     char: 'r',
+    description:
+      'Retry failed entries from the logfile (optional, overrides all other flags) This flag is used to retry publishing entries that failed to publish in a previous attempt. A log file for the previous session will be required for processing the failed entries',
+    hidden: true,
+    parse: printFlagDeprecation(['-r', '--retryFailed'], ['--retry-failed']),
+  }),
+  'retry-failed': flags.string({
     description:
       'Retry failed entries from the logfile (optional, overrides all other flags) This flag is used to retry publishing entries that failed to publish in a previous attempt. A log file for the previous session will be required for processing the failed entries',
   }),
@@ -120,18 +135,40 @@ EntriesCommand.flags = {
     char: 'b',
     description:
       "This flag is set to true by default. It indicates that contentstack's bulkpublish API will be used for publishing the entries",
+    hidden: true,
+    parse: printFlagDeprecation(['-b', '--bulkPublish'], ['--bulk-publish']),
+  }),
+  'bulk-publish': flags.string({
+    description:
+      "This flag is set to true by default. It indicates that contentstack's bulkpublish API will be used for publishing the entries",
     default: 'true',
+  }),
+  'publish-all-content-types': flags.boolean({
+    description: 'Publish all content-types (optional, cannot be set when contentTypes flag is set)',
   }),
   publishAllContentTypes: flags.boolean({
     char: 'o',
     description: 'Publish all content-types (optional, cannot be set when contentTypes flag is set)',
+    hidden: true,
+    parse: printFlagDeprecation(['-o', '--publishAllContentTypes'], ['--publish-all-content-types']),
+  }),
+  'content-types': flags.string({
+    description: 'The Content-Types from which entries need to be published',
+    multiple: true,
   }),
   contentTypes: flags.string({
     char: 't',
     description: 'The Content-types from which entries need to be published',
     multiple: true,
+    parse: printFlagDeprecation(['-t', '--contentTypes'], ['--content-types']),
+    hidden: true,
   }),
-  locales: flags.string({ char: 'l', description: 'Locales to which entries need to be published', multiple: true }),
+  locales: flags.string({
+    char: 'l',
+    description: 'Locales to which entries need to be published',
+    multiple: true,
+    parse: printFlagDeprecation(['-l'], ['--locales']),
+  }),
   environments: flags.string({
     char: 'e',
     description: 'Environments to which entries need to be published',
@@ -147,24 +184,27 @@ EntriesCommand.flags = {
     char: 'B',
     default: 'main',
     description: 'Specify the branch to fetch the content from (default is main branch)',
+    parse: printFlagDeprecation(['-B'], ['--branch']),
   }),
 };
 
-EntriesCommand.examples = [
+PublishEntriesCommand.examples = [
   'General Usage',
-  'csdx cm:bulk-publish:entries -t [CONTENT TYPE 1] [CONTENT TYPE 2] -e [ENVIRONMENT 1] [ENVIRONMENT 2] -l [LOCALE 1] [LOCALE 2] -a [MANAGEMENT TOKEN ALIAS]',
+  'csdx cm:entries:publish -t [CONTENT TYPE 1] [CONTENT TYPE 2] -e [ENVIRONMENT 1] [ENVIRONMENT 2] --locale [LOCALE 1] [LOCALE 2] -a [MANAGEMENT TOKEN ALIAS]',
   '',
   'Using --config or -c flag',
-  'Generate a config file at the current working directory using `csdx cm:bulk-publish:configure -a [ALIAS]`',
-  'csdx cm:bulk-publish:entries --config [PATH TO CONFIG FILE]',
-  'csdx cm:bulk-publish:entries -c [PATH TO CONFIG FILE]',
+  'Generate a config file at the current working directory using `csdx cm:stacks:publish-configure -a [ALIAS]`',
+  'csdx cm:entries:publish --config [PATH TO CONFIG FILE]',
+  'csdx cm:entries:publish -c [PATH TO CONFIG FILE]',
   '',
-  'Using --retryFailed or -r flag',
-  'csdx cm:bulk-publish:entries --retryFailed [LOG FILE NAME]',
-  'csdx cm:bulk-publish:entries -r [LOG FILE NAME]',
+  'Using --retry-failed',
+  'csdx cm:entries:publish --retry-failed [LOG FILE NAME]',
+  'csdx cm:entries:publish -r [LOG FILE NAME]',
   '',
-  'Using --branch or -B flag',
-  'csdx cm:bulk-publish:entries -t [CONTENT TYPE 1] [CONTENT TYPE 2] -e [ENVIRONMENT 1] [ENVIRONMENT 2] -l [LOCALE 1] [LOCALE 2] -a [MANAGEMENT TOKEN ALIAS] -B [BRANCH NAME]',
+  'Using --branch',
+  'csdx cm:entries:publish --content-type [CONTENT TYPE 1] [CONTENT TYPE 2] -e [ENVIRONMENT 1] [ENVIRONMENT 2] --locale [LOCALE 1] [LOCALE 2] -a [MANAGEMENT TOKEN ALIAS] --branch [BRANCH NAME]',
 ];
 
-module.exports = EntriesCommand;
+PublishEntriesCommand.aliases = ['cm:bulk-publish:entries']
+
+module.exports = PublishEntriesCommand;
