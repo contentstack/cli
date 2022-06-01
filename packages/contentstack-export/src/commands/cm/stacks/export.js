4dffb90e56f7b8716e982195db3714dc16b75da2
Netraj Patel <netraj.patel@contentstack.com>
2022-06-01 19:03:26 +0530

    Updated export command examples

--- packages/contentstack-export/src/commands/cm/stacks/export.js
+++ packages/contentstack-export/src/commands/cm/stacks/export.js
@@ -93,30 +93,41 @@ ExportCommand.description = `Export content from a stack
 Export content from one stack to another
 `;
 ExportCommand.examples = [
-  'csdx cm:export -k <stack_ApiKey> -d <path/of/export/destination/dir>',
-  'csdx cm:export -c <path/to/config/dir>',
-  'csdx cm:export -a <management_token_alias>',
-  'csdx cm:export -a <management_token_alias> --data-dir <path/to/export/destination/dir>',
-  'csdx cm:export -a <management_token_alias> -c <path/to/config/file>',
-  'csdx cm:export --module <single module name>',
-  'csdx cm:export --branch [optional] branch name',
+  'csdx cm:stacks:export -k <stack_ApiKey> -d <path/of/export/destination/dir>',
+  'csdx cm:stacks:export -c <path/to/config/dir>',
+  'csdx cm:stacks:export -a <management_token_alias>',
+  'csdx cm:stacks:export -a <management_token_alias> --data-dir <path/to/export/destination/dir>',
+  'csdx cm:stacks:export -a <management_token_alias> -c <path/to/config/file>',
+  'csdx cm:stacks:export --module <single module name>',
+  'csdx cm:stacks:export --branch [optional] branch name',
 ];
 
 ExportCommand.flags = {
-  config: flags.string({ char: 'c', description: '[optional] path of the config' }),
+  config: flags.string({
+    char: 'c',
+    description: '[optional] path of the config',
+  }),
   'stack-uid': flags.string({
     char: 's',
     description: 'API key of the source stack',
     parse: printFlagDeprecation(['-s', '--stack-uid'], ['-k', '--stack-api-key']),
   }),
-  'stack-api-key': flags.string({ char: 'k', description: 'API key of the source stack' }),
+  'stack-api-key': flags.string({
+    char: 'k',
+    description: 'API key of the source stack',
+  }),
   data: flags.string({
     char: 'd',
     description: 'path or location to store the data',
     parse: printFlagDeprecation(['--data'], ['--data-dir']),
   }),
-  'data-dir': flags.string({ description: 'path or location to store the data' }),
-  'management-token-alias': flags.string({ char: 'a', description: 'alias of the management token' }),
+  'data-dir': flags.string({
+    description: 'path or location to store the data',
+  }),
+  'management-token-alias': flags.string({
+    char: 'a',
+    description: 'alias of the management token',
+  }),
   'auth-token': flags.boolean({
     char: 'A',
     description: 'to use auth token',
@@ -138,7 +149,9 @@ ExportCommand.flags = {
     description: '[optional] branch name',
     parse: printFlagDeprecation(['-B'], ['--branch']),
   }),
-  'secured-assets': flags.boolean({ description: '[optional] use when assets are secured' }),
+  'secured-assets': flags.boolean({
+    description: '[optional] use when assets are secured',
+  }),
 };
 
 ExportCommand.aliases = ['cm:export'];
