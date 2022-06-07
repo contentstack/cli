a0e48cab3cead1d609d9d7241db9acef71fcce16
Antony <antonyagustineraj@gmail.com>
2022-06-07 10:29:27 +0530

    Lodash dependency added in package manager.

--- packages/contentstack-utilities/src/config-handler.ts
+++ packages/contentstack-utilities/src/config-handler.ts
@@ -1,6 +1,8 @@
 import Conf from 'conf';
 import { v4 as uuid } from 'uuid';
 import { existsSync, unlinkSync } from 'fs'
+import isEmpty from 'lodash.isempty'
+
 
 const ENC_KEY = 'encryptionKey';
 const ENCRYPT_CONF: boolean = true
@@ -57,7 +59,7 @@ class Config {
         const encryptionKey: any = this.getObfuscationKey()
         this.config = new Conf({ configName: CONFIG_NAME, encryptionKey })
 
-        if (Object.keys(configData || {})?.length) {
+        if (!isEmpty(configData)) {
           this.config.set(configData) // NOTE set config data if passed any
         }
       } catch (error) {
@@ -93,7 +95,7 @@ class Config {
     try {
       this.config = new Conf({ configName: CONFIG_NAME })
 
-      if (Object.keys(configData || {})?.length) {
+      if (!isEmpty(configData)) {
         this.config.set(configData) // NOTE set config data if passed any
       }
     } catch (error) {
