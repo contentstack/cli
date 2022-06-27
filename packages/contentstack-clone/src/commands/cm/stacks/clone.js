4f82aadeb76a0cc55075cd7970c510493eb544cc
Antony <antonyagustineraj@gmail.com>
2022-06-27 14:01:24 +0530

    import stack with management alias token code added

--- packages/contentstack-clone/src/commands/cm/stacks/clone.js
+++ packages/contentstack-clone/src/commands/cm/stacks/clone.js
@@ -122,8 +122,8 @@ class StackCloneCommand extends Command {
       if (exitOrError === true) process.exit();
     };
 
-    // exceptions.forEach((event) => process.on(event, cleanUp));
-    // interrupt.forEach((signal) => process.on(signal, () => cleanUp(true)));
+    exceptions.forEach((event) => process.on(event, cleanUp));
+    interrupt.forEach((signal) => process.on(signal, () => cleanUp(true)));
   }
 }
 
