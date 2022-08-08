7c4753a32693752f1e208f8e3c4eb6a94353883a
Antony <antonyagustineraj@gmail.com>
2022-08-08 17:55:23 +0530

    Integration test POC created for auth and config modules

---
+++ packages/contentstack-export/test/integration/helper.js
@@ -0,0 +1,73 @@
+const fs = require('fs');
+const config = require('../../src/config/default');
+const managementSdk = require('../../src/lib/util/contentstack-management-sdk');
+
+const getStack = () => {
+  return managementSdk.Client(config)
+    .stack({ api_key: config.source_stack, management_token: config.management_token })
+}
+
+const getAssetAndFolderCount = () => {
+  return new Promise(async (resolve) => {
+    const stack = getStack()
+    const assetCount = await stack.asset()
+      .query({ include_count: true, limit: 1 })
+      .find()
+      .then(({ count }) => count)
+    const folderCount = await stack.asset()
+      .query({
+        limit: 1,
+        include_count: true,
+        include_folders: true,
+        query: { is_dir: true }
+      })
+      .find()
+      .then(({ count }) => count);
+
+    resolve({ assetCount, folderCount })
+  })
+}
+
+const readJsonFileContents = (filePath) => {
+  return new Promise((resolve, reject) => {
+    try {
+      let buf = '';
+      const stream = fs.createReadStream(filePath, { flags: 'r', encoding: 'utf-8' });
+
+      const processLine = (line) => { // here's where we do something with a line
+        if (line[line.length - 1] == '\r') line = line.substr(0, line.length - 1); // discard CR (0x0D)
+  
+        if (line.length > 0) { // ignore empty lines
+          resolve(JSON.parse(line)) // parse the JSON
+        }
+      }
+
+      const pump = () => {
+        let pos;
+  
+        while ((pos = buf.indexOf('\n')) >= 0) { // keep going while there's a newline somewhere in the buffer
+          if (pos == 0) { // if there's more than one newline in a row, the buffer will now start with a newline
+            buf = buf.slice(1); // discard it
+            continue; // so that the next iteration will start with data
+          }
+          processLine(buf.slice(0, pos)); // hand off the line
+          buf = buf.slice(pos + 1); // and slice the processed data off the buffer
+        }
+      }
+  
+      stream.on('data', function (d) {
+        buf += d.toString(); // when data is read, stash it in a string buffer
+        pump(); // then process the buffer
+      });
+    } catch (error) {
+      console.trace(error)
+      reject(error)
+    }
+  })
+}
+
+module.exports = {
+  getStack,
+  readJsonFileContents,
+  getAssetAndFolderCount
+}
