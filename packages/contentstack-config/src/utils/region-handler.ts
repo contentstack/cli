13e625a45435873df3de1218bbeb6e62fbf21374
Netraj Patel <netraj.patel@contentstack.com>
2022-08-10 17:32:59 +0530

    Reverted region regex validator

--- packages/contentstack-config/src/utils/region-handler.ts
+++ packages/contentstack-config/src/utils/region-handler.ts
@@ -1,8 +1,15 @@
 import { configHandler } from '@contentstack/cli-utilities';
 
 function validURL(str) {
-  const pattern =
-    /^(https?:\/\/)?((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|((\d{1,3}\.){3}\d{1,3}))(\:\d+)?(\/[-a-z\d%_.~+]*)*(\?[;&a-z\d%_.~+=-]*)?(\#[-a-z\d_]*)?$/i;
+  const pattern = new RegExp(
+    '^(https?:\\/\\/)?' + // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+      '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+      '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+      '(\\#[-a-z\\d_]*)?$',
+    'i',
+  ); // fragment locator
   return Boolean(pattern.test(str));
 }
 
