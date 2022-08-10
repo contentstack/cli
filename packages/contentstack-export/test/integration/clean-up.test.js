bc83c737ba331859a4d11b67bca67a56dbc3dcbe
Antony <antonyagustineraj@gmail.com>
2022-08-10 13:39:47 +0530

    Create a fancy-test custom plugin to run one command onto another [Done]

---
+++ packages/contentstack-export/test/integration/clean-up.test.js
@@ -0,0 +1,56 @@
+const { join } = require('path')
+const { existsSync, unlinkSync } = require('fs')
+const { test, expect } = require("./oclif-test")
+const { DEFAULT_TIMEOUT, PRINT_LOGS } = require("./config.json")
+const LogoutCommand = require('@contentstack/cli-auth/lib/commands/auth/logout').default
+const { cliux: CliUx, messageHandler, configHandler } = require("@contentstack/cli-utilities")
+
+describe("Clear all the configurations used to perform the export.", () => {
+  describe('Logging out', async () => {
+    const messageFilePath = join(__dirname, '..', '..', '..', 'contentstack-utilities', 'messages/auth.json');
+
+    beforeEach(() => {
+      messageHandler.init({ messageFilePath });
+    })
+
+    afterEach(() => {
+      messageHandler.init({ messageFilePath: '' });
+    });
+
+    test
+      .timeout(DEFAULT_TIMEOUT || 600000)
+      .stub(CliUx, 'inquire', async () => 'Yes')
+      .stdout({ print: PRINT_LOGS || false })
+      .command(LogoutCommand)
+      .it('Logout should succeed.!', (ctx) => {
+        expect(ctx.stdout).to.a('string').includes('Successfully logged out');
+      });
+  })
+
+  describe('Remove the config file', async () => {
+    let config
+
+    beforeEach(() => {
+      config = configHandler.init()
+    })
+
+    afterEach(() => {
+      config = ''
+    })
+
+    test
+      .timeout(DEFAULT_TIMEOUT || 600000)
+      .stdout({ print: PRINT_LOGS || false })
+      .do(() => {
+        if (config && config.path) {
+          if (existsSync(config.path)) {
+            unlinkSync(config.path) // NOTE remove test config file
+            console.log('Config file removed.!')
+          }
+        }
+      })
+      .it('Config file removed', (ctx) => {
+        expect(ctx.stdout).to.a('string').includes('Config file removed.!');
+      });
+  })
+})
--- packages/contentstack-export/test/integration/init.test.js
+++ packages/contentstack-export/test/integration/init.test.js
@@ -1,13 +1,56 @@
-const { test } = require("./oclif-test")
-const { DEFAULT_TIMEOUT, PRINT_LOGS, REGION_NAME } = require("./config.json")
+const { join } = require('path')
+const { test, expect } = require("./oclif-test")
+const { NodeCrypto, messageHandler } = require("@contentstack/cli-utilities")
+const LoginCommand = require('@contentstack/cli-auth/lib/commands/auth/login').default
 const RegionGetCommand = require('@contentstack/cli-config/lib/commands/config/set/region').default
+const { USERNAME, PASSWORD, ENCRYPTION_KEY, DEFAULT_TIMEOUT, PRINT_LOGS, REGION_NAME } = require("./config.json")
 
-describe("Check if all the configurations available to perform the export else set all configuration data.", () => {
+const crypto = new NodeCrypto({
+  typeIdentifier: '◈',
+  algorithm: 'aes-192-cbc',
+  encryptionKey: ENCRYPTION_KEY || 'gjh67567mn',
+});
+const username = ENCRYPTION_KEY ? crypto.decrypt(USERNAME) : USERNAME;
+const password = ENCRYPTION_KEY ? crypto.decrypt(PASSWORD) : PASSWORD;
+
+describe("Set all the configurations to perform the export.", () => {
   describe('Setting region', async () => {
+    const messageFilePath = join(__dirname, '..', '..', '..', 'contentstack-config', 'messages/index.json');
+
+    beforeEach(() => {
+      messageHandler.init({ messageFilePath });
+    })
+
+    afterEach(() => {
+      messageHandler.init({ messageFilePath: '' });
+    });
+
     test
       .timeout(DEFAULT_TIMEOUT || 600000) // NOTE setting default timeout as 10 minutes
       .stdout({ print: PRINT_LOGS || false })
       .command(RegionGetCommand, [`${REGION_NAME || 'NA'}`])
-      .it('Setting region is done')
+      .it('Setting region is done', (ctx) => {
+        expect(ctx.stdout).to.be.a('string').that.have.includes(`Region has been set to ${REGION_NAME || 'NA'}`);
+      })
+  })
+
+  describe('Authenticate', async () => {
+    const messageFilePath = join(__dirname, '..', '..', '..', 'contentstack-utilities', 'messages/auth.json');
+
+    beforeEach(() => {
+      messageHandler.init({ messageFilePath });
+    })
+
+    afterEach(() => {
+      messageHandler.init({ messageFilePath: '' });
+    });
+
+    test
+      .timeout(DEFAULT_TIMEOUT || 600000) // NOTE setting default timeout as 10 minutes
+      .stdout({ print: PRINT_LOGS || false })
+      .command(LoginCommand, [`-u=${username}`, `-p=${password}`])
+      .it('Login should complete', (ctx) => {
+        expect(ctx.stdout).to.a('string').includes('Successfully logged in!!');
+      });
   })
 })
