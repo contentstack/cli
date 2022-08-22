215a65de980c16cbff2d480c6fe5024f7be48dbf
Antony <antonyagustineraj@gmail.com>
2022-08-22 13:46:19 +0530

    Reading credentials from env code added, clean up test and pre-config test updates

--- packages/contentstack-export/test/integration/clean-up.test.js
+++ packages/contentstack-export/test/integration/clean-up.test.js
@@ -1,65 +1,44 @@
 const { join } = require('path')
 const { existsSync, unlinkSync } = require('fs')
 const { DEFAULT_TIMEOUT, PRINT_LOGS } = require("./config.json")
-const { test, expect } = require("@contentstack/cli-utilities/lib/oclif-test")
+const { test } = require("@contentstack/cli-utilities/lib/oclif-test")
 const LogoutCommand = require('@contentstack/cli-auth/lib/commands/auth/logout').default
 const { cliux: CliUx, messageHandler, configHandler } = require("@contentstack/cli-utilities")
 
-describe("Clear all the configurations used to perform the export.", () => {
-  describe('Logging out', async () => {
-    const messageFilePath = join(__dirname, '..', '..', '..', 'contentstack-utilities', 'messages/auth.json');
 
-    beforeEach(() => {
-      messageHandler.init({ messageFilePath });
-    })
+describe("Cleaning up", () => {
+  let config
 
-    afterEach(() => {
-      messageHandler.init({ messageFilePath: '' });
-    });
+  // NOTE logging out
+  let messageFilePath = join(__dirname, '..', '..', '..', 'contentstack-utilities', 'messages/auth.json');
+  messageHandler.init({ messageFilePath });
 
-    test
-      .timeout(DEFAULT_TIMEOUT || 600000)
-      .stub(CliUx, 'inquire', async () => 'Yes')
-      .stdout({ print: PRINT_LOGS || false })
-      .command(LogoutCommand)
-      .it('Logout should succeed.!', (ctx) => {
-        expect(ctx.stdout).to.a('string').includes('Successfully logged out');
-      });
-  })
-
-  describe('Remove the config file', async () => {
-    let config
-
-    beforeEach(() => {
+  test
+    .timeout(DEFAULT_TIMEOUT || 600000)
+    .stub(CliUx, 'inquire', async () => 'Yes')
+    .stdout({ print: PRINT_LOGS || false })
+    .command(LogoutCommand)
+    .do(() => {
       config = configHandler.init()
     })
+    .do(() => {
+      if (config && config.path) {
+        let keyPath = config.path.split('/')
+        keyPath.pop()
+        keyPath.push(`${process.env.ENC_CONFIG_NAME}.json`)
+        keyPath = keyPath.join('/')
 
-    afterEach(() => {
-      config = ''
-    })
-
-    test
-      .timeout(DEFAULT_TIMEOUT || 600000)
-      .stdout({ print: PRINT_LOGS || false })
-      .do(() => {
-        if (config && config.path) {
-          let keyPath = config.path.split('/')
-          keyPath.pop()
-          keyPath.push(`${process.env.ENC_CONFIG_NAME}.json`)
-          keyPath = keyPath.join('/')
-
-          if (existsSync(keyPath)) {
-            unlinkSync(keyPath) // NOTE remove test config encryption key file
-          }
-
-          if (existsSync(config.path)) {
-            unlinkSync(config.path) // NOTE remove test config file
-            console.log('Config file removed.!')
-          }
+        if (existsSync(keyPath)) {
+          unlinkSync(keyPath) // NOTE remove test config encryption key file
         }
-      })
-      .it('Config file removed', (ctx) => {
-        expect(ctx.stdout).to.a('string').includes('Config file removed.!');
-      });
-  })
+
+        if (existsSync(config.path)) {
+          unlinkSync(config.path) // NOTE remove test config file
+        }
+      }
+    })
+    .it('Cleaning up is done', () => {
+      config = ''
+      messageHandler.init({ messageFilePath: '' });
+    });
 })
