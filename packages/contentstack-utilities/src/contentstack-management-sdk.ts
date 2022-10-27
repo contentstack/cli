bbdf26e4549e9793f795401dd369a109e117faa6
Netraj Patel <netraj.patel@contentstack.com>
2022-10-27 20:42:24 +0530

    Introduced management sdk connection from utility with refresh token

---
+++ packages/contentstack-utilities/src/contentstack-management-sdk.ts
@@ -0,0 +1,94 @@
+import * as ContentstackManagementSDK from '@contentstack/management';
+const https = require('https');
+import { default as configStore } from './config-handler';
+import { default as authHandler } from './auth-handler';
+
+export default async (config) => {
+  try {
+    let managementAPIClient;
+    const option = {
+      host: config.host,
+      management_token: config.management_token,
+      api_key: config.target_stack,
+      maxContentLength: 100000000,
+      maxBodyLength: 1000000000,
+      maxRequests: 10,
+      retryLimit: 3,
+      timeout: 60000,
+      httpsAgent: new https.Agent({
+        maxSockets: 100,
+        maxFreeSockets: 10,
+        keepAlive: true,
+        timeout: 60000, // active socket keepalive for 60 seconds
+        freeSocketTimeout: 30000, // free socket keepalive for 30 seconds
+      }),
+      retryDelay: Math.floor(Math.random() * (8000 - 3000 + 1) + 3000),
+      logHandler: (level, data) => {},
+      retryCondition: (error) => {
+        if (error.response && error.response.status) {
+          switch (error.response.status) {
+            case 401:
+            case 429:
+            case 408:
+              return true;
+
+            default:
+              return false;
+          }
+        }
+      },
+      retryDelayOptions: {
+        base: 1000,
+        customBackoff: (retryCount, error) => {
+          return 1;
+        },
+      },
+      refreshToken: () => {
+        return new Promise((resolve, reject) => {
+          const authorisationType = configStore.get('authorisationType');
+          if (authorisationType === 'BASIC') {
+            // Handle basic auth 401 here
+            reject('Your session is timed out, please login to proceed');
+          } else if (authorisationType === 'OAUTH') {
+            return authHandler
+              .compareOAuthExpiry(true)
+              .then(() => {
+                resolve({
+                  authorization: `Bearer ${configStore.get('oauthAccessToken')}`,
+                });
+              })
+              .catch((error) => {
+                reject(error);
+              });
+          } else {
+            reject('You do not have permissions to perform this action, please login to proceed');
+          }
+        });
+      },
+    };
+    if (typeof config.branchName === 'string') {
+      option['headers'] = {
+        branch: config.branchName,
+      };
+    }
+
+    if (!config.management_token) {
+      const authorisationType = configStore.get('authorisationType');
+      if (authorisationType === 'BASIC') {
+        option['authtoken'] = configStore.get('authtoken');
+      } else if (authorisationType === 'OAUTH') {
+        await authHandler.compareOAuthExpiry();
+        option['authorization'] = `Bearer ${configStore.get('oauthAccessToken')}`;
+      } else {
+        option['authtoken'] = '';
+        option['authorization'] = '';
+      }
+    }
+
+    managementAPIClient = ContentstackManagementSDK.client(option);
+    return managementAPIClient;
+  } catch (error) {
+    console.error(error);
+    throw new Error(error);
+  }
+};
