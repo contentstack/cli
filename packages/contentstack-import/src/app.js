f7681e6ea019e98ded940bbe8e7c4a12b1d14696
Antony <antonyagustineraj@gmail.com>
2022-11-01 12:18:16 +0530

    Marketplace app added in the module list

--- packages/contentstack-import/src/app.js
+++ packages/contentstack-import/src/app.js
@@ -80,7 +80,10 @@ let singleImport = async (moduleName, types, config) => {
         }
       }
 
-      let exportedModule = _.includes(['assets', 'environments', 'extensions', 'global-fields'], moduleName)
+      let exportedModule = _.includes(
+        ['assets', 'environments', 'extensions', 'marketplace-apps', 'global-fields'],
+        moduleName,
+      )
         ? new (require('./lib/import/' + moduleName))(config)
         : require('./lib/import/' + moduleName);
 
@@ -118,7 +121,10 @@ let allImport = async (config, types) => {
     try {
       for (let i = 0; i < types.length; i++) {
         let type = types[i];
-        let exportedModule = _.includes(['assets', 'environments', 'extensions', 'global-fields'], moduleName)
+        let exportedModule = _.includes(
+          ['assets', 'environments', 'extensions', 'marketplace-apps', 'global-fields'],
+          moduleName,
+        )
           ? new (require('./lib/import/' + moduleName))(config)
           : require('./lib/import/' + moduleName);
 
