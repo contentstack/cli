8d861a0ceca8fdf0b2d9693c7d3cd1b4003750ce
Netraj Patel <netraj.patel@contentstack.com>
2022-11-28 19:06:11 +0530

    Removed old management connection dependancy from export command

--- packages/contentstack-export/test/unit/commands/export.test.js
+++ packages/contentstack-export/test/unit/commands/export.test.js
@@ -1,23 +1,22 @@
-const {expect, test, it} = require('@oclif/test')
-const { cliux } = require('@contentstack/cli-utilities');
-const {Client} = require('../../../src/lib/util/contentstack-management-sdk')
-let defaultConfig = require('../../../src/config/default')
-let _ = require('lodash')
-var environmentsMock = require('../mock/environment')
-var extensionsMock = require('../mock/extensions')
-var localeMock = require('../mock/locales')
-var workflowMock = require('../mock/workflow')
-var globalFieldsMock = require('../mock/globalFields')
-var webhooksMock = require('../mock/webhook')
-var assetsMock = require('../mock/assets')
-var assetFetchMock = require('../mock/assetFetch')
-var entriesMock = require('../mock/entries')
-var entriesFetchMock = require('../mock/entryFetch')
-var contentTypeMock = require('../mock/content-types')
-let message = require('../../../messages/index.json')
+const { expect, test, it } = require('@oclif/test');
+const { cliux, managementClient } = require('@contentstack/cli-utilities');
+let defaultConfig = require('../../../src/config/default');
+let _ = require('lodash');
+var environmentsMock = require('../mock/environment');
+var extensionsMock = require('../mock/extensions');
+var localeMock = require('../mock/locales');
+var workflowMock = require('../mock/workflow');
+var globalFieldsMock = require('../mock/globalFields');
+var webhooksMock = require('../mock/webhook');
+var assetsMock = require('../mock/assets');
+var assetFetchMock = require('../mock/assetFetch');
+var entriesMock = require('../mock/entries');
+var entriesFetchMock = require('../mock/entryFetch');
+var contentTypeMock = require('../mock/content-types');
+let message = require('../../../messages/index.json');
 
 // test
-// .stub(require('../../src/lib/util/contentstack-management-sdk'), 'Client', (e) => {
+// .stub(managementClient(), 'Client', (e) => {
 // 	return {
 // 		stack: function() {
 // 			return {
@@ -47,7 +46,7 @@ let message = require('../../../messages/index.json')
 
 // //global-fields
 // test
-// .stub(require('../../src/lib/util/contentstack-management-sdk'), 'Client', (e) => {
+// .stub(managementClient(), 'Client', (e) => {
 // 	return {
 // 		stack: function() {
 // 			return {
@@ -75,8 +74,7 @@ let message = require('../../../messages/index.json')
 // .it('runs method of Global Fields', ctx => {
 // })
 
-test
-.stub(require('../../../src/lib/util/contentstack-management-sdk'), 'Client', e => {
+managementClient(managementClient(), 'Client', (e) => {
   return {
     stack: function () {
       return {
@@ -85,57 +83,54 @@ test
             query: function () {
               return {
                 find: function () {
-                  return Promise.resolve(workflowMock)
+                  return Promise.resolve(workflowMock);
                 },
-              }
+              };
             },
-          }
+          };
         },
-      }
+      };
     },
-  }
+  };
 })
-.stub(cliux, 'prompt', _name => async name => {
-  if (name === message.promptMessageList.promptMasterLocale) return 'en-us'
-  if (name === message.promptMessageList.promptSourceStack) return 'newstackUid'
-  if (name === message.promptMessageList.promptPathStoredData) return '../contents'
-})
-.command(['cm:export',  '--auth-token', '-m',  'locales'])
-.it('runs method of Locales', ctx => {
-})
+  .stub(cliux, 'prompt', (_name) => async (name) => {
+    if (name === message.promptMessageList.promptMasterLocale) return 'en-us';
+    if (name === message.promptMessageList.promptSourceStack) return 'newstackUid';
+    if (name === message.promptMessageList.promptPathStoredData) return '../contents';
+  })
+  .command(['cm:export', '--auth-token', '-m', 'locales'])
+  .it('runs method of Locales', (ctx) => {});
 
 test
-.stub(require('../../../src/lib/util/contentstack-management-sdk'), 'Client', e => {
-  return {
-    stack: function () {
-      return {
-        workflow: function () {
-          // return {
-          // query: function () {
-          return {
-            findAll: function () {
-              return Promise.resolve(localeMock)
-            },
-          }
-          // },
-          // }
-        },
-      }
-    },
-  }
-})
-.stub(cliux, 'prompt', _name => async name => {
-  if (name === message.promptMessageList.promptMasterLocale) return 'en-us'
-  if (name === message.promptMessageList.promptSourceStack) return 'newstackUid'
-  if (name === message.promptMessageList.promptPathStoredData) return '../contents'
-})
-.command(['cm:export',  '--auth-token', '-m',  'workflows'])
-.it('runs method of workflows', ctx => {
-})
-
+  .stub(managementClient(), 'Client', (e) => {
+    return {
+      stack: function () {
+        return {
+          workflow: function () {
+            // return {
+            // query: function () {
+            return {
+              findAll: function () {
+                return Promise.resolve(localeMock);
+              },
+            };
+            // },
+            // }
+          },
+        };
+      },
+    };
+  })
+  .stub(cliux, 'prompt', (_name) => async (name) => {
+    if (name === message.promptMessageList.promptMasterLocale) return 'en-us';
+    if (name === message.promptMessageList.promptSourceStack) return 'newstackUid';
+    if (name === message.promptMessageList.promptPathStoredData) return '../contents';
+  })
+  .command(['cm:export', '--auth-token', '-m', 'workflows'])
+  .it('runs method of workflows', (ctx) => {});
 
 // test
-// .stub(require('../../src/lib/util/contentstack-management-sdk'), 'Client', (e) => {
+// .stub(managementClient(), 'Client', (e) => {
 // 	return {
 // 		stack: function() {
 // 			return {
@@ -164,7 +159,7 @@ test
 // })
 
 // test
-// .stub(require('../../src/lib/util/contentstack-management-sdk'), 'Client', (e) => {
+// .stub(managementClient(), 'Client', (e) => {
 // 	return {
 // 		stack: function() {
 // 			return {
@@ -189,7 +184,7 @@ test
 // })
 
 // test
-// .stub(require('../../src/lib/util/contentstack-management-sdk'), 'Client', (e) => {
+// .stub(managementClient(), 'Client', (e) => {
 // 	return {
 // 		stack: function() {
 // 			return {
@@ -221,7 +216,7 @@ test
 // })
 
 // test
-// .stub(require('../../src/lib/util/contentstack-management-sdk'), 'Client', (e) => {
+// .stub(managementClient(), 'Client', (e) => {
 // 	return {
 // 		stack: function() {
 // 			return {
@@ -253,107 +248,104 @@ test
 // })
 
 test
-.stub(require('../../../src/lib/util/contentstack-management-sdk'), 'Client', e => {
-  return {
-    stack: function () {
-      return {
-        contentType: function () {
-          return {
-            query: function () {
-              return {
-                find: function () {
-                  return Promise.resolve(contentTypeMock)
-                },
-              }
-            },
-          }
-        },
-      }
-    },
-  }
-})
-.stub(cliux, 'prompt', _name => async name => {
-  if (name === message.promptMessageList.promptMasterLocale) return 'en-us'
-  if (name === message.promptMessageList.promptSourceStack) return 'newstackUid'
-  if (name === message.promptMessageList.promptPathStoredData) return '../contents'
-})
-.command(['cm:export',  '--auth-token', '-m',  'content-types'])
-.it('runs method of ContentTypes', ctx => {
-})
+  .stub(managementClient(), 'Client', (e) => {
+    return {
+      stack: function () {
+        return {
+          contentType: function () {
+            return {
+              query: function () {
+                return {
+                  find: function () {
+                    return Promise.resolve(contentTypeMock);
+                  },
+                };
+              },
+            };
+          },
+        };
+      },
+    };
+  })
+  .stub(cliux, 'prompt', (_name) => async (name) => {
+    if (name === message.promptMessageList.promptMasterLocale) return 'en-us';
+    if (name === message.promptMessageList.promptSourceStack) return 'newstackUid';
+    if (name === message.promptMessageList.promptPathStoredData) return '../contents';
+  })
+  .command(['cm:export', '--auth-token', '-m', 'content-types'])
+  .it('runs method of ContentTypes', (ctx) => {});
 
 test
-.stub(require('../../../src/lib/util/contentstack-management-sdk'), 'Client', e => {
-  return {
-    stack: function () {
-      return {
-        contentType: function () {
-          return {
-            entry: function () {
-              return {
-                query: function () {
-                  return {
-                    find: function () {
-                      return Promise.resolve(entriesMock)
-                    },
-                  }
-                },
-                fetch: function () {
-                  return Promise.resolve(entriesFetchMock)
-                },
-              }
-            },
-          }
-        },
-      }
-    },
-  }
-})
-.stub(cliux, 'prompt', _name => async name => {
-  if (name === message.promptMessageList.promptSourceStack) return 'newstackUid'
-  if (name === message.promptMessageList.promptPathStoredData) return '../contents-test'
-})
-.command(['cm:export',  '--auth-token', '-m',  'entries'])
-.it('runs method of environment', ctx => {
-})
-
+  .stub(managementClient(), 'Client', (e) => {
+    return {
+      stack: function () {
+        return {
+          contentType: function () {
+            return {
+              entry: function () {
+                return {
+                  query: function () {
+                    return {
+                      find: function () {
+                        return Promise.resolve(entriesMock);
+                      },
+                    };
+                  },
+                  fetch: function () {
+                    return Promise.resolve(entriesFetchMock);
+                  },
+                };
+              },
+            };
+          },
+        };
+      },
+    };
+  })
+  .stub(cliux, 'prompt', (_name) => async (name) => {
+    if (name === message.promptMessageList.promptSourceStack) return 'newstackUid';
+    if (name === message.promptMessageList.promptPathStoredData) return '../contents-test';
+  })
+  .command(['cm:export', '--auth-token', '-m', 'entries'])
+  .it('runs method of environment', (ctx) => {});
 
 test
-.stub(require('../../../src/lib/util/contentstack-management-sdk'), 'Client', e => {
-  return {
-    stack: function () {
-      return {
-        contentType: function () {
-          return {
-            entry: function () {
-              return {
-                query: function () {
-                  return {
-                    find: function () {
-                      return Promise.resolve(entriesMock)
-                    },
-                  }
-                },
-                fetch: function () {
-                  return Promise.resolve(entriesFetchMock)
-                },
-              }
-            },
-          }
-        },
-      }
-    },
-  }
-})
-.stub(cliux, 'prompt', _name => async name => {
-  if (name === message.promptMessageList.promptMasterLocale) return 'en-us'
-  if (name === message.promptMessageList.promptSourceStack) return 'newstackUid'
-  if (name === message.promptMessageList.promptPathStoredData) return '../contents'
-})
-.command(['cm:export',  '--auth-token', '-m',  'entries', '-t',  'author'])
-.it('Export entry of a specific type')
+  .stub(managementClient(), 'Client', (e) => {
+    return {
+      stack: function () {
+        return {
+          contentType: function () {
+            return {
+              entry: function () {
+                return {
+                  query: function () {
+                    return {
+                      find: function () {
+                        return Promise.resolve(entriesMock);
+                      },
+                    };
+                  },
+                  fetch: function () {
+                    return Promise.resolve(entriesFetchMock);
+                  },
+                };
+              },
+            };
+          },
+        };
+      },
+    };
+  })
+  .stub(cliux, 'prompt', (_name) => async (name) => {
+    if (name === message.promptMessageList.promptMasterLocale) return 'en-us';
+    if (name === message.promptMessageList.promptSourceStack) return 'newstackUid';
+    if (name === message.promptMessageList.promptPathStoredData) return '../contents';
+  })
+  .command(['cm:export', '--auth-token', '-m', 'entries', '-t', 'author'])
+  .it('Export entry of a specific type');
 
 // test
-// .stub(require('../../src/lib/util/contentstack-management-sdk'), 'Client', (e) => {
+// .stub(managementClient(), 'Client', (e) => {
 // 	return {
 // 		stack: function() {
 // 			return {
