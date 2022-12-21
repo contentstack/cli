71e67736f1be4d9e0ee46a300830ff0ae0b9df95
Netraj Patel <netraj.patel@contentstack.com>
2022-12-21 20:18:18 +0530

    Consumed utility based CMA SDK object in exports module

--- packages/contentstack-export/src/lib/util/marketplace-app-helper.js
+++ packages/contentstack-export/src/lib/util/marketplace-app-helper.js
@@ -1,42 +1,43 @@
 let config = require('../../config/default');
-const sdk = require('./contentstack-management-sdk');
-const { cliux, HttpClient, configHandler } = require('@contentstack/cli-utilities');
+const { cliux, HttpClient, configHandler, managementSDKClient } = require('@contentstack/cli-utilities');
 
 const getInstalledExtensions = (config) => {
-  const client = sdk.Client(config)
-
   return new Promise((resolve, reject) => {
-    const queryRequestOptions = {
-      include_marketplace_extensions: true
-    }
-    const {
-      management_token_data: { apiKey: api_key, token: management_token } = { apiKey: config.source_stack },
-      auth_token
-    } = config || { management_token_data: {} }
-
-    if (api_key && management_token) {
-      client
-        .stack({ api_key, management_token })
-        .extension()
-        .query(queryRequestOptions)
-        .find()
-        .then(({ items }) => resolve(items))
-        .catch(reject)
-    } else if (api_key && auth_token) {
-      const { cma } = configHandler.get('region') || {};
-      const headers = {
-        api_key,
-        authtoken: auth_token
-      }
-      const httpClient = new HttpClient().headers(headers);
-      httpClient.get(`${cma}/v3/extensions/?include_marketplace_extensions=true`)
-        .then(({ data: { extensions } }) => resolve(extensions))
-        .catch(reject)
-    } else {
-      resolve([])
-    }
-  })
-}
+    managementSDKClient(config)
+      .then((APIClient) => {
+        const queryRequestOptions = {
+          include_marketplace_extensions: true,
+        };
+        const {
+          management_token_data: { apiKey: api_key, token: management_token } = { apiKey: config.source_stack },
+          auth_token,
+        } = config || { management_token_data: {} };
+        if (api_key && management_token) {
+          const stackAPIClient = APIClient.stack({ api_key, management_token });
+          stackAPIClient
+            .extension()
+            .query(queryRequestOptions)
+            .find()
+            .then(({ items }) => resolve(items))
+            .catch(reject);
+        } else if (api_key && auth_token) {
+          const { cma } = configHandler.get('region') || {};
+          const headers = {
+            api_key,
+            authtoken: auth_token,
+          };
+          const httpClient = new HttpClient().headers(headers);
+          httpClient
+            .get(`${cma}/v3/extensions/?include_marketplace_extensions=true`)
+            .then(({ data: { extensions } }) => resolve(extensions))
+            .catch(reject);
+        } else {
+          resolve([]);
+        }
+      })
+      .catch(reject);
+  });
+};
 
 const getDeveloperHubUrl = async () => {
   const { cma, name } = configHandler.get('region') || {};
@@ -56,6 +57,6 @@ const getDeveloperHubUrl = async () => {
   }
 
   return developerHubBaseUrl.startsWith('http') ? developerHubBaseUrl : `https://${developerHubBaseUrl}`;
-}
+};
 
-module.exports = { getInstalledExtensions, getDeveloperHubUrl }
+module.exports = { getInstalledExtensions, getDeveloperHubUrl };
