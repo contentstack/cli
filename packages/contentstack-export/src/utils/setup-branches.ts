21e5523c426aa352738d03ffd64da7f20a92ecf3
Shafeeq PP <shafeeq.pp@Shafeeqs-MacBook-Pro.local>
2023-01-02 17:32:54 +0530

    fixed bugs

--- packages/contentstack-export/src/utils/setup-branches.ts
+++ packages/contentstack-export/src/utils/setup-branches.ts
@@ -2,7 +2,7 @@ import * as path from 'path';
 import * as mkdirp from 'mkdirp';
 import { writeFileSync, makeDirectory } from './file-helper';
 
-const setupBranches = async (config, branch, stackAPIClient) => {
+const setupBranches = async (config, stackAPIClient) => {
   if (typeof config !== 'object') {
     throw new Error('Invalid config to setup the branch');
   }
@@ -15,13 +15,13 @@ const setupBranches = async (config, branch, stackAPIClient) => {
     headers['authorization'] = config.management_token;
   }
 
-  if (typeof branch === 'string') {
+  if (config.branchName) {
     // check branch exists
-    const result = await stackAPIClient.branch(branch).fetch();
+    const result = await stackAPIClient.branch(config.branchName).fetch();
     if (result && typeof result === 'object') {
       branches.push(result);
     } else {
-      throw new Error('No branch found with the name ' + branch);
+      throw new Error('No branch found with the name ' + config.branchName);
     }
   } else {
     try {
