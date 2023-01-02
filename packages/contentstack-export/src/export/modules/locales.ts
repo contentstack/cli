8a0cc868a9ef4cbaccc0b066014e42913fb6bdd4
Shafeeq PP <shafeeq.pp@Shafeeqs-MacBook-Pro.local>
2023-01-02 16:14:46 +0530

    export highlevel changes

---
+++ packages/contentstack-export/src/export/modules/locales.ts
@@ -0,0 +1,64 @@
+import * as path from 'path';
+import { logger } from '@contentstack/cli-utilities';
+import { fileHelper } from '../../utils';
+export default class LocaleExport {
+  private context: any;
+  private stackAPIClient: any;
+  private exportConfig: any;
+  private qs: any;
+  private localeConfig: any;
+  private localesPath: string;
+
+  constructor({ context, stackAPIClient, exportConfig }) {
+    this.context = context;
+    this.stackAPIClient = stackAPIClient;
+    this.exportConfig = exportConfig;
+    this.localeConfig = exportConfig.moduleLevelConfig.locales;
+    this.qs = {
+      include_count: true,
+      asc: 'updated_at',
+      query: {
+        code: {
+          $nin: [exportConfig.masterLocale],
+        },
+      },
+      only: {
+        BASE: this.localeConfig.requiredKeys,
+      },
+    };
+    this.localesPath = path.resolve(exportConfig.branchDir || exportConfig.exportDir, this.localeConfig.dirName);
+  }
+
+  async start() {
+    try {
+      await fileHelper.makeDirectory(this.localesPath);
+      const locales = await this.getLocales();
+      await fileHelper.writeFile(path.join(this.localesPath, this.localeConfig.fileName), locales);
+      console.log('completed locale export');
+    } catch (error) {
+      logger.error('error in locale export', error);
+    }
+  }
+
+  async getLocales() {
+    let locales = await this.stackAPIClient.locale().query(this.qs).find();
+    if (Array.isArray(locales.items) && locales.items.length > 0) {
+      let updatedLocales = this.sanitizeAttribs(locales.items);
+      return updatedLocales;
+    }
+    logger.info('No locales found');
+  }
+
+  sanitizeAttribs(locales) {
+    let updatedLocales = {};
+    locales.forEach((locale) => {
+      for (let key in locale) {
+        if (this.localeConfig.requiredKeys.indexOf(key) === -1) {
+          delete locale[key];
+        }
+      }
+      updatedLocales[locale.uid] = locale;
+    });
+    return updatedLocales;
+  }
+}
