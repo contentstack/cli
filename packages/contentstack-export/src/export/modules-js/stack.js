017739af20473edab658c0980754f038f0617111
Shafeeq PP <shafeeq.pp@Shafeeqs-MacBook-Pro.local>
2023-01-02 17:32:54 +0530

    fixed bugs

--- packages/contentstack-export/src/export/modules-js/stack.js
+++ packages/contentstack-export/src/export/modules-js/stack.js
@@ -4,66 +4,54 @@
  * MIT Licensed
  */
 
-let path = require('path');
-let mkdirp = require('mkdirp');
-
-let helper = require('../util/helper');
-let { addlogs } = require('../util/log');
-let config = require('../../config/default');
-const stack = require('../util/contentstack-management-sdk');
-
-let client;
+const path = require('path');
+const mkdirp = require('mkdirp');
+const { fileHelper, log } = require('../../utils');
+const { default: config } = require('../../config');
 let stackConfig = config.modules.stack;
-
-function ExportStack() {
+function ExportStack(config, stackAPIClient) {
   this.requestOption = {
     uri: config.host + config.apis.stacks,
     headers: config.headers,
     json: true,
   };
+  this.config = config;
+  this.stackAPIClient = stackAPIClient;
 }
 
-ExportStack.prototype.start = async function (credentialConfig) {
-  config = credentialConfig;
-  client = stack.Client(config);
+ExportStack.prototype.start = async function () {
   const self = this;
 
   // NOTE get org uid
-  if (config.auth_token) {
-    const stack = await client
-      .stack({ api_key: config.source_stack, authtoken: config.auth_token })
-      .fetch()
-      .catch((error) => {
-        console.log(error);
-      });
+  const stack = await this.stackAPIClient.fetch().catch((error) => {
+    console.log(error);
+  });
 
-    if (stack && stack.org_uid) {
-      config.org_uid = stack.org_uid;
-      config.sourceStackName = stack.name;
-    }
+  if (stack && stack.org_uid) {
+    this.config.org_uid = stack.org_uid;
+    this.config.sourceStackName = stack.name;
   }
 
-  if (!config.preserveStackVersion && !config.hasOwnProperty('master_locale')) {
+  if (!this.config.preserveStackVersion && !this.config.hasOwnProperty('master_locale')) {
     const apiDetails = {
       limit: 100,
       skip: 0,
       include_count: true,
     };
     return self.getLocales(apiDetails);
-  } else if (config.preserveStackVersion) {
-    addlogs(config, 'Exporting stack details', 'success');
-    let stackFolderPath = path.resolve(config.data, stackConfig.dirName);
+  } else if (this.config.preserveStackVersion) {
+    log(this.config, 'Exporting stack details', 'success');
+    let stackFolderPath = path.resolve(this.config.data, stackConfig.dirName);
     let stackContentsFile = path.resolve(stackFolderPath, stackConfig.fileName);
 
     mkdirp.sync(stackFolderPath);
 
     return new Promise((resolve, reject) => {
-      return client
-        .stack({ api_key: config.source_stack })
+      return this.stackAPIClient
         .fetch()
         .then((response) => {
-          helper.writeFile(stackContentsFile, response);
-          addlogs(config, 'Exported stack details successfully!', 'success');
+          fileHelper.writeFileSync(stackContentsFile, response);
+          log(this.config, 'Exported stack details successfully!', 'success');
           return resolve(response);
         })
         .catch(reject);
@@ -74,10 +62,7 @@ ExportStack.prototype.start = async function (credentialConfig) {
 ExportStack.prototype.getLocales = function (apiDetails) {
   let self = this;
   return new Promise((resolve, reject) => {
-    const result = client
-      .stack({ api_key: config.source_stack, management_token: config.management_token })
-      .locale()
-      .query(apiDetails);
+    const result = this.stackAPIClient.locale().query(apiDetails);
 
     result
       .find()
--- packages/contentstack-export/src/utils/index.ts
+++ packages/contentstack-export/src/utils/index.ts
@@ -5,3 +5,4 @@ export { default as setupBranches } from './setup-branches';
 export { default as setupExportDir } from './setup-export-dir';
 export { log, unlinkFileLogger } from './logger';
 export { default as login } from './login-handler';
+export * from './common-helper';
