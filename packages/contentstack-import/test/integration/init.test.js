5daa46105a5cb04671abb93273c16be298caf736
Vikram Kalta <vikram.kalta@contentstack.com>
2023-01-04 11:04:11 +0000

    test(cli-import): added initial test setup

---
+++ packages/contentstack-import/test/integration/init.test.js
@@ -0,0 +1,63 @@
+const { join } = require('path');
+const { test } = require('@contentstack/cli-dev-dependencies');
+const { NodeCrypto, messageHandler } = require('@contentstack/cli-utilities');
+
+const { getEnvData, getStacksFromEnv } = require('./utils/helper');
+const LoginCommand = require('@contentstack/cli-auth/lib/commands/auth/login').default;
+const AddTokenCommand = require('@contentstack/cli-auth/lib/commands/auth/tokens/add').default;
+const RegionGetCommand = require('@contentstack/cli-config/lib/commands/config/set/region').default;
+const { DEFAULT_TIMEOUT, PRINT_LOGS, REGION_NAME } = require('./config.json');
+
+const { ENCRYPTION_KEY, NA_USERNAME: USERNAME, NA_PASSWORD: PASSWORD } = getEnvData();
+const { DELIMITER, KEY_VAL_DELIMITER } = process.env;
+
+const stacksFromEnv = getStacksFromEnv();
+const stackDetails = {};
+for (let stack of stacksFromEnv) {
+  stackDetails[stack] = {};
+  process.env[stack].split(DELIMITER).forEach(element => {
+    const [key, value] = element.split(KEY_VAL_DELIMITER);
+    stackDetails[stack][key] = value;
+  });
+}
+
+const crypto = new NodeCrypto({
+  typeIdentifier: '◈',
+  algorithm: 'aes-192-cbc',
+  encryptionKey: ENCRYPTION_KEY || 'gjh67567mn',
+});
+
+const username = ENCRYPTION_KEY ? crypto.decrypt(USERNAME) : USERNAME;
+const password = ENCRYPTION_KEY ? crypto.decrypt(PASSWORD) : PASSWORD;
+
+describe('Setting Pre-requests', () => {
+  let messageFilePath = join(__dirname, '..', '..', '..', 'contentstack-config', 'messages/index.json');
+  messageHandler.init({ messageFilePath });
+  test
+    .timeout(DEFAULT_TIMEOUT || 600000) // NOTE setting default timeout as 10 minutes
+    .stdout({ print: PRINT_LOGS || false })
+    .command(RegionGetCommand, [`${REGION_NAME || 'NA'}`])
+    .do(() => {
+      messageFilePath = join(__dirname, '..', '..', '..', 'contentstack-utilities', 'messages/auth.json');
+      messageHandler.init({ messageFilePath });
+    })
+    .command(LoginCommand, [`-u=${username}`, `-p=${password}`])
+    .do(() => {
+      messageFilePath = join(__dirname, '..', '..', '..', 'contentstack-utilities', 'messages/auth.json');
+      messageHandler.init({ messageFilePath });
+    })
+    .it('Pre-config is done', () => {
+      messageFilePath = '';
+      messageHandler.init({ messageFilePath });
+    });
+
+  for (const stack of stacksFromEnv) {
+    test
+      .timeout(DEFAULT_TIMEOUT || 600000)
+      .command(AddTokenCommand, ['-a', stackDetails[stack].ALIAS_NAME, '-k', stackDetails[stack].STACK_API_KEY, '--management', '--token', stackDetails[stack].MANAGEMENT_TOKEN])
+      .it(`Adding token for ${stack}`, (_, done) => {
+        console.log('done');
+        done();
+      });
+  }
+});
