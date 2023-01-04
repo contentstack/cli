59547687a937e032a9bff1ac606b7a5462c07ca9
Antony <antonyagustineraj@gmail.com>
2023-01-04 13:40:15 +0530

    Assets TS rewrite code for export functionality has been added.

--- packages/contentstack-export-new/src/fs-utility/index.ts
+++
@@ -1,5 +0,0 @@
-import FsUtility from './core';
-
-export { FsUtility };
-export * from './types';
-export { mapKeyAndVal } from './helper';
--- packages/contentstack-export-new/src/utility/index.ts
+++
@@ -1,1 +0,0 @@
-export { ValidateFlags, SetConfigData } from "./decorators";
