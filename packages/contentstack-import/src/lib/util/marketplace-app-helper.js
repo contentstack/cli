084dec371816e231173b0604d8685b6606706b92
Netraj Patel <netraj.patel@contentstack.com>
2023-01-06 18:13:03 +0530

    Used commaon utility for SDK connection object

--- packages/contentstack-import/src/lib/util/marketplace-app-helper.js
+++ packages/contentstack-import/src/lib/util/marketplace-app-helper.js
@@ -1,37 +1,39 @@
 let config = require('../../config/default');
-const sdk = require('./contentstack-management-sdk');
-const { cliux, HttpClient, configHandler } = require('@contentstack/cli-utilities');
+const { cliux, HttpClient, configHandler, managementSDKClient } = require('@contentstack/cli-utilities');
 
 const getInstalledExtensions = (config) => {
-  const client = sdk.Client(config);
-
   return new Promise((resolve, reject) => {
-    const queryRequestOptions = {
-      include_marketplace_extensions: true,
-    };
-    const { target_stack: api_key, management_token, auth_token } = config || {};
+    managementSDKClient(config)
+      .then((APIClient) => {
+        const queryRequestOptions = {
+          include_marketplace_extensions: true,
+        };
+        const { target_stack: api_key, management_token, auth_token } = config || {};
 
-    if (api_key && management_token) {
-      return client
-        .stack({ api_key, management_token })
-        .extension()
-        .query(queryRequestOptions)
-        .find()
-        .then(({ items }) => resolve(items))
-        .catch(reject);
-    } else if (api_key && auth_token) {
-      const { cma } = configHandler.get('region') || {};
-      const headers = {
-        api_key,
-        authtoken: auth_token,
-      };
-      const httpClient = new HttpClient().headers(headers);
-      httpClient
-        .get(`${cma}/v3/extensions/?include_marketplace_extensions=true`)
-        .then(({ data: { extensions } }) => resolve(extensions));
-    } else {
-      resolve([]);
-    }
+        if (api_key && management_token) {
+          const stackAPIClient = APIClient.stack({ api_key, management_token });
+          return stackAPIClient
+            .extension()
+            .query(queryRequestOptions)
+            .find()
+            .then(({ items }) => resolve(items))
+            .catch(reject);
+        } else if (api_key && auth_token) {
+          const { cma } = configHandler.get('region') || {};
+          const headers = {
+            api_key,
+            authtoken: auth_token,
+          };
+          const httpClient = new HttpClient().headers(headers);
+          httpClient
+            .get(`${cma}/v3/extensions/?include_marketplace_extensions=true`)
+            .then(({ data: { extensions } }) => resolve(extensions))
+            .catch(reject);
+        } else {
+          resolve([]);
+        }
+      })
+      .catch(reject);
   });
 };
 
