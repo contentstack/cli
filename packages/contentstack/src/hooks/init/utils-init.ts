11bbf8a269455be5f62717dba0a63ddeb585c84f
Shafeeq PP <shafeeq.pp@Shafeeqs-MacBook-Pro.local>
2023-02-28 11:07:54 +0530

    latest CLI changes

--- packages/contentstack/src/hooks/init/utils-init.ts
+++ packages/contentstack/src/hooks/init/utils-init.ts
@@ -1,10 +1,12 @@
-import { logger, messageHandler, cliux } from '@contentstack/cli-utilities';
+import { logger, messageHandler, cliux, managementSDKInitiator } from '@contentstack/cli-utilities';
 
 /**
  * Initialize the utilities
  */
 export default function (_opts): void {
-  logger.init(this.config.context);
-  messageHandler.init(this.config.context);
-  cliux.init(this.config.context);
+  const { context } = this.config;
+  logger.init(context);
+  messageHandler.init(context);
+  cliux.init(context);
+  managementSDKInitiator.init(context);
 }
