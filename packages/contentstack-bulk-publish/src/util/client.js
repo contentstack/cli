cda4b1741892e5553c290d1a6cfdf1858f4ebd8c
Netraj Patel <netraj.patel@contentstack.com>
2023-03-24 22:16:37 +0530

    Removed formatting of hostname in bulk publish and migrate-rte

--- packages/contentstack-bulk-publish/src/util/client.js
+++ packages/contentstack-bulk-publish/src/util/client.js
@@ -2,12 +2,10 @@ const { managementSDKClient } = require('@contentstack/cli-utilities');
 const { Command } = require('@contentstack/cli-command');
 const command = new Command();
 
-const { formatHostname } = require('../util');
-
 async function getStack(data) {
   const tokenDetails = command.getToken(data.alias);
-  const managementClient = await managementSDKClient({ host: formatHostname(data.host), branchName: data.branch })
-  const stack = managementClient.stack({ api_key: tokenDetails.apiKey, management_token: tokenDetails.token })
+  const managementClient = await managementSDKClient({ host: data.host, branchName: data.branch });
+  const stack = managementClient.stack({ api_key: tokenDetails.apiKey, management_token: tokenDetails.token });
   stack.alias = data.alias;
   stack.host = data.host;
   return stack;
