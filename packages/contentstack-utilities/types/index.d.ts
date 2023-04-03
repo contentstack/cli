ccaee850a56fb535551b45d4338e75c05cd2dbdd
Antony <antonyagustineraj@gmail.com>
2023-04-03 15:58:14 +0530

    Utility type fixes

--- packages/contentstack-utilities/types/index.d.ts
+++ packages/contentstack-utilities/types/index.d.ts
@@ -1,9 +1,6 @@
-export { default as logger } from './logger';
-export { default as cliux } from './cli-ux';
-export { default as CLIError } from './cli-error';
 export { default as messageHandler } from './message-handler';
 export { default as configHandler } from './config-handler';
-export { default as managementSDKClient } from './contentstack-management-sdk';
+export { default as managementSDKClient, ContentstackClient, ContentstackConfig } from './contentstack-management-sdk';
 export { default as printFlagDeprecation } from './flag-deprecation-check';
 export * from './http-client';
 export { default as NodeCrypto } from './encrypter';
