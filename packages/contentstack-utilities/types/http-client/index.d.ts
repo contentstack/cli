92bf818c432be6553c72de14a0cb46b22296f562
Antony <antonyagustineraj@gmail.com>
2023-04-03 15:58:14 +0530

    Utility type fixes

--- packages/contentstack-utilities/types/http-client/index.d.ts
+++ packages/contentstack-utilities/types/http-client/index.d.ts
@@ -2,5 +2,3 @@ import { HttpClient } from './client';
 export default HttpClient;
 export * from './client';
 export * from './http-response';
-export * from './http-client-decorator';
-export * from './oauth-decorator';
