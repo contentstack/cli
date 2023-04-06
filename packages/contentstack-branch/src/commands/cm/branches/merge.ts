a842c35993f746acca6a27c2a7f99688324d984e
Shafeeq PP <shafeeq.pp@Shafeeqs-MacBook-Pro.local>
2023-04-06 11:55:52 +0530

    merge base

--- packages/contentstack-branch/src/commands/cm/branches/merge.ts
+++ packages/contentstack-branch/src/commands/cm/branches/merge.ts
@@ -2,25 +2,35 @@ import { Command } from '@contentstack/cli-command';
 import { cliux, messageHandler, printFlagDeprecation, managementSDKClient, flags } from '@contentstack/cli-utilities';
 
 export default class BranchMergeCommand extends Command {
-  static description: string = messageHandler.parse('Merge a branch'); // Note: Update the description
+  static description: string = messageHandler.parse('Merge changes from a branch'); // Note: Update the description
 
-  static examples: string[] = ['csdx cm:branches:merge --base-branch main --stack-api-key bltxxxxxxxx']; // Note: Add and modify the examples
+  static examples: string[] = [
+    'csdx cm:branches:merge --compare-branch feature-branch --module=content-types',
+    'csdx cm:branches:merge --compare-branch feature-branch --module=global-fields',
+    'csdx cm:branches:merge --compare-branch feature-branch',
+  ];
 
-  static usage: string = 'cm:branches:merge [--base-branch <value>] [--stack-api-key <value>]'; // Note: Add and modify the usage
+  static usage: string = 'cm:branches:merge [--compare-branch <value>] [--module <value>]'; // Note: Add and modify the usage
 
-  static flags = {};
+  static flags = {
+    module: flags.string({
+      description: '[optional] specific module name',
+    }),
+    'compare-branch': flags.string({
+      description: 'Compare branch name',
+      required: true,
+    }),
+  };
 
   static aliases: string[] = []; // Note: alternative usage if any
 
   async run(): Promise<any> {
     try {
-      const managementAPIClient = await managementSDKClient({ host: this.cmaHost });
-      const { flags: branchMergeFlags } = await this.parse(BranchMergeCommand);
-      await cliux.inquire({ type: 'input', message: 'ENTER_API_KEY', name: 'stack-api-key' });
-      cliux.print('Running merge command');
-      /**
-       * Implementation
-       */
+      // get config
+      //  Display summary - displaySummary
+      //  Collect the mergeSettings
+      //  Show Merge summary
+      //  Execute/export summary ( request payload )
     } catch (error) {}
   }
 }
