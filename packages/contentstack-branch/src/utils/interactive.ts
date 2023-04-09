bc2e1058b60dc96b570c213f7d21671f8256eb18
Aman Kumar <aman.kumar@contentstack.com>
2023-04-10 02:49:06 +0530

    fix diff unit testcases

--- packages/contentstack-branch/src/utils/interactive.ts
+++ packages/contentstack-branch/src/utils/interactive.ts
@@ -2,68 +2,61 @@ import isEmpty from 'lodash/isEmpty';
 import { cliux, messageHandler } from '@contentstack/cli-utilities';
 
 export async function selectModule(): Promise<string> {
-  return cliux.inquire<string>({
+  return await cliux.inquire({
     type: 'list',
     name: 'module',
-    message: 'CLI_SELECT_TOKEN_TYPE',
+    message: 'CLI_BRANCH_MODULE',
     choices: [
       { name: 'Content Types', value: 'content_types'},
       { name: 'Global Fields', value: 'global_fields'},
       { name: 'Both', value: 'both'},
-    ]
+    ],
+    validate: inquireRequireFieldValidation
   });
 }
 
 export async function askCompareBranch(): Promise<string> {
-  const resp = await cliux.inquire<string>({
+  return await cliux.inquire<string>({
     type: 'input',
     message: 'CLI_BRANCH_COMPARE_BRANCH',
     name: 'compare_branch',
+    validate: inquireRequireFieldValidation
   });
-  inquireRequireFieldValidation(resp);
-  return resp;
 }
 
 export async function askStackAPIKey(): Promise<string> {
-  const resp = await cliux.inquire<string>({
+  return await cliux.inquire<string>({
     type: 'input',
     message: 'CLI_BRANCH_STACK_API_KEY',
     name: 'api_key',
+    validate: inquireRequireFieldValidation
   });
-  inquireRequireFieldValidation(resp);
-  return resp;
 }
 
 export async function askBaseBranch(): Promise<string> {
-  const resp = await cliux.inquire<string>({
+  return await cliux.inquire<string>({
     type: 'input',
     message: 'CLI_BRANCH_BASE_BRANCH',
     name: 'branch_branch',
+    validate: inquireRequireFieldValidation
   });
-  inquireRequireFieldValidation(resp);
-  return resp;
 }
-
 export async function askSourceBranch(): Promise<string> {
-  const resp = await cliux.inquire<string>({
+ return await cliux.inquire<string>({
     type: 'input',
     message: 'CLI_BRANCH_SOURCE_BRANCH',
     name: 'source_branch',
+    validate: inquireRequireFieldValidation
   });
-  inquireRequireFieldValidation(resp);
-  return resp;
 }
-
 export async function askBranchUid(): Promise<string> {
-  const resp = await cliux.inquire<string>({
+  return await cliux.inquire<string>({
     type: 'input',
     message: 'CLI_BRANCH_BRANCH_UID',
     name: 'branch_uid',
+    validate: inquireRequireFieldValidation
   });
-  inquireRequireFieldValidation(resp);
-  return resp;
 }
-
 export async function askConfirmation(): Promise<boolean> {
   const resp = await cliux.inquire<boolean>({
     type: 'confirm',
@@ -75,8 +68,7 @@ export async function askConfirmation(): Promise<boolean> {
 
 export function inquireRequireFieldValidation(input: any): string | boolean {
   if (isEmpty(input)) {
-    cliux.error(messageHandler.parse('CLI_BRANCH_REQUIRED_FIELD'));
-    process.exit(1);
+    return messageHandler.parse('CLI_BRANCH_REQUIRED_FIELD');
   }
   return true;
 }
