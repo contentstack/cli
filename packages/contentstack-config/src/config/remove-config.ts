0c54df29ed4e88e94145ee0073e2d73685f0f405
Akash Bhosale <akash.bhosale@contentstack.com>
2023-04-10 15:34:55 +0530

    changed the remove config command to handle multiple cases

--- packages/contentstack-config/src/config/remove-config.ts
+++
@@ -1,8 +0,0 @@
-import { cliux, configHandler } from '@contentstack/cli-utilities';
-
-export async function removeConfig(apiKey) {
-  await configHandler.set(`baseBranch.${apiKey}`, 'main');
-  cliux.success(`base branch : ${configHandler.get(`baseBranch.${apiKey}`)}`);
-  cliux.success(`stack-api-key: ${apiKey}`);
-  cliux.success(`Base branch configuration for stack-api-key: ${apiKey} has been removed successfully`);
-}
