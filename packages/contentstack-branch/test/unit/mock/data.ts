788b98449571ad207061a7d3f7d28b5cc6260693
Akash Bhosale <akash.bhosale@contentstack.com>
2023-04-10 16:18:50 +0530

    test cases for delete and create added

--- packages/contentstack-branch/test/unit/mock/data.ts
+++ packages/contentstack-branch/test/unit/mock/data.ts
@@ -1,3 +1,17 @@
 const mockData = {};
-
-export { mockData };
+const createBranchMockData = {
+  flags: {
+    source: 'main',
+    uid: 'new_branch',
+    apiKey: 'abcd',
+  },
+};
+const deleteBranchMockData = {
+  flags: {
+    uid: 'new_branch',
+    apiKey: 'abcd',
+    force: false,
+    confirm: false,
+  },
+};
+export { mockData, createBranchMockData, deleteBranchMockData };
