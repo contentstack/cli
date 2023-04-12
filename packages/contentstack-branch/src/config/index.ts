bedef64603ac5b58fb67d6c56d4e1102916cd990
Shafeeq PP <shafeeq.pp@Shafeeqs-MacBook-Pro.local>
2023-04-12 17:07:49 +0530

    merge summary completed

--- packages/contentstack-branch/src/config/index.ts
+++ packages/contentstack-branch/src/config/index.ts
@@ -1,5 +1,7 @@
 const config = {
-  baseUrl: "http://dev16-branches.csnonprod.com/api/compare"
+  baseUrl: 'http://dev16-branches.csnonprod.com/api/compare',
+  mergeUrl: 'https://dev16-branches.csnonprod.com/api/merge',
+  mergeQueueUrl: 'https://dev16-branches.csnonprod.com/api/merge-queue',
 };
 
-export default config
+export default config;
