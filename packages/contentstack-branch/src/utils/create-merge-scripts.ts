f0be414df1e669c9795e034fc4f61c5b029f3ffb
Akash Bhosale <akash.bhosale@contentstack.com>
2023-04-19 13:38:02 +0530

    changed config and merge script utility

--- packages/contentstack-branch/src/utils/create-merge-scripts.ts
+++ packages/contentstack-branch/src/utils/create-merge-scripts.ts
@@ -13,19 +13,17 @@ type CreateMergeScriptsProps = {
 export function generateMergeScripts(mergeSummary) {
   try {
     let { added, modified } = mergeSummary.content_type;
-    console.log('mergeSummary', mergeSummary, mergeSummary.content_type);
+    console.log('mergeSummary', added, modified);
 
-    modified.length &&
+    modified.length !== 0 &&
       modified.map((contentType) => {
         console.log(contentType);
-
         createMergeScripts(contentType);
       });
 
-    added.length &&
+    added.length !== 0 &&
       added.map((contentType) => {
         console.log(contentType);
-
         createMergeScripts(contentType);
       });
   } catch (error) {
@@ -41,19 +39,17 @@ export function createMergeScripts({ status, uid }: CreateMergeScriptsProps) {
   const fileCreatedAt = `${date.getFullYear()}${
     date.getMonth().toString.length === 1 ? `0${date.getMonth() + 1}` : date.getMonth() + 1
   }${date.getUTCDate()}${date.getHours()}${date.getMinutes()}${date.getSeconds()}`;
-  // const mergeScriptsSlug = `merge_scripts_${mergeJobID}_${fileCreatedAt}`;
   const mergeScriptsSlug = `merge_scripts_${mergeJobID}_${fileCreatedAt}`;
 
   const fullPath = `${rootFolder}/${mergeScriptsSlug}`;
   try {
-    if (!fs.existsSync(`${rootFolder}`)) {
-      fs.mkdirSync(`${rootFolder}`);
-      fs.mkdirSync(`${fullPath}`);
-      fs.writeFileSync(`${fullPath}/${fileCreatedAt}_${status}_${uid}.js`, content, 'utf-8');
-    } else {
-      fs.mkdirSync(`${fullPath}`);
-      fs.writeFileSync(`${fullPath}/${fileCreatedAt}_${status}_${uid}.js`, content, 'utf-8');
+    if (!fs.existsSync(rootFolder)) {
+      fs.mkdirSync(rootFolder);
     }
+    if (!fs.existsSync(fullPath)) {
+      fs.mkdirSync(fullPath);
+    }
+    fs.writeFileSync(`${fullPath}/${fileCreatedAt}_${status}_${uid}.js`, content, 'utf-8');
   } catch (error) {
     console.log(error);
   }
