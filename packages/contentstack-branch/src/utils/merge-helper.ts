2af092097ad76a8959f9781ee5e5d1e75931f649
Aman Kumar <aman.kumar@contentstack.com>
2023-04-19 16:10:28 +0530

    merge command, helper and handler testcases

--- packages/contentstack-branch/src/utils/merge-helper.ts
+++ packages/contentstack-branch/src/utils/merge-helper.ts
@@ -115,7 +115,7 @@ export const executeMerge = async (apiKey, mergePayload): Promise<any> => {
   }
 };
 
-const fetchMergeStatus = async (mergePayload): Promise<any> => {
+export const fetchMergeStatus = async (mergePayload): Promise<any> => {
   return new Promise(async (resolve, reject) => {
     const mergeStatusResponse = await apiGetRequest({
       apiKey: mergePayload.apiKey,
@@ -133,7 +133,7 @@ const fetchMergeStatus = async (mergePayload): Promise<any> => {
           await fetchMergeStatus(mergePayload).then(resolve).catch(reject);
         }, 5000);
       } else if (mergeStatus === 'failed') {
-        console.log('errors', mergeRequestStatusResponse.errors);
+        //console.log('errors', mergeRequestStatusResponse.errors);
         return reject(`merge uid: ${mergePayload.uid}`);
       } else {
         return reject(`Invalid merge status found with merge id ${mergePayload.uid}`);
