6f3e2da8e7abcf911cbe4dfdde13f91949396b44
Akash Bhosale <akash.bhosale@contentstack.com>
2023-04-21 10:51:06 +0530

    entry create script

---
+++ packages/contentstack-branch/src/utils/entry-create-script.ts
@@ -0,0 +1,54 @@
+import { configHandler, managementSDKClient } from '@contentstack/cli-utilities';
+import { omit } from 'lodash';
+
+export async function createEntries() {
+  const managementAPIClient = await managementSDKClient({ host: 'dev16-api.csnonprod.com' });
+  const apiKey = 'bltafe5d06b229df996';
+
+  const baseBranchClient = managementAPIClient.stack({ api_key: apiKey, branch_uid: 'main' });
+
+  const compareEntries = await managementAPIClient
+    .stack({ api_key: apiKey, branch_uid: 'akash_base' })
+    .contentType('banner')
+    .entry()
+    .query()
+    .find();
+
+  const compareFilteredProperties = compareEntries.items.map((entry) =>
+    omit(entry, [
+      'stackHeaders',
+      'single_line',
+      'tags',
+      'created_by',
+      'updated_by',
+      'created_at',
+      'updated_at',
+      'ACL',
+      '_version',
+      'update',
+      'delete',
+      'fetch',
+      'publish',
+      'unpublish',
+      'publishRequest',
+      'setWorkflowStage',
+      'import',
+      '_in_progress',
+      'locale',
+    ]),
+  );
+
+  compareFilteredProperties.forEach((entry) =>
+    baseBranchClient
+      .contentType(entry.content_type_uid)
+      .entry()
+      .create({
+        entry: {
+          title: entry.title,
+          url: entry.urlPath,
+        },
+      })
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err.errors)),
+  );
+}
