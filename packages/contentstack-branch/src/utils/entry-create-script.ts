cacef7f25487c86df4167e3963668ebfe0ba1dcf
Akash Bhosale <akash.bhosale@contentstack.com>
2023-04-25 18:03:51 +0530

    added validation for keys in create and update commands

--- packages/contentstack-branch/src/utils/entry-create-script.ts
+++ packages/contentstack-branch/src/utils/entry-create-script.ts
@@ -1,14 +1,18 @@
 export function entryCreateScript(contentType) {
-  return `module.exports = async ({ migration, stackSDKInstance, managementAPIClient, config }) => {
+  return `
+module.exports = async ({ migration, stackSDKInstance, managementAPIClient, config, branch, apiKey }) => {
   const keysToRemove = [
-    'stackHeaders',
-    'tags',
-    'created_by',
-    'updated_by',
+    'content_type_uid',
+    'uid',
     'created_at',
     'updated_at',
+    'created_by',
+    'updated_by',
     'ACL',
+    'stackHeaders',
+    'urlPath',
     '_version',
+    '_in_progress',
     'update',
     'delete',
     'fetch',
@@ -17,41 +21,30 @@ export function entryCreateScript(contentType) {
     'publishRequest',
     'setWorkflowStage',
     'import',
-    '_in_progress',
-    'locale',
   ];
 
-  let comparebranch = '';
+  let compareBranch = config['compare-branch'];
 
-  if (config.compare_branch) {
-    comparebranch = config.compare_branch;
-  }
-
-  const compareBranchEntries = await managementAPIClient
-    .stack({ api_key: stackSDKInstance.api_key, branch_uid: comparebranch }) //
-    .contentType('${contentType}')
-    .entry()
-    .query()
-    .find();
-
-  const compareFilteredProperties = compareBranchEntries.items.map((entry) => {
-    keysToRemove.map((key) => delete entry[key]);
-    return entry;
-  });
-
-  const createEntryTask = (params) => {
+  const createEntryTask = () => {
     return {
       title: 'Create Entries',
       successTitle: 'Entries Created Successfully',
       failedTitle: 'Failed to create entries',
-      task: async (params) => {
+      task: async () => {
+        const compareBranchEntries = await managementAPIClient
+          .stack({ api_key: stackSDKInstance.api_key, branch_uid: compareBranch }) //
+          .contentType('${contentType}')
+          .entry()
+          .query()
+          .find();
+        const compareFilteredProperties = compareBranchEntries.items.map((entry) => {
+          keysToRemove.map((key) => delete entry[key]);
+          return entry;
+        });
         try {
           compareFilteredProperties.length !== 0 &&
             compareFilteredProperties.forEach(async (entryDetails) => {
-              await stackSDKInstance
-                .contentType('${contentType}')
-                .entry()
-                .create({ entry: entryDetails });
+              await stackSDKInstance.contentType('${contentType}').entry().create({ entry: entryDetails });
             });
         } catch (error) {
           throw error;
@@ -59,6 +52,19 @@ export function entryCreateScript(contentType) {
       },
     };
   };
-  migration.addTask(createEntryTask());
-};`;
+  if (compareBranch && branch.length !== 0 && apiKey.length !== 0) {
+    migration.addTask(createEntryTask());
+  } else {
+    if (apiKey.length === 0) {
+      console.error('Please provide api key using --stack-api-key flag');
+    }
+    if (!compareBranch) {
+      console.error('Please provide compare branch through --config compare-branch:<value> flag');
+    }
+    if (branch.length === 0) {
+      console.error('Please provide branch name through --branch flag');
+    }
+  }
+};
+`;
 }
