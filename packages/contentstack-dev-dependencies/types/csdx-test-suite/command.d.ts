0329829b4042f04e02af073efbad7e689a12d664
Antony <antonyagustineraj@gmail.com>
2023-05-12 16:38:49 +0530

    TS config updated for TS packages

--- packages/contentstack-dev-dependencies/types/csdx-test-suite/command.d.ts
+++
@@ -1,9 +0,0 @@
-import { Command } from '@oclif/core/lib/interfaces';
-import { Interfaces } from '@oclif/core';
-import { loadConfig } from './load-config';
-export declare function command(commandInstance: Command.Class, args: string[] | string, opts?: loadConfig.Options): {
-    run(ctx: {
-        config: Interfaces.Config;
-        expectation: string;
-    }): Promise<void>;
-};
