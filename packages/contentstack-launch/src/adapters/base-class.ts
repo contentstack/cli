354e8d27912c8a7404c207b9b36492bc92a4bf5e
Antony <antonyagustineraj@gmail.com>
2023-05-18 20:03:29 +0530

    Code clean, Org pagination added, server log error handling code added

--- packages/contentstack-launch/src/adapters/base-class.ts
+++ packages/contentstack-launch/src/adapters/base-class.ts
@@ -19,7 +19,7 @@ import { writeFileSync, existsSync, readFileSync } from 'fs';
 import { cliux as ux, ContentstackClient } from '@contentstack/cli-utilities';
 
 import config from '../config';
-import { print, GraphqlApiClient, LogPolling } from '../util';
+import { print, GraphqlApiClient, LogPolling, getOrganizations } from '../util';
 import {
   branchesQuery,
   frameworkQuery,
@@ -49,7 +49,7 @@ export default class BaseClass {
   public projectCreationRetryCount: number = 0;
   public apolloLogsClient: ApolloClient<any> | undefined;
   public envVariables: Array<Record<string, any>> = [];
-  public managementSdk: ContentstackClient | undefined;
+  public managementSdk: ContentstackClient;
 
   constructor(options: AdapterConstructorInputs) {
     const { log, exit, config, $event, apolloClient, managementSdk, analyticsInfo, apolloLogsClient } = options;
@@ -117,16 +117,7 @@ export default class BaseClass {
    * @memberof BaseClass
    */
   async selectOrg(): Promise<void> {
-    const organizations =
-      (await this.managementSdk
-        ?.organization()
-        .fetchAll()
-        .then(({ items }) => map(items, ({ uid, name }) => ({ name, value: name, uid })))
-        .catch((error) => {
-          this.log('Unable to fetch organizations.', 'warn');
-          this.log(error, 'error');
-          this.exit(1);
-        })) || [];
+    const organizations = (await getOrganizations({ log: this.log, managementSdk: this.managementSdk })) || [];
 
     if (this.config.flags.org && find(organizations, { uid: this.config.flags.org })) {
       this.config.currentConfig.organizationUid = this.config.flags.org;
