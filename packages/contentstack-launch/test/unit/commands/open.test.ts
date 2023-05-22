ebbdc305a6409330267aaab2c131b95c96c80caf
Akash Bhosale <akash.bhosale@contentstack.com>
2023-05-22 13:09:47 +0530

    added more test cases in open command

--- packages/contentstack-launch/test/unit/commands/open.test.ts
+++ packages/contentstack-launch/test/unit/commands/open.test.ts
@@ -1,10 +1,10 @@
 import { describe, it } from 'mocha';
 import { expect } from 'chai';
-import { stub, spy } from 'sinon';
+import { stub } from 'sinon';
 import Open from '../../../src/commands/launch/open';
 import { cliux } from '@contentstack/cli-utilities';
 import { deploymentFlags } from '../mock';
-import open from 'open';
+
 describe('Open Command', () => {
   it('should open the deployment URL in browser if all flags are passed', async function () {
     const args = [
@@ -31,10 +31,38 @@ describe('Open Command', () => {
     inquireStub.restore();
   });
   it('Should ask for org flag when org flag is not passed', async function () {
-    const args = ['--project', deploymentFlags.project];
-    const inquireStub = stub(cliux, 'inquire').resolves(deploymentFlags.org.uid);
+    const args = ['--project', deploymentFlags.project, '-e', deploymentFlags.environment];
     await Open.run(args);
-    expect(inquireStub.calledOnce).to.be.true;
+    const inquireStub = stub(cliux, 'inquire').resolves(deploymentFlags.org.uid);
+    expect(inquireStub.calledOnce);
     inquireStub.restore();
+  });
+  it('Should ask for organization with a warning when passed incorrect org uid', async function () {
+    const args = [
+      '--org',
+      deploymentFlags.invalidOrg.uid,
+      '--project',
+      deploymentFlags.project,
+      '-e',
+      deploymentFlags.environment,
+    ];
+    await Open.run(args);
+    const orgStub = stub(cliux, 'inquire').resolves(deploymentFlags.invalidOrg.uid);
+    expect(orgStub.calledOnce);
+    orgStub.restore();
+  });
+  it('Should ask for project when passed incorrect project name', async function () {
+    const args = [
+      '--org',
+      deploymentFlags.org.uid,
+      '--project',
+      deploymentFlags.invalidProj,
+      '-e',
+      deploymentFlags.environment,
+    ];
+    await Open.run(args);
+    const projectStub = stub(cliux, 'inquire').resolves(deploymentFlags.invalidProj);
+    expect(projectStub.calledOnce);
+    projectStub.restore();
   });
 });
