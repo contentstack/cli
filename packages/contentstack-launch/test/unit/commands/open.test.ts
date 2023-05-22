6aaa445cdfe94f6676fc7e9ea5ce4b12d0a2bd10
Akash Bhosale <akash.bhosale@contentstack.com>
2023-05-22 12:29:36 +0530

    open command test cases

--- packages/contentstack-launch/test/unit/commands/open.test.ts
+++ packages/contentstack-launch/test/unit/commands/open.test.ts
@@ -6,23 +6,23 @@ import { cliux } from '@contentstack/cli-utilities';
 import { deploymentFlags } from '../mock';
 import open from 'open';
 describe('Open Command', () => {
-  // it('should open the deployment URL in browser if all flags are passed', async function () {
-  //   const args = [
-  //     '--org',
-  //     deploymentFlags.org.uid,
-  //     '--project',
-  //     deploymentFlags.project,
-  //     '-e',
-  //     deploymentFlags.environment,
-  //   ];
-  //   const inquireStub = stub(cliux, 'inquire');
-  //   const printStub = stub(cliux, 'print');
-  //   await Open.run(args);
-  //   expect(inquireStub.calledOnce).to.be.false;
-  //   expect(printStub.calledOnce);
-  //   inquireStub.restore();
-  //   printStub.restore();
-  // });
+  it('should open the deployment URL in browser if all flags are passed', async function () {
+    const args = [
+      '--org',
+      deploymentFlags.org.uid,
+      '--project',
+      deploymentFlags.project,
+      '-e',
+      deploymentFlags.environment,
+    ];
+    const inquireStub = stub(cliux, 'inquire');
+    const printStub = stub(cliux, 'print');
+    await Open.run(args);
+    expect(inquireStub.calledOnce).to.be.false;
+    expect(printStub.calledOnce);
+    inquireStub.restore();
+    printStub.restore();
+  });
   it('Should ask for project flag when project flag is not passed', async function () {
     const args = ['--org', deploymentFlags.org.uid, '-e', deploymentFlags.environment];
     const inquireStub = stub(cliux, 'inquire').resolves(deploymentFlags.project);
