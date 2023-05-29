a40d01b6a65f929cc98eed86fecca4c356beab74
Akash Bhosale <akash.bhosale@contentstack.com>
2023-05-29 16:44:47 +0530

    changed test cases to work with env variables

--- packages/contentstack-launch/test/unit/commands/environments.test.ts
+++ packages/contentstack-launch/test/unit/commands/environments.test.ts
@@ -3,12 +3,15 @@ import { expect } from 'chai';
 import { stub } from 'sinon';
 import Environments from '../../../src/commands/launch/environments';
 import { cliux } from '@contentstack/cli-utilities';
-import { deploymentFlags } from '../mock';
+import { testFlags } from '../mock';
 import sinon from 'sinon';
+import { config } from 'dotenv';
+
+config();
 
 describe('Environments', () => {
   it('Should run the command when all the flags are passed', async function () {
-    const args = ['--org', deploymentFlags.org.uid, '--project', deploymentFlags.project];
+    const args = ['--org', process.env.ORG!, '--project', process.env.PROJECT!];
     const inquireStub = stub(cliux, 'inquire');
     const tableStub = stub(cliux, 'table');
     await Environments.run(args);
@@ -18,20 +21,20 @@ describe('Environments', () => {
     tableStub.restore();
   });
   it('Should ask for project flag when project flag is not passed', async function () {
-    const args = ['--org', deploymentFlags.org.uid];
+    const args = ['--org', process.env.ORG!];
     const tableStub = stub(cliux, 'table');
-    const inquireStub = stub(cliux, 'inquire').resolves(deploymentFlags.project);
+    const inquireStub = stub(cliux, 'inquire').resolves(process.env.PROJECT!);
     await Environments.run(args);
     sinon.assert.calledOnce(inquireStub);
     inquireStub.restore();
     tableStub.restore();
   });
   it('Should ask for org flag when org flag is not passed', async function () {
-    const args = ['--project', deploymentFlags.project];
+    const args = ['--project', process.env.PROJECT!];
     const mock = sinon.mock(Environments);
     const expectation = mock.expects('run');
     expectation.exactly(1);
-    const inquireStub = stub(cliux, 'inquire').resolves(deploymentFlags.org.uid);
+    const inquireStub = stub(cliux, 'inquire').resolves(process.env.ORG!);
     await Environments.run(args);
     sinon.assert.notCalled(inquireStub);
     inquireStub.restore();
@@ -39,11 +42,11 @@ describe('Environments', () => {
     mock.restore();
   });
   it('Should ask for organization with a warning when passed incorrect org uid', async function () {
-    const args = ['--org', deploymentFlags.invalidOrg.uid, '--project', deploymentFlags.project];
+    const args = ['--org', testFlags.invalidOrg.uid, '--project', process.env.PROJECT!];
     const mock = sinon.mock(Environments);
     const expectation = mock.expects('run');
     expectation.exactly(1);
-    const orgStub = stub(cliux, 'inquire').resolves(deploymentFlags.invalidOrg.uid);
+    const orgStub = stub(cliux, 'inquire').resolves(testFlags.invalidOrg.uid);
     await Environments.run(args);
     sinon.assert.notCalled(orgStub);
     orgStub.restore();
@@ -51,11 +54,11 @@ describe('Environments', () => {
     mock.restore();
   });
   it('Should ask for project when passed incorrect project name', async function () {
-    const args = ['--org', deploymentFlags.org.uid, '--project', deploymentFlags.invalidProj];
+    const args = ['--org', process.env.ORG!, '--project', testFlags.invalidProj];
     const mock = sinon.mock(Environments);
     const expectation = mock.expects('run');
     expectation.exactly(1);
-    const projectStub = stub(cliux, 'inquire').resolves(deploymentFlags.invalidProj);
+    const projectStub = stub(cliux, 'inquire').resolves(testFlags.invalidProj);
     await Environments.run(args);
     sinon.assert.notCalled(projectStub);
     projectStub.restore();
