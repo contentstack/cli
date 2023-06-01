356a93b688d69d11eacca81e3925fd21f6909f4a
Aman Kumar <aman.kumar@contentstack.com>
2023-06-01 13:57:09 +0530

    fix: identical image in all assets subfolder

--- packages/contentstack-export/src/lib/export/assets.js
+++ packages/contentstack-export/src/lib/export/assets.js
@@ -354,7 +354,7 @@ module.exports = class ExportAssets {
           : asset.url,
       };
 
-      await helper.makeDirectory(assetFolderPath);
+      mkdirp.sync(assetFolderPath);
       const assetFileStream = fs.createWriteStream(assetFilePath);
       self.assetStream.url = encodeURI(self.assetStream.url);
       self.httpClient
