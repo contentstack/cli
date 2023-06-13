7d79c5afe7fc2d53f62cc58faae4f56a8ea3fb20
Vikram Kalta <vikram.kalta@contentstack.com>
2023-06-13 09:33:05 +0100

    test(bulk-publish): setup unit tests

---
+++ packages/contentstack-bulk-publish/test/unit/commands/assets/publish.test.js
@@ -0,0 +1,22 @@
+const { describe, it } = require('mocha');
+const AssetsPublish = require('../../../../src/commands/cm/assets/publish');
+const { cliux } = require('@contentstack/cli-utilities');
+const sinon = require('sinon');
+const { config } = require('dotenv');
+
+const { stub } = sinon;
+
+config();
+
+const environments = process.env.ENVIRONMENTS.split(',');
+const locales = process.env.LOCALES.split(',');
+
+describe('AssetsPublish', () => {
+  it('Should run the command when all the flags are passed', async function () {
+    const args = ['--environments', environments, '--locales', locales, '--alias', process.env.MANAGEMENT_ALIAS, '--yes'];
+    const inquireStub = stub(cliux, 'inquire');
+    await AssetsPublish.run(args);
+    sinon.assert.notCalled(inquireStub);
+    inquireStub.restore();
+  });
+});
