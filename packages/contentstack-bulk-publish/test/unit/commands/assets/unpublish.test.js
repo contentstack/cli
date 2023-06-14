637625fe865e13bf7110f581215e3c3dce4af503
Vikram Kalta <vikram.kalta@contentstack.com>
2023-06-14 10:03:03 +0100

    fix: added mocha config file

--- packages/contentstack-bulk-publish/test/unit/commands/assets/unpublish.test.js
+++ packages/contentstack-bulk-publish/test/unit/commands/assets/unpublish.test.js
@@ -1,5 +1,5 @@
 const { describe, it } = require('mocha');
-const AssetsPublish = require('../../../../src/commands/cm/assets/publish');
+const AssetsUnpublish = require('../../../../src/commands/cm/assets/unpublish');
 const { cliux } = require('@contentstack/cli-utilities');
 const sinon = require('sinon');
 const { config } = require('dotenv');
@@ -11,13 +11,13 @@ config();
 const environments = process.env.ENVIRONMENTS.split(',');
 const locales = process.env.LOCALES.split(',');
 
-describe('AssetsPublish', () => {
+describe('AssetsUnpublish', () => {
   it('Should run the command when all the flags are passed', async function () {
     for (const env of environments) {
       for (const locale of locales) {
-        const args = ['--environment', env, '--locale', locale, '--alias', process.env.MANAGEMENT_ALIAS, '--yes'];
+        const args = ['--environment', env, '--locale', locale, '--alias', process.env.MANAGEMENT_ALIAS, '--delivery-token', process.env.DELIVERY_TOKEN, '--yes'];
         const inquireStub = stub(cliux, 'inquire');
-        await AssetsPublish.run(args);
+        await AssetsUnpublish.run(args);
         sinon.assert.notCalled(inquireStub);
         inquireStub.restore();
       }
