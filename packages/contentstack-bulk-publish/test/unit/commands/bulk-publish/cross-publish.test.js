36acc9123261a249410d608e6540f9c86174cb1c
Vikram Kalta <vikram.kalta@contentstack.com>
2023-06-21 12:27:19 +0100

    test(bulk-publish): added skeleton of unit tests

--- packages/contentstack-bulk-publish/test/unit/commands/bulk-publish/cross-publish.test.js
+++ packages/contentstack-bulk-publish/test/unit/commands/bulk-publish/cross-publish.test.js
@@ -1,26 +1,26 @@
-// const { describe, it } = require('mocha');
-// const CrossPublish = require('../../../../src/commands/cm/bulk-publish/cross-publish');
-// const { cliux } = require('@contentstack/cli-utilities');
-// const sinon = require('sinon');
-// const { config } = require('dotenv');
+const { describe, it } = require('mocha');
+const CrossPublish = require('../../../../src/commands/cm/bulk-publish/cross-publish');
+const { cliux } = require('@contentstack/cli-utilities');
+const sinon = require('sinon');
+const { config } = require('dotenv');
 
-// const { stub } = sinon;
+const { stub } = sinon;
 
-// config();
+config();
 
-// const environments = process.env.ENVIRONMENTS.split(',');
-// const locales = process.env.LOCALES.split(',');
+const environments = process.env.ENVIRONMENTS.split(',');
+const locales = process.env.LOCALES.split(',');
 
-// describe('CrossPublish', () => {
-//   it('Should run the command when all the flags are passed', async function () {
-//     for (const env of environments) {
-//       for (const locale of locales) {
-//         const args = ['--environment', env, '--locale', locale, '--alias', process.env.MANAGEMENT_ALIAS, '--delivery-token', process.env.DELIVERY_TOKEN, '--yes'];
-//         const inquireStub = stub(cliux, 'inquire');
-//         await CrossPublish.run(args);
-//         sinon.assert.notCalled(inquireStub);
-//         inquireStub.restore();
-//       }
-//     }
-//   });
-// });
+describe('CrossPublish', () => {
+  it('Should run the command when all the flags are passed', async function () {
+    for (const env of environments) {
+      for (const locale of locales) {
+        const args = ['--source-env', env, '--environments', process.env.DESTINATION_ENV, '--locale', locale, '--alias', process.env.MANAGEMENT_ALIAS, '--delivery-token', process.env.DELIVERY_TOKEN, '--onlyAssets', '--yes'];
+        const inquireStub = stub(cliux, 'inquire');
+        await CrossPublish.run(args);
+        sinon.assert.notCalled(inquireStub);
+        inquireStub.restore();
+      }
+    }
+  });
+});
