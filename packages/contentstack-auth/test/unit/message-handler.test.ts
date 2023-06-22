eed82b982fb593bb321ff83008fbf57a5f624bc9
Shrutika Almeida <shrutika.almeida@Shrutikas-MacBook-Pro.local>
2023-06-22 11:54:02 +0530

    update unit test cases

--- packages/contentstack-auth/test/unit/message-handler.test.ts
+++ packages/contentstack-auth/test/unit/message-handler.test.ts
@@ -2,12 +2,6 @@ import { expect } from 'chai';
 import { messageHandler } from '@contentstack/cli-utilities';
 
 describe('Message Handler', () => {
-  it.skip('parse with valid message key, should resolve to message', function () {
-    const message = 'Enter your email address';
-    const result = messageHandler.parse('CLI_AUTH_LOGIN_ENTER_EMAIL_ADDRESS');
-    expect(result).to.be.equal(message);
-  });
-
   it('parse with invalid message key, returns the key itself', function () {
     const result = messageHandler.parse('CLI_LOGIN_ENTER_EMAIL_ADDRESS');
     expect(result).to.be.equal('CLI_LOGIN_ENTER_EMAIL_ADDRESS');
