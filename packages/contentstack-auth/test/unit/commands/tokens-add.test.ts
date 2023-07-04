ca682ed92bf1c07c1287134cff4890b3bac22394
Akash Bhosale <akash.bhosale@contentstack.com>
2023-07-04 15:59:46 +0530

    removed deprecated flags and errors

--- packages/contentstack-auth/test/unit/commands/tokens-add.test.ts
+++ packages/contentstack-auth/test/unit/commands/tokens-add.test.ts
@@ -23,6 +23,7 @@ describe('Tokens Add Command', () => {
   let deliveryTokenValidationStub;
   let managementTokenValidationStub;
   let environmentTokenValidationStub;
+  let printStub;
   const validAPIKey = 'adasdfagsf';
   const validDeliveryToken = '***REMOVED***';
   const validmanagementToken = 'cmajhsd98939482';
@@ -30,6 +31,7 @@ describe('Tokens Add Command', () => {
 
   before(function () {
     resetConfig();
+    printStub = stub(cliux, 'print');
     apiKeyValidationStub = sinon
       .stub(tokenValidation, 'validateAPIKey')
       .callsFake(function (client: any, apiKey: string): Promise<any> {
@@ -70,36 +72,69 @@ describe('Tokens Add Command', () => {
     deliveryTokenValidationStub.restore();
     managementTokenValidationStub.restore();
     environmentTokenValidationStub.restore();
+    printStub.restore();
     resetConfig();
   });
 
   it('Add a token with valid api key, should be added scuccessfully', async function () {
     const inquireStub = sinon.stub(cliux, 'inquire').resolves(validEnvironment);
-    await TokensAddCommand.run(['-a', 'test-api-key-token', '-k', validAPIKey, '-d', '-t', validDeliveryToken]);
+    await TokensAddCommand.run([
+      '--alias',
+      'test-api-key-token',
+      '--stack-api-key',
+      validAPIKey,
+      '--delivery',
+      '--token',
+      validDeliveryToken,
+    ]);
     expect(Boolean(config.get(`${configKeyTokens}.test-api-key-token`))).to.be.true;
     inquireStub.restore();
   });
 
   it('Add a valid management token, should be added scuccessfully', async function () {
-    await TokensAddCommand.run(['-a', 'test-management-token', '-k', validAPIKey, '-m', '-t', validmanagementToken]);
+    await TokensAddCommand.run([
+      '--alias',
+      'test-management-token',
+      '--stack-api-key',
+      validAPIKey,
+      '--management',
+      '--token',
+      validmanagementToken,
+    ]);
     expect(Boolean(config.get(`${configKeyTokens}.test-management-token`))).to.be.true;
   });
 
   it('Add a invalid management token, should fail to add', async function () {
-    await TokensAddCommand.run(['-a', 'test-management-token2', '-k', validAPIKey, '-m', '-t', 'invalid']);
+    await TokensAddCommand.run([
+      '--alias',
+      'test-management-token2',
+      '--stack-api-key',
+      validAPIKey,
+      '--management',
+      '--token',
+      'invalid',
+    ]);
     expect(Boolean(config.get(`${configKeyTokens}.test-management-token2`))).to.be.false;
   });
 
   it('Replace an existing token, should prompt for confirmation', async function () {
     const inquireStub = sinon.stub(cliux, 'inquire').resolves(true);
-    await TokensAddCommand.run(['-a', 'test-management-token', '-k', validAPIKey, '-m', '-t', 'invalid']);
+    await TokensAddCommand.run([
+      '--alias',
+      'test-management-token',
+      '--stack-api-key',
+      validAPIKey,
+      '--management',
+      '--token',
+      'invalid',
+    ]);
     expect(inquireStub.calledOnce).to.be.true;
     inquireStub.restore();
   });
 
   it('Add a token without alias, should prompt for alias', async function () {
     const inquireStub = sinon.stub(cliux, 'inquire').resolves(true);
-    await TokensAddCommand.run(['-k', validAPIKey, '-m', '-t', 'invalid']);
+    await TokensAddCommand.run(['--stack-api-key', validAPIKey, '--management', '--token', 'invalid']);
     expect(inquireStub.calledOnce).to.be.true;
     inquireStub.restore();
   });
@@ -109,11 +144,13 @@ describe('Management and Delivery token flags', () => {
   let inquireStub;
   let errorStub;
   let successStub;
+  let printStub;
 
   beforeEach(() => {
     inquireStub = stub(cliux, 'inquire');
     errorStub = stub(logger, 'error');
     successStub = stub(cliux, 'success');
+    printStub = stub(cliux, 'print');
     resetConfig();
   });
 
@@ -121,10 +158,11 @@ describe('Management and Delivery token flags', () => {
     inquireStub.restore();
     errorStub.restore();
     successStub.restore();
+    printStub.restore();
     resetConfig();
   });
 
-  describe('-Management token', () => {
+  describe('- Management token', () => {
     it('Should ask for a prompt to select type of token to add', async () => {
       await TokensAddCommand.run([]);
       assert.calledWith(inquireStub, {
@@ -139,33 +177,41 @@ describe('Management and Delivery token flags', () => {
     });
 
     it('Should ask for api key ', async () => {
-      await TokensAddCommand.run(['-m', '-a', 'newToken']);
+      await TokensAddCommand.run(['--management', '--alias', 'newToken']);
       assert.calledWith(inquireStub, { type: 'input', message: 'CLI_AUTH_TOKENS_ADD_ENTER_API_KEY', name: 'apiKey' });
     });
     it('Should ask for api key ', async () => {
-      await TokensAddCommand.run(['-m', '-a', 'newToken']);
+      await TokensAddCommand.run(['--management', '--alias', 'newToken']);
       assert.calledWith(inquireStub, { type: 'input', message: 'CLI_AUTH_TOKENS_ADD_ENTER_API_KEY', name: 'apiKey' });
     });
     it('Invalid API key should throw error', async () => {
-      await TokensAddCommand.run(['-m', '-a', 'newToken', '-k', 'asdf', '-t', 'asdf']);
+      await TokensAddCommand.run(['--management', '--alias', 'newToken', '--stack-api-key', 'asdf', '--token', 'asdf']);
       assert.calledTwice(errorStub);
     });
     it('Throw error if api key is kept empty', async () => {
-      await TokensAddCommand.run(['-m', '-a', 'newToken', '-k', ' ', '-t', 'asdf']);
+      await TokensAddCommand.run(['--management', '--alias', 'newToken', '--stack-api-key', ' ', '--token', 'asdf']);
       assert.calledTwice(errorStub);
     });
     it('Throw error if token is kept empty', async () => {
-      await TokensAddCommand.run(['-m', '-a', 'newToken', '-k', process.env.BRANCH_ENABLED_API_KEY!, '-t', '']);
+      await TokensAddCommand.run([
+        '--management',
+        '--alias',
+        'newToken',
+        '--stack-api-key',
+        process.env.BRANCH_ENABLED_API_KEY!,
+        '--token',
+        '',
+      ]);
       assert.calledWith(errorStub);
     });
     it('Should add a token successfully after all the values are passed with stack having branches enabled', async () => {
       await TokensAddCommand.run([
-        '-m',
-        '-a',
+        '--management',
+        '--alias',
         'newToken',
-        '-k',
+        '--stack-api-key',
         process.env.BRANCH_ENABLED_API_KEY!,
-        '-t',
+        '--token',
         process.env.BRANCH_ENABLED_MGMT_TOKEN!,
         '--branch',
         'main',
@@ -174,24 +220,24 @@ describe('Management and Delivery token flags', () => {
     });
     it('Should add a token successfully for stack with branches disabled after all the values are passed', async () => {
       await TokensAddCommand.run([
-        '-m',
-        '-a',
+        '--management',
+        '--alias',
         'newToken',
-        '-k',
+        '--stack-api-key',
         process.env.BRANCH_DISABLED_API_KEY!,
-        '-t',
+        '--token',
         process.env.BRANCH_DISABLED_MGMT_TOKEN!,
       ]);
       assert.calledWith(successStub, 'CLI_AUTH_TOKENS_ADD_SUCCESS');
     });
     it('Should throw an error if branch flag is passed along with stack not having branches enabled', async () => {
       await TokensAddCommand.run([
-        '-m',
-        '-a',
+        '--management',
+        '--alias',
         'newToken',
-        '-k',
+        '--stack-api-key',
         process.env.BRANCH_DISABLED_API_KEY!,
-        '-t',
+        '--token',
         process.env.BRANCH_DISABLED_MGMT_TOKEN!,
         '--branch',
         'main',
@@ -200,23 +246,29 @@ describe('Management and Delivery token flags', () => {
     });
   });
 
-  describe('-Delivery token', () => {
+  describe('- Delivery token', () => {
     it('Should ask for api key', async () => {
-      await TokensAddCommand.run(['-d', '-a', 'newToken']);
+      await TokensAddCommand.run(['--delivery', '--alias', 'newToken']);
       assert.calledWith(inquireStub, { type: 'input', message: 'CLI_AUTH_TOKENS_ADD_ENTER_API_KEY', name: 'apiKey' });
     });
     it('Should ask for delivery token', async () => {
-      await TokensAddCommand.run(['-d', '-a', 'newToken', '-k', process.env.BRANCH_ENABLED_API_KEY!]);
+      await TokensAddCommand.run([
+        '--delivery',
+        '--alias',
+        'newToken',
+        '--stack-api-key',
+        process.env.BRANCH_ENABLED_API_KEY!,
+      ]);
       assert.calledWith(inquireStub, { type: 'input', message: 'CLI_AUTH_TOKENS_ADD_ENTER_TOKEN', name: 'token' });
     });
     it('Should ask for environment', async () => {
       await TokensAddCommand.run([
-        '-d',
-        '-a',
+        '--delivery',
+        '--alias',
         'newToken',
-        '-k',
+        '--stack-api-key',
         process.env.BRANCH_ENABLED_API_KEY!,
-        '-t',
+        '--token',
         process.env.BRANCH_ENABLED_DELIVERY_TOKEN!,
       ]);
       assert.calledWith(inquireStub, {
@@ -227,28 +279,28 @@ describe('Management and Delivery token flags', () => {
     });
     it('Should add a new token if all the values are set correctly for stack with branches enabled', async () => {
       await TokensAddCommand.run([
-        '-d',
-        '-a',
+        '--delivery',
+        '--alias',
         'newToken',
-        '-k',
+        '--stack-api-key',
         process.env.BRANCH_ENABLED_API_KEY!,
-        '-t',
+        '--token',
         process.env.BRANCH_ENABLED_DELIVERY_TOKEN!,
-        '-e',
+        '--environment',
         process.env.BRANCH_ENABLED_ENVIRONMENT!,
       ]);
       assert.calledWith(successStub, 'CLI_AUTH_TOKENS_ADD_SUCCESS');
     });
     it('Should add a new token if all the values are set correctly for stack with branches disabled', async () => {
       await TokensAddCommand.run([
-        '-d',
-        '-a',
+        '--delivery',
+        '--alias',
         'newToken',
-        '-k',
+        '--stack-api-key',
         process.env.BRANCH_DISABLED_API_KEY!,
-        '-t',
+        '--token',
         process.env.BRANCH_DISABLED_DELIVERY_TOKEN!,
-        '-e',
+        '--environment',
         process.env.BRANCH_DISABLED_ENVIRONMENT!,
       ]);
       assert.calledWith(successStub, 'CLI_AUTH_TOKENS_ADD_SUCCESS');
@@ -256,14 +308,14 @@ describe('Management and Delivery token flags', () => {
     it('Should throw and error for stack with branches disabled', async () => {
       let branch = 'my-branch';
       await TokensAddCommand.run([
-        '-d',
-        '-a',
+        '--delivery',
+        '--alias',
         'newToken',
-        '-k',
+        '--stack-api-key',
         process.env.BRANCH_DISABLED_API_KEY!,
-        '-t',
+        '--token',
         process.env.BRANCH_DISABLED_DELIVERY_TOKEN!,
-        '-e',
+        '--environment',
         process.env.BRANCH_DISABLED_ENVIRONMENT!,
         '--branch',
         branch,
