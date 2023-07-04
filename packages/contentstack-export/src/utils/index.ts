708fc851a8b9112a5f370f715948128ad3ff6f1e
Shafeeq PP <shafeeq.pp@Shafeeqs-MacBook-Pro.local>
2023-07-04 17:46:36 +0530

    locales update

--- packages/contentstack-export/src/utils/index.ts
+++ packages/contentstack-export/src/utils/index.ts
@@ -1,6 +1,7 @@
 export * as interactive from './interactive';
 export { default as setupExportConfig } from './export-config-handler';
 export * as fileHelper from './file-helper';
+export { fsUtil } from './file-helper';
 export { default as setupBranches } from './setup-branches';
 export { default as setupExportDir } from './setup-export-dir';
 export { log, unlinkFileLogger } from './logger';
