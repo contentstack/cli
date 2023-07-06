2944ed7a84eca17ba6a22da1c970f8a25ad69ba2
Aman Kumar <aman.kumar@contentstack.com>
2023-07-06 14:18:01 +0530

    fix: extension integration test

--- packages/contentstack-export/src/export/modules-js/index.js
+++ packages/contentstack-export/src/export/modules-js/index.js
@@ -1,6 +1,8 @@
-export default async function startModuleExport(modulePayload) {
+async function startModuleExport(modulePayload) {
   const { moduleName, exportConfig, stackAPIClient } = modulePayload;
-  const { default: ModuleRunner } = await import(`./${moduleName}`);
+  const { default: ModuleRunner } = await import(`./${moduleName}.js`);
   const moduleRunner = new ModuleRunner(exportConfig, stackAPIClient);
   return moduleRunner.start();
 }
+
+module.exports = startModuleExport;
