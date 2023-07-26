b545db1e9fe57ca36829b0588289703f3837f9bd
Aman Kumar <aman.kumar@contentstack.com>
2023-07-26 14:05:11 +0530

    revert: backup dir changes

--- packages/contentstack-import/src/utils/backup-handler.ts
+++ packages/contentstack-import/src/utils/backup-handler.ts
@@ -1,17 +1,13 @@
 import * as path from 'path';
 import ncp from 'ncp';
-
 import { ImportConfig } from '../types';
-import { fileHelper } from './index';
 
 export default function setupBackupDir(importConfig: ImportConfig): Promise<string> {
   return new Promise(async (resolve, reject) => {
-    if (fileHelper.fileExistsSync(importConfig.useBackedupDir)) {
+    if (importConfig.hasOwnProperty('useBackedupDir')) {
       return resolve(importConfig.useBackedupDir);
     }
-    
-    const backupDirPath =
-      importConfig?.useBackedupDir || path.join(process.cwd(), '_backup_' + Math.floor(Math.random() * 1000));
+    const backupDirPath = path.join(process.cwd(), '_backup_' + Math.floor(Math.random() * 1000));
     const limit = importConfig.backupConcurrency || 16;
     if (path.isAbsolute(importConfig.contentDir)) {
       return ncp(importConfig.contentDir, backupDirPath, { limit }, (error) => {
