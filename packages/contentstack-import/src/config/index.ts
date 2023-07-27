dfaa8ae3302b672bdff87fac6d7982cc22eb32ac
Aman Kumar <aman.kumar@contentstack.com>
2023-07-27 12:37:31 +0530

    refactor: import config

--- packages/contentstack-import/src/config/index.ts
+++ packages/contentstack-import/src/config/index.ts
@@ -388,8 +388,7 @@ const config: DefaultConfig = {
   developerHubBaseUrl: '',
   marketplaceAppEncryptionKey: 'nF2ejRQcTv',
   getEncryptionKeyMaxRetry: 3,
-  useNewModuleStructure: true,
-  createBackupDir: './temp'
+  useNewModuleStructure: true
   // useBackedupDir: '',
   // backupConcurrency: 10,
 };
