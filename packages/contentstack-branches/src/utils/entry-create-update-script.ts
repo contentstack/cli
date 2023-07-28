fcfdee0a2aa2cbf21193a976997fb46853d10f99
Shrutika Almeida <shrutika.almeida@Shrutikas-MacBook-Pro.local>
2023-07-28 10:39:10 +0530

    update migration scripts with reference logic

--- packages/contentstack-branches/src/utils/entry-create-update-script.ts
+++ packages/contentstack-branches/src/utils/entry-create-update-script.ts
@@ -45,6 +45,46 @@ export function entryCreateUpdateScript(contentType) {
       return uniqueArray;
     }
   
+    function getValueByPath(obj, path) {
+      return path.split('[').reduce((o, key) => o && o[key.replace(/\]$/, '')], obj);
+    }
+  
+    function updateValueByPath(obj, path, newValue) {
+      path.split('[').reduce((o, key, index, arr) => {
+        if (index === arr.length - 1) {
+          o[key.replace(/\]$/, '')][0].uid = newValue;
+        } else {
+          return o[key.replace(/\]$/, '')];
+        }
+      }, obj);
+    }
+
+    const findReference = function (schema, path, flag) {
+      let references = [];
+  
+      for (const i in schema) {
+        const currentPath = path ? path + '[' + schema[i].uid : schema[i].uid;
+        if (schema[i].data_type === 'group' || schema[i].data_type === 'global_field') {
+          references = references.concat(findReference(schema[i].schema, currentPath, flag));
+        } else if (schema[i].data_type === 'blocks') {
+          for (const block in schema[i].blocks) {
+            references = references.concat(
+              findReference(
+                schema[i].blocks[block].schema,
+                currentPath + '[' + block + '][' + schema[i].blocks[block].uid + ']',
+                flag,
+              ),
+            );
+          }
+        } else if (schema[i].data_type === 'reference') {
+          flag.references = true;
+          references.push(currentPath);
+        }
+      }
+  
+      return references;
+    };
+
     const updateEntryTask = () => {
       return {
         title: 'Update Entries',
@@ -64,6 +104,40 @@ export function entryCreateUpdateScript(contentType) {
           .stack({ api_key: stackSDKInstance.api_key, branch_uid: compareBranch })
           .contentType('${contentType}')
           .fetch();
+
+          let flag = {
+            references: false
+          };
+  
+          const references = await findReference(contentType.schema, '', flag);
+  
+          async function updateEntry(entry, entryDetails) {
+            Object.assign(entry, { ...entryDetails });
+            await entry.update();
+          }
+
+          async function updateReferences(entryDetails, baseEntry, references) {
+            for (let i in references) {
+              let compareEntryRef = getValueByPath(entryDetails, references[i]);
+              let baseEntryRef = getValueByPath(baseEntry, references[i]);
+  
+              if (compareEntryRef.length > 0 && baseEntryRef.length >= 0) {
+                let compareRefEntry = await managementAPIClient
+                  .stack({ api_key: stackSDKInstance.api_key, branch_uid: compareBranch })
+                  .contentType(compareEntryRef[0]._content_type_uid)
+                  .entry(compareEntryRef[0].uid)
+                  .fetch();
+                let baseRefEntry = await stackSDKInstance
+                  .contentType(compareEntryRef[0]._content_type_uid)
+                  .entry()
+                  .query({ query: { title: compareRefEntry.title } })
+                  .find();
+  
+                updateValueByPath(entryDetails, references[i], baseRefEntry.items[0].uid);
+              }
+            }
+          }
+
           try {
             if (contentType.options.singleton) {
               compareBranchEntries.items.map(async (el) => {
@@ -72,10 +146,20 @@ export function entryCreateUpdateScript(contentType) {
                 if (baseBranchEntries.items.length) {
                   let baseEntryUid = baseBranchEntries.items[0].uid;
                   let entry = await stackSDKInstance.contentType('${contentType}').entry(baseEntryUid);
-                  Object.assign(entry, { ...entryDetails });
-                  entry.update();
+                  
+                  if (flag.references) {
+                    await updateReferences(entryDetails, baseBranchEntries.items[0], references);
+                  }
+  
+                  await updateEntry(entry, entryDetails);
                 } else {
-                  await stackSDKInstance.contentType('${contentType}').entry().create({ entry: entryDetails });
+                  let createdEntry = await stackSDKInstance.contentType('${contentType}').entry().create({ entry: entryDetails });
+                
+                  if (flag.references) {
+                    await updateReferences(entryDetails, createdEntry, references);
+                  }
+  
+                  await updateEntry(createdEntry, entryDetails);
                 }
               });
             } else {
@@ -88,13 +172,22 @@ export function entryCreateUpdateScript(contentType) {
                 let entryDetails = deleteUnwantedKeysFromObject(compareMap.get(el), keysToRemove);
   
                 if (compareMap.get(el) && !baseMap.get(el)) {
-                  await stackSDKInstance.contentType('${contentType}').entry().create({ entry: entryDetails });
+                  let createdEntry = await stackSDKInstance.contentType('${contentType}').entry().create({ entry: entryDetails });
+                
+                  if (flag.references) {
+                    await updateReferences(entryDetails, createdEntry, references);
+                  }
+  
+                  await updateEntry(createdEntry, entryDetails);
                 } else if (compareMap.get(el) && baseMap.get(el)) {
                   let baseEntry = baseMap.get(el);
-  
                   let entry = await stackSDKInstance.contentType('${contentType}').entry(baseEntry.uid);
-                  Object.assign(entry, { ...entryDetails });
-                  entry.update();
+                  
+                  if (flag.references) {
+                    await updateReferences(entryDetails, baseEntry, references);
+                  }
+
+                  await updateEntry(entry, entryDetails);
                 }
               });
             }
