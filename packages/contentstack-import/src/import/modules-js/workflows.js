735bbec505b08b210c33281fd601ecb68a6f366a
raj pandey <raj.pandey@contentstack.com>
2023-08-01 12:57:50 +0530

    Changes Done in modules of import command to handle errors

--- packages/contentstack-import/src/import/modules-js/workflows.js
+++ packages/contentstack-import/src/import/modules-js/workflows.js
@@ -147,15 +147,6 @@ module.exports = class importWorkflows {
         })
         .catch(function (error) {
           fileHelper.writeFileSync(workflowFailsPath, self.fails);
-          let message_content_type = '';
-          if (error.request !== undefined && JSON.parse(error.request.data).workflow !== undefined) {
-            if (JSON.parse(error.request.data).workflow.name) {
-              message_content_type =
-                ' Failed workflow: ' + JSON.parse(error.request.data).workflow.name;
-            }
-            error.errorMessage = error.errorMessage + message_content_type;
-          }
-          log(self.config, formatError(error.errorMessage), 'error');
           log(self.config, `Workflows import failed. ${formatError(error)}`, 'error');
           return reject(error);
         });
