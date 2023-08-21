cffcf4a91277a76b53658ca76fd90ff3668d03bc
Aman Kumar <aman.kumar@contentstack.com>
2023-08-21 15:35:10 +0530

    fix: recursive backup directory issue

--- packages/contentstack-import/src/utils/backup-handler.ts
+++ packages/contentstack-import/src/utils/backup-handler.ts
@@ -1,8 +1,10 @@
+import * as os from 'os';
 import * as path from 'path';
-import ncp from 'ncp';
+import camelCase from 'lodash/camelCase';
+import { copy, copySync, removeSync } from 'fs-extra';
 
-import { ImportConfig } from '../types';
 import { fileHelper } from './index';
+import { ImportConfig } from '../types';
 
 export default function setupBackupDir(importConfig: ImportConfig): Promise<string> {
   return new Promise(async (resolve, reject) => {
@@ -20,16 +22,13 @@ export default function setupBackupDir(importConfig: ImportConfig): Promise<stri
       backupDirPath = importConfig.createBackupDir;
     }
 
-    const limit = importConfig.backupConcurrency || 16;
-    if (path.isAbsolute(importConfig.contentDir)) {
-      return ncp(importConfig.contentDir, backupDirPath, { limit }, (error) => {
-        if (error) {
-          return reject(error);
-        }
-        return resolve(backupDirPath);
-      });
+    const currentWorkingDir = process.cwd();
+    //handle error :- Cannot copy to a subdirectory of itself
+    if (importConfig.contentDir === currentWorkingDir || importConfig.contentDir === `${currentWorkingDir}/`) {
+      handleSubDirectoryError(importConfig, backupDirPath);
+      return resolve(backupDirPath);
     } else {
-      ncp(importConfig.contentDir, backupDirPath, (error) => {
+      return copy(importConfig.contentDir, backupDirPath, (error: any) => {
         if (error) {
           return reject(error);
         }
@@ -38,3 +37,16 @@ export default function setupBackupDir(importConfig: ImportConfig): Promise<stri
     }
   });
 }
+
+/**
+ * handle subdirectory error
+ * https://github.com/jprichardson/node-fs-extra/issues/708
+ * @param {ImportConfig} importConfig
+ * @param {string} backupDirPath
+ */
+const handleSubDirectoryError = (importConfig: ImportConfig, backupDirPath: string) => {
+  const tempDestination = `${os.platform() === 'darwin' ? '/private/tmp' : '/tmp'}/${camelCase(backupDirPath)}`;
+  copySync(importConfig.contentDir, tempDestination);
+  copySync(tempDestination, backupDirPath);
+  removeSync(tempDestination);
+};
