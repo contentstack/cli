61288da9de0647754a276cfe67c40493dee97b5e
Aman Kumar <aman.kumar@contentstack.com>
2023-08-24 12:24:25 +0530

    fix: ctSchema is not iterable

--- packages/contentstack-branches/src/utils/entry-create-script.ts
+++ packages/contentstack-branches/src/utils/entry-create-script.ts
@@ -166,7 +166,7 @@ export function entryCreateScript(contentType) {
     }
   
     function findAssetIdsFromJsonRte(entryObj, ctSchema) {
-      if(ctSchema !== undefined || ctSchema !== null){
+      if(ctSchema !== undefined){
         for (const element of ctSchema) {
           switch (element.data_type) {
             case 'blocks': {
