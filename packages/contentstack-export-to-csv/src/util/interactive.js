a5a43ff52e0b67c36c2506eceeb66f6cd3a7d584
Aman Kumar <aman.kumar@contentstack.com>
2023-09-07 13:10:22 +0530

    feat: taxonomies & terms support in export-to-csv

---
+++ packages/contentstack-export-to-csv/src/util/interactive.js
@@ -0,0 +1,20 @@
+const isEmpty = require('lodash/isEmpty');
+const { cliux } = require('@contentstack/cli-utilities');
+
+function inquireRequireFieldValidation(input) {
+  if (isEmpty(input)) {
+    return "This field can't be empty";
+  }
+  return true;
+}
+
+async function askTaxonomyUID() {
+  return await cliux.inquire({
+    type: 'input',
+    message: 'Enter taxonomy UID',
+    name: 'taxonomy_uid',
+    validate: inquireRequireFieldValidation,
+  });
+}
+
+module.exports = { askTaxonomyUID };
