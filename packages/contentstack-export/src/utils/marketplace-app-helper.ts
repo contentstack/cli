b4cf543438eeb07673ebe7d124b85fbbc3be35d8
raj pandey <raj.pandey@contentstack.com>
2023-09-12 15:10:09 +0530

    Used Axios Instance in both import and export to replace with httpClient call

--- packages/contentstack-export/src/utils/marketplace-app-helper.ts
+++ packages/contentstack-export/src/utils/marketplace-app-helper.ts
@@ -3,7 +3,6 @@ import { cliux, configHandler, NodeCrypto, HttpClient, managementSDKClient } fro
 import { formatError, log } from '../utils';
 import { ExportConfig } from '../types';
 import { askDeveloperHub } from './interactive';
-
 export const getDeveloperHubUrl = async (exportConfig: ExportConfig) => {
   const { cma, name } = configHandler.get('region') || {};
   let developerHubBaseUrl = exportConfig?.developerHubUrls[cma];
@@ -56,8 +55,16 @@ export const getStackSpecificApps = async (params: {
   skip: number;
 }) => {
   const { developerHubBaseUrl, httpClient, config, skip } = params;
-  return httpClient
-    .get(`${developerHubBaseUrl}/installations?target_uids=${config.source_stack}&skip=${skip}`)
+  const appSdkAxiosInstance = await managementSDKClient({
+    host: developerHubBaseUrl.split('://').pop(),
+    endpoint: developerHubBaseUrl,
+  });
+  return appSdkAxiosInstance.axiosInstance
+    .get(`${developerHubBaseUrl}/installations?target_uids=${config.source_stack}&skip=${skip}`, {
+      headers: {
+        organization_uid: config.org_uid,
+      },
+    })
     .then((data: any) => data.data)
     .catch((error: any) => log(config, `Failed to export marketplace-apps ${formatError(error)}`, 'error'));
 };
