13bdf95a8ed8254cfd7e13a94999124dadd9d674
Antony <antonyagustineraj@gmail.com>
2023-09-13 13:05:02 +0530

    Fix: cm:stacks:audit functionality for Content type [IP]

--- packages/contentstack-audit/src/config/index.ts
+++ packages/contentstack-audit/src/config/index.ts
@@ -1,5 +1,12 @@
 const config = {
-  modues: []
+  modules: ['content-types', 'entries', 'global-fields'],
+  skipRefs: ['sys_assets'],
+  moduleConfig: {
+    'content-types': {
+      dirName: 'content_types',
+      fileName: 'content_types.json',
+    },
+  },
 };
 
 export default config;
