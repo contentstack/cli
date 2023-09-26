4e34630fdc850f83a5af56955d1ae8bf3132b638
Aman Kumar <aman.kumar@contentstack.com>
2023-09-26 11:19:19 +0530

    fix: test cases

--- packages/contentstack-export-to-csv/test/unit/commands/export-to-csv.test.js
+++ packages/contentstack-export-to-csv/test/unit/commands/export-to-csv.test.js
@@ -9,15 +9,15 @@ const { cliux, configHandler } = require('@contentstack/cli-utilities');
 const mockData = require('../../mock-data/common.mock.json');
 
 const { cma } = configHandler.get('region');
-// const directory = './data';
+const directory = join(process.cwd(), 'data');
 const taxonomyFileName = join(process.cwd(), 'data', mockData.stacks[0].name);
-//const userFileName = join(process.cwd(), 'data', mockData.organizations[0].name);
 
 describe('export-to-csv with action taxonomies', () => {
+  if(!fs.existsSync(directory)) fs.mkdirSync(directory);
+
   describe('Create taxonomies & terms csv file with all flags including taxonomy uid', () => {
     test
-      .stdout({ print: true })
-      .stub(fs, 'existsSync', () => new PassThrough())
+      .stdout({ print: process.env.PRINT === "true" || false  })
       .stub(fs, 'createWriteStream', () => new PassThrough())
       .nock(cma, (api) => {
         api
@@ -51,10 +51,10 @@ describe('export-to-csv with action taxonomies', () => {
       })
       .it('CSV file should be created');
   });
+
   describe('Create taxonomies & terms csv file with all flags excluding taxonomy uid', () => {
     test
-      .stdout({ print: process.env.PRINT === "true" || false })
-      .stub(fs, 'existsSync', () => new PassThrough())
+      .stdout({ print: process.env.PRINT === "true" || false  })
       .stub(fs, 'createWriteStream', () => new PassThrough())
       .nock(cma, (api) => {
         api
@@ -87,14 +87,13 @@ describe('export-to-csv with action taxonomies', () => {
       ])
       .do(({ stdout }) => {
         expect(stdout).to.contain(`Writing taxonomies to file: ${taxonomyFileName}_taxonomies.csv`);
-        //expect(stdout).to.contain(`Writing taxonomies to file: ${taxonomyFileName}_taxonomies.csv`)
       })
       .it('file should be created');
   });
+
   describe('Create taxonomies & terms csv file with prompt', () => {
     test
       .stdout({ print: process.env.PRINT === "true" || false })
-      .stub(fs, 'existsSync', () => new PassThrough())
       .stub(fs, 'createWriteStream', () => new PassThrough())
       .stub(inquirer, 'registerPrompt', () => {})
       .stub(inquirer, 'prompt', () => {
@@ -128,35 +127,38 @@ describe('export-to-csv with action taxonomies', () => {
 });
 
 describe('export-to-csv with action entries', () => {
+  if(!fs.existsSync(directory)) fs.mkdirSync(directory);
+
   describe('Create entries csv file with flags', () => {
     test
       .stdout({ print: true })
-      .stub(fs, 'existsSync', () => new PassThrough())
       .stub(fs, 'createWriteStream', () => new PassThrough())
-      //nock api for content type
-      //nock api for list branches
-      //nock api for branch
       .nock(cma, (api) => {
         api
           .get(`/v3/stacks?&query={"org_uid":"${mockData.organizations[0].uid}"}`)
           .reply(200, { stacks: mockData.stacks });
       })
       .nock(cma, (api) => {
-        api.get('/v3/environments').reply(200, mockData.environments);
+        api.get('/v3/environments').reply(200, {environments: mockData.environments});
+      })
+      // .nock(cma, (api) => {
+      //   api.get('/v3/stack/branches').reply(200, { branch: mockData.branch });
+      // })
+      .nock(cma, (api) => {
+        api.get('/v3/content_types?count=true').reply(200, { content_types: 2 });
       })
       .nock(cma, (api) => {
-        api.get('/v3/stack/branches').reply(200, { branch: mockData.branch });
-      })
-      .nock(cma, (api) => {
-        api.get('/v3/content_types?count=true').reply(200, { count: 2 });
-      })
-      .nock(cma, (api) => {
-        api.get('/v3/content_types').reply(200, { resp: mockData.contentTypes });
+        api.get('/v3/content_types').reply(200, {res: mockData.contentTypes} );
       })
       .nock(cma, (api) => {
         api
-          .get(`/v3/content_types/${mockData.contentTypes.items[0].uid}/entries`)
-          .reply(200, { entry: mockData.entry });
+          .get(`/v3/content_types/${mockData.contentTypes[0].uid}/entries?include_publish_details=true&locale=en1&count=true`)
+          .reply(200, { entries: 2 });
+      })
+      .nock(cma, (api) => {
+        api
+          .get(`/v3/content_types/${mockData.contentTypes[0].uid}/entries?include_publish_details=true&locale=en1&skip=0&limit=100&include_workflow=true`)
+          .reply(200, {entries: mockData.entry});
       })
       .command([
         'cm:export-to-csv',
@@ -171,19 +173,14 @@ describe('export-to-csv with action entries', () => {
         '--locale',
         'en1',
         '--content-type',
-        mockData.contentTypes.items[0].uid,
+        mockData.contentTypes[0].uid,
       ])
-      .do(({ stdout }) => {
-        //expect(stdout).to.contain(`Writing taxonomies to file: ${taxonomyFileName}_taxonomies.csv`);
-        //expect(stdout).to.contain(`Writing taxonomies to file: ${taxonomyFileName}_taxonomies.csv`)
-      })
       .it('Entries CSV file should be created');
   });
 
   describe('Create entries csv file with prompt', () => {
     test
       .stdout({ print: true })
-      .stub(fs, 'existsSync', () => new PassThrough())
       .stub(fs, 'createWriteStream', () => new PassThrough())
       .stub(inquirer, 'registerPrompt', () => {})
       .stub(inquirer, 'prompt', () => {
@@ -192,8 +189,7 @@ describe('export-to-csv with action entries', () => {
           chosenOrg: mockData.organizations[0].name,
           chosenStack: mockData.stacks[0].name,
           chosenLanguage: 'en1',
-          chosenContentTypes: mockData.contentTypes.items[0].uid,
-          branch: mockData.branch.uid,
+          chosenContentTypes: mockData.contentTypes[0].title
         });
       })
       .nock(cma, (api) => {
@@ -210,21 +206,26 @@ describe('export-to-csv with action entries', () => {
           .reply(200, { stacks: mockData.stacks });
       })
       .nock(cma, (api) => {
-        api.get('/v3/environments').reply(200, mockData.environments);
+        api.get('/v3/environments').reply(200, {environments: mockData.environments});
+      })
+      // .nock(cma, (api) => {
+      //   api.get('/v3/stack/branches').reply(200, { branch: mockData.branch });
+      // })
+      .nock(cma, (api) => {
+        api.get('/v3/content_types?count=true').reply(200, { content_types: 2 });
       })
       .nock(cma, (api) => {
-        api.get('/v3/stack/branches').reply(200, { branch: mockData.branch });
-      })
-      .nock(cma, (api) => {
-        api.get('/v3/content_types?count=true').reply(200, { count: 2 });
-      })
-      .nock(cma, (api) => {
-        api.get('/v3/content_types').reply(200, { resp: mockData.contentTypes });
+        api.get('/v3/content_types?skip=0&include_branch=true').reply(200, {contentTypes: mockData.contentTypes} );
       })
       .nock(cma, (api) => {
         api
-          .get(`/v3/content_types/${mockData.contentTypes.items[0].uid}/entries`)
-          .reply(200, { entry: mockData.entry });
+          .get(`/v3/content_types/${mockData.contentTypes[0].uid}/entries?include_publish_details=true&locale=en1&count=true`)
+          .reply(200, { entries: 2 });
+      })
+      .nock(cma, (api) => {
+        api
+          .get(`/v3/content_types/${mockData.contentTypes[0].uid}/entries?include_publish_details=true&locale=en1&skip=0&limit=100&include_workflow=true`)
+          .reply(200, {entry: mockData.entry});
       })
       .command(['cm:export-to-csv'])
       .it('Entries CSV file should be created with prompt');
@@ -234,17 +235,16 @@ describe('export-to-csv with action entries', () => {
 describe('export-to-csv with action users', () => {
   describe('Export users csv file with flags', () => {
     test
-      .stdout({ print: true })
-      .stub(fs, 'existsSync', () => new PassThrough())
+      .stdout({ print: process.env.PRINT === "true" || false  })
       .stub(fs, 'createWriteStream', () => new PassThrough())
       .nock(cma, (api) => {
-        api.get('/v3/user?include_orgs_roles=true').reply(200, { user: mockData.users.items[0] });
+        api.get('/v3/user?include_orgs_roles=true').reply(200, { user: mockData.users[0] });
       })
       .nock(cma, (api) => {
         api.get(`/v3/organizations/${mockData.organizations[0].uid}/roles`).reply(200, { roles: mockData.roles });
       })
       .nock(cma, (api) => {
-        api.get('/v3/user?include_orgs_roles=true').reply(200, { user: mockData.users.items[0] });
+        api.get('/v3/user?include_orgs_roles=true').reply(200, { user: mockData.users[0] });
       })
       .nock(cma, (api) => {
         api
@@ -252,16 +252,12 @@ describe('export-to-csv with action users', () => {
           .reply(200, { users: mockData.users });
       })
       .command(['cm:export-to-csv', '--action', 'users', '--org', mockData.organizations[0].uid])
-      // .do(({ stdout }) =>
-      //   expect(stdout).to.contain(`Writing organization details to file: ${userFileName}_users_export.csv`),
-      // )
       .it('Users csv file should be successfully created');
   });
   
   describe('Export users csv file with prompt', () => {
     test
-      .stdout({ print: true })
-      .stub(fs, 'existsSync', () => new PassThrough())
+      .stdout({ print: process.env.PRINT === "true" || false  })
       .stub(fs, 'createWriteStream', () => new PassThrough())
       .stub(inquirer, 'registerPrompt', () => {})
       .stub(inquirer, 'prompt', () => {
@@ -271,23 +267,20 @@ describe('export-to-csv with action users', () => {
         api.get(`/v3/organizations?limit=100`).reply(200, { organizations: mockData.organizations });
       })
       .nock(cma, (api) => {
-        api.get('/v3/user?include_orgs_roles=true').reply(200, { user: mockData.users.items[0] });
+        api.get('/v3/user?include_orgs_roles=true').reply(200, { user: mockData.users[0] });
       })
       .nock(cma, (api) => {
         api.get(`/v3/organizations/${mockData.organizations[0].uid}/roles`).reply(200, { roles: mockData.roles });
       })
       .nock(cma, (api) => {
-        api.get('/v3/user?include_orgs_roles=true').reply(200, { user: mockData.users.items[0] });
+        api.get('/v3/user?include_orgs_roles=true').reply(200, { user: mockData.users[0] });
       })
       .nock(cma, (api) => {
         api
           .get(`/v3/organizations/${mockData.organizations[0].uid}/share?skip=0&page=1&limit=100`)
-          .reply(200, { users: mockData.users.items });
+          .reply(200, { users: mockData.users });
       })
       .command(['cm:export-to-csv'])
-      // .do(({ stdout }) =>
-      //   expect(stdout).to.contain(`Writing organization details to file: ${userFileName}_users_export.csv`),
-      // )
       .it('Users csv file should be successfully created');
   });
 });
