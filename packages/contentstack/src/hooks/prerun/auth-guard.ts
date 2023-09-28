7a8a7aec2eda37b2c043b26cc4f961a9370cb644
raj pandey <raj.pandey@contentstack.com>
2023-09-28 13:08:36 +0530

    Changed Logs so that a common log file is created

--- packages/contentstack/src/hooks/prerun/auth-guard.ts
+++ packages/contentstack/src/hooks/prerun/auth-guard.ts
@@ -1,12 +1,13 @@
 import { 
-  logger, 
   cliux, 
   managementSDKClient,
   isAuthenticated
 } from '@contentstack/cli-utilities';
-
+import {LoggerService} from '@contentstack/cli-utilities'
 // TBD: run region command if region is not there
+
 export default async function (opts): Promise<void> {
+  const newLogger = new LoggerService(process.cwd(),'cli-log');
   const { context: { plugin: { config: { protectedCommands = {} } = {} } = {}, region = null } = {} } = this.config;
   if (opts.Command.id !== 'config:set:region') {
     if (!region) {
@@ -19,7 +20,7 @@ export default async function (opts): Promise<void> {
   // Auth guard
   if (protectedCommands[opts.Command.id]) {
     if (!isAuthenticated()) {
-      logger.error('No auth token found for command', opts.Command.id);
+      newLogger.error('No auth token found for command', opts.Command.id);
       cliux.error('Please login to execute the command');
       this.exit();
     }
@@ -27,13 +28,13 @@ export default async function (opts): Promise<void> {
     try {
       const result = await client.getUser();
       if (!result) {
-        logger.error('error in auth validation');
+        newLogger.error('error in auth validation');
         cliux.error('Please login to execute the command');
         this.exit();
       }
-      logger.debug('logged in user', result.data);    
+      newLogger.debug('logged in user', result.data);    
     } catch (error) {
-      logger.error('error in auth validation', error);
+      newLogger.error('error in auth validation', error);
       cliux.error('Please login to execute the command');
       process.exit();
     }   
