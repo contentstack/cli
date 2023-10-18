d606bbe76814e7a3c485bcae51f0395d8dbc737b
Aman Kumar <aman.kumar@contentstack.com>
2023-10-18 14:49:31 +0530

    refactor: log warning msg, update sdk version & handle taxonomies query

--- packages/contentstack-import/src/utils/logger.ts
+++ packages/contentstack-import/src/utils/logger.ts
@@ -81,7 +81,12 @@ function init(_logPath: string) {
     logger = winston.createLogger({
       transports: [
         new winston.transports.File(successTransport),
-        new winston.transports.Console({ format: winston.format.simple() }),
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.simple(),
+            winston.format.colorize({ all: true, colors: { warn: 'yellow', info: 'white' } }),
+          ),
+        }),
       ],
       levels: myCustomLevels.levels,
     });
@@ -109,7 +114,7 @@ function init(_logPath: string) {
         logger.log('info', logString);
       }
     },
-    warn: function () {
+    warn: function (message: any) {
       let args = slice.call(arguments);
       let logString = returnString(args);
       if (logString) {
@@ -138,7 +143,8 @@ export const log = async (config: ImportConfig, message: any, type: string) => {
   // ignoring the type argument, as we are not using it to create a logfile anymore
   if (type !== 'error') {
     // removed type argument from init method
-    init(config.data).log(message);
+    if (type === 'warn') init(config.data).warn(message); //logged warning message in log file
+    else init(config.data).log(message);
   } else {
     init(config.data).error(message);
   }
