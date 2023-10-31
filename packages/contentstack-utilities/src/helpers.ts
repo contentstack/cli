ff891b4733e4fea141db85f71f1002f0402cc898
raj pandey <raj.pandey@contentstack.com>
2023-10-31 13:15:50 +0530

    added api check to check the validity of the management token

--- packages/contentstack-utilities/src/helpers.ts
+++ packages/contentstack-utilities/src/helpers.ts
@@ -1,4 +1,5 @@
 import authHandler from './auth-handler';
+import { HttpClient, cliux, configHandler } from '.';
 export const isAuthenticated = () => authHandler.isAuthenticated();
 export const doesBranchExist = async (stack, branchName) => {
   return stack
@@ -8,3 +9,15 @@ export const doesBranchExist = async (stack, branchName) => {
       return error;
     });
 };
+
+export const isManagementTokenValid =async (stackAPIKey, managementToken) => {
+  const httpClient = new HttpClient({ headers: { api_key: stackAPIKey, authorization: managementToken } });
+
+  const response = (await httpClient.get(`${configHandler.get('region').cma}/v3/environments?limit=1`)).data;
+  
+  if (response?.error_code === 105 || "error_code" in response) {
+    cliux.print(`error: Management Token or Stack API key is Not Valid. ${response.error_message || response.errorMessage}`,{color:"red"});
+    return false
+  }
+  return true
+}
