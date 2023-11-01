b430f5db0b2aada9b14875de6f9ff12f7180a165
raj pandey <raj.pandey@contentstack.com>
2023-11-01 19:30:19 +0530

    modified the test cases

--- packages/contentstack-export-to-csv/test/unit/commands/export-to-csv.test.js
+++ packages/contentstack-export-to-csv/test/unit/commands/export-to-csv.test.js
@@ -118,7 +118,7 @@ describe('Testing the teams support in cli export-to-csv',()=>{
       .stub(process, 'chdir', () => {})
       .stub(inquirer, 'registerPrompt', () => {})
       .stub(inquirer, 'prompt', () => {
-          return Promise.resolve({ action: 'teams', chosenOrg: mockData.organizations[2].name});
+          return Promise.resolve({ action: 'teams', chosenOrg: mockData.organizations[2].name });
       })
         .nock(cma, (api) => {
           api.get('/v3/user?include_orgs_roles=true').reply(200, { user: mockData.users[2] });
@@ -143,4 +143,70 @@ describe('Testing the teams support in cli export-to-csv',()=>{
         ])
      .it('CSV file should be created');
   });
+  describe('Testing Teams Command with prompt and no stack role data', () => {
+    test
+    .stdout({ print: process.env.PRINT === 'true' || true })
+    .stub(fs, 'createWriteStream', () => new PassThrough())
+    .stub(mkdirp, 'sync', () => {})
+    .stub(process, 'chdir', () => {})
+    .stub(inquirer, 'registerPrompt', () => {})
+    .stub(inquirer, 'prompt', () => {
+        return Promise.resolve({ action: 'teams', chosenOrg: mockData.organizations[2].name,  chooseExport: 'yes'});
+    })
+      .nock(cma, (api) => {
+        api.get('/v3/user?include_orgs_roles=true').reply(200, { user: mockData.users[2] });
+      })
+    .nock(cma, (api) => {
+        api
+          .get(`/organizations/org_uid_1_teams/teams?skip=0&limit=100&includeUserDetails=true`)
+          .reply(200, mockData.Teams.allTeams);
+      })
+      .nock(cma, (api) => {
+        api
+          .get(`/v3/organizations/org_uid_1_teams/roles`)
+          .reply(200,  mockData.org_roles);
+      })
+      .nock(cma, (api) => {
+        api
+          .get(`/v3/roles`)
+          .reply(200, {roles: {}});
+      })
+      .command([
+        'cm:export-to-csv'
+      ])
+   .it('CSV file should be created');
+});
+describe('Testing Teams Command with prompt and no stack role data', () => {
+  test
+  .stdout({ print: process.env.PRINT === 'true' || true })
+  .stub(fs, 'createWriteStream', () => new PassThrough())
+  .stub(mkdirp, 'sync', () => {})
+  .stub(process, 'chdir', () => {})
+  .stub(inquirer, 'registerPrompt', () => {})
+  .stub(inquirer, 'prompt', () => {
+      return Promise.resolve({ action: 'teams', chosenOrg: mockData.organizations[2].name,  chooseExport: 'no'});
+  })
+    .nock(cma, (api) => {
+      api.get('/v3/user?include_orgs_roles=true').reply(200, { user: mockData.users[2] });
+    })
+  .nock(cma, (api) => {
+      api
+        .get(`/organizations/org_uid_1_teams/teams?skip=0&limit=100&includeUserDetails=true`)
+        .reply(200, mockData.Teams.allTeams);
+    })
+    .nock(cma, (api) => {
+      api
+        .get(`/v3/organizations/org_uid_1_teams/roles`)
+        .reply(200,  mockData.org_roles);
+    })
+    .nock(cma, (api) => {
+      api
+        .get(`/v3/roles`)
+        .reply(200, {roles: {}});
+    })
+    .command([
+      'cm:export-to-csv'
+    ])
+ .it('No CSV file should be created');
+});
 });
