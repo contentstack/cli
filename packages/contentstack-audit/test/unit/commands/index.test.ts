bab492b1fc394cbf7e74b5e4b8cd7e2007f3aab0
Antony <antonyagustineraj@gmail.com>
2023-11-06 22:48:38 +0530

    Test: Unit test cases added for utility and content-types audit command

---
+++ packages/contentstack-audit/test/unit/commands/index.test.ts
@@ -0,0 +1,66 @@
+import sinon from 'sinon';
+import winston from 'winston';
+import { expect, test } from '@oclif/test';
+import { FileTransportInstance } from 'winston/lib/winston/transports';
+import { AuditBaseCommand } from '../../../src/audit-base-command';
+
+describe('Audit command', () => {
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  describe('Audit run method', () => {
+    test
+      .stdout({ print: process.env.PRINT === 'true' || false })
+      .stub(
+        winston.transports,
+        'File',
+        () =>
+          class FsTransport {
+            constructor() {}
+          } as FileTransportInstance,
+      )
+      .stub(winston, 'createLogger', () => ({ log: () => {}, error: () => {} }))
+      .stub(AuditBaseCommand.prototype, 'start', () => {})
+      .command(['cm:stacks:audit'])
+      .it('should trigger AuditBaseCommand start method', ({ stdout }) => {
+        expect(stdout).to.be.string;
+      });
+
+    test
+      .stderr({ print: false })
+      .stdout({ print: process.env.PRINT === 'true' || false })
+      .stub(
+        winston.transports,
+        'File',
+        () =>
+          class FsTransport {
+            constructor() {}
+          } as FileTransportInstance,
+      )
+      .stub(winston, 'createLogger', () => ({ log: console.log, error: console.error }))
+      .stub(AuditBaseCommand.prototype, 'start', () => Promise.reject('process failed'))
+      .command(['cm:stacks:audit'])
+      .exit(1)
+      .it('should log any error and exit with status code 1');
+
+    test
+      .stderr({ print: false })
+      .stdout({ print: process.env.PRINT === 'true' || false })
+      .stub(
+        winston.transports,
+        'File',
+        () =>
+          class FsTransport {
+            constructor() {}
+          } as FileTransportInstance,
+      )
+      .stub(winston, 'createLogger', () => ({ log: console.log, error: console.error }))
+      .stub(AuditBaseCommand.prototype, 'start', () => {
+        throw Error('process failed');
+      })
+      .command(['cm:stacks:audit'])
+      .exit(1)
+      .it('should log the error objet message and exit with status code 1');
+  });
+});
