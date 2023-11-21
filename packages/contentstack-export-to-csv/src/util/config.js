61a225810115c29b03e927874f39e64347f2f036
raj pandey <raj.pandey@contentstack.com>
2023-11-21 13:01:21 +0530

    added teamsUser field to be removed when exporting the teams, some ui text changes.

--- packages/contentstack-export-to-csv/src/util/config.js
+++ packages/contentstack-export-to-csv/src/util/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   cancelString: 'Cancel and Exit',
   exportEntries: 'Export entries to a .CSV file',
-  exportUsers: "Export organization user's data to a .CSV file",
-  exportTeams: "Export organization team's data to a .csv file",
+  exportUsers: "Export organization users' data to a .CSV file",
+  exportTeams: "Export organization teams' data to a .CSV file",
   adminError: "Unable to export data. Make sure you're an admin or owner of this organization",
   organizationNameRegex: /\'/,
   CLI_EXPORT_CSV_LOGIN_FAILED: "You need to login to execute this command. See: auth:login --help",
