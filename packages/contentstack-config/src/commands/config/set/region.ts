f69d92d00bb1c8b05cdcfeda93d7b4ff02779100
raj pandey <raj.pandey@contentstack.com>
2024-01-24 12:26:46 +0530

    removed GCP-NA from args

--- packages/contentstack-config/src/commands/config/set/region.ts
+++ packages/contentstack-config/src/commands/config/set/region.ts
@@ -89,7 +89,7 @@ export default class RegionSetCommand extends BaseCommand<typeof RegionSetComman
         this.logger.error('failed to set the region', error);
         cliux.error(`Failed to set region due to: ${error.message}`);
       }
-    } else if (['NA', 'EU', 'AZURE-NA', 'AZURE-EU', 'GCP-NA'].includes(selectedRegion)) {
+    } else if (['NA', 'EU', 'AZURE-NA', 'AZURE-EU'].includes(selectedRegion)) {
       const regionDetails: Region = regionHandler.setRegion(selectedRegion);
       await authHandler.setConfigData('logout'); //Todo: Handle this logout flow well through logout command call
       cliux.success(`Region has been set to ${regionDetails.name}`);
