389b6904f9cce11d4e539ec9571e13305d446210
raj pandey <raj.pandey@contentstack.com>
2024-02-20 14:24:04 +0530

    added stack name to the config

--- packages/contentstack-import/src/types/import-config.ts
+++ packages/contentstack-import/src/types/import-config.ts
@@ -48,6 +48,7 @@ export default interface ImportConfig extends DefaultConfig, ExternalConfig {
   replaceExisting?: boolean;
   skipExisting?: boolean;
   skipAudit?: boolean;
+  stack_name: string;
 }
 
 type branch = {
