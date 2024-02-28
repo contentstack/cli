e506870cb5479fb510ac9d729758ef7c0d38824b
raj pandey <raj.pandey@contentstack.com>
2024-02-28 18:34:28 +0530

    audit fix for extensions

--- packages/contentstack-audit/src/modules/extensions.ts
+++ packages/contentstack-audit/src/modules/extensions.ts
@@ -55,38 +55,91 @@ export default class Extensions {
     this.extensionsSchema = existsSync(this.extensionsPath)
       ? values(JSON.parse(readFileSync(this.extensionsPath, 'utf-8')) as Extension[])
       : [];
-    this.ctSchema = [];
     this.ctSchema.forEach((ct) => this.ctUidSet.add(ct.uid));
 
     this.extensionsSchema.forEach((ext: Extension) => {
       const { title, uid, scope } = ext;
       let ctNotPresent: string[] = [];
+
       scope?.content_types.forEach((ct) => {
         if (!this.ctUidSet.has(ct)) {
           ctNotPresent.push(ct);
           this.missingCts.add(ct);
         }
       });
-      if (ctNotPresent.length) {
+
+      if (ctNotPresent.length && ext.scope) {
         ext.scope.content_types = ctNotPresent;
         this.missingCtInExtensions.push(cloneDeep(ext));
       }
+
       this.log(
-        $t(auditMsg.SCAN_CT_SUCCESS_MSG, {
-          name: title,
+        $t(auditMsg.SCAN_EXT_SUCCESS_MSG, {
+          title: title,
           module: this.config.moduleConfig[this.moduleName].name,
+          uid: uid,
         }),
         'info',
       );
     });
-    this.missingCtInExtensions.forEach((ext) => {
-      console.log(ext.scope);
-    });
-    console.log(this.missingCtInExtensions);
+
+    if (this.fix && this.missingCtInExtensions.length) {
+      await this.fixExtensionsScope(cloneDeep(this.missingCtInExtensions));
+    }
+
     return this.missingCtInExtensions;
   }
 
-  async fixExtensionsScope() {}
+  async fixExtensionsScope(missingCtInExtensions: Extension[]) {
+    for (let ext in missingCtInExtensions) {
+      if (missingCtInExtensions[ext].scope) {
+        missingCtInExtensions[ext].scope.content_types = missingCtInExtensions[ext].scope.content_types.filter((ct) => {
+          !this.missingCts.has(ct);
+        });
+      }
+    }
 
-  async writeFixContent() {}
+    let newExtensionSchema: Record<string, Extension> = existsSync(this.extensionsPath)
+      ? JSON.parse(readFileSync(this.extensionsPath, 'utf8'))
+      : {};
+
+    if (Object.keys(newExtensionSchema).length !== 0) {
+      for (let ext in missingCtInExtensions) {
+        let fixedCts = missingCtInExtensions[ext].scope.content_types.filter((ct) => {
+          !this.missingCts.has(ct);
+        });
+        if (fixedCts.length) {
+          newExtensionSchema[missingCtInExtensions[ext].uid].scope.content_types = fixedCts;
+        } else {
+          this.log(
+            $t(commonMsg.EXTENSION_FIX_WARN, {
+              name: missingCtInExtensions[ext].title,
+              uid: missingCtInExtensions[ext].uid,
+            }),
+            { color: 'yellow' },
+          );
+          if (this.config.flags.yes || (await ux.confirm(commonMsg.EXTENSION_FIX_CONFIRMATION))) {
+            delete newExtensionSchema[missingCtInExtensions[ext].uid];
+          }
+        }
+      }
+    }
+    this.writeFixContent(newExtensionSchema);
+  }
+
+  async writeFixContent(fixedExtensions: Record<string, Extension>) {
+    let canWrite = true;
+
+    if (this.fix) {
+      if (!this.config.flags['copy-dir'] && !this.config.flags['external-config']?.skipConfirm) {
+        canWrite = this.config.flags.yes ?? (await ux.confirm(commonMsg.FIX_CONFIRMATION));
+      }
+      if (canWrite) {
+        writeFileSync(
+          join(this.folderPath, this.config.moduleConfig[this.moduleName].fileName),
+          JSON.stringify(fixedExtensions),
+        );
+      }
+    }
+  }
 }
