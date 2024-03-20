f9ea45ecbec45ee3f28b10d64e01c4b6e3dd4578
raj pandey <raj.pandey@contentstack.com>
2024-03-20 18:32:23 +0530

    Added error logs only and for the api errors showing only the selected fields, removed the error handler and updated the entry migration script

--- packages/contentstack-migration/src/utils/error-handler.js
+++ packages/contentstack-migration/src/utils/error-handler.js
@@ -1,21 +1,19 @@
 'use strict';
 
-const { error } = require('./logger');
 const { errorMessageHandler } = require('./constants');
 
 module.exports = (data, type, method, err) => {
-  if (data && type && method) {
-    error(`Error occurred while ${errorMessageHandler[method]} ${type}: ${data}.`);
-  }
-
-  if (err.errorMessage) {
-    error(err.errorMessage);
-  }
-  if (err instanceof Error && err && err.message && err.stack) {
-    error(err.message);
-    // error(err.stack)
-  } else {
-    error(err);
-  }
+  // NOTE: Commenting this code as the API errors are handled through error-helper.js also
+  // if (data && type && method) {
+  //   logger.log('error', { error: `Error occurred while ${errorMessageHandler[method]} ${type}: ${data}.` });
+  // }
+  // if (err.errorMessage) {
+  //   logger.log('error', { errorAPI: err.errorMessage });
+  // }
+  // if (err instanceof Error && err && err.message && err.stack) {
+  //   logger.log('error', { error: err.message });
+  // } else {
+  //   logger.log('error', { error: err });
+  // }
   // throw new Error(err);
 };
