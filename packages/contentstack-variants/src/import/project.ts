3e8715d11dd5fe4d6fd12dfa8a2b9737e1dc4f82
Antony <antonyagustineraj@gmail.com>
2024-04-01 13:31:18 +0530

    Feat: Import project functionality added

--- packages/contentstack-variants/src/import/project.ts
+++ packages/contentstack-variants/src/import/project.ts
@@ -1,3 +1,37 @@
-import { PersonalizationAdapter } from '../utils';
+import { join } from 'path';
+import { existsSync } from 'fs';
 
-export class Project extends PersonalizationAdapter {}
+import { PersonalizationAdapter } from '../utils';
+import { APIConfig, CreateProjectInput, ImportConfig, LogType } from '../types';
+
+export class Project extends PersonalizationAdapter<ImportConfig> {
+  constructor(public readonly config: ImportConfig, private readonly log: LogType = console.log) {
+    const conf: APIConfig = {
+      config,
+      baseURL: config.personalizationHost,
+    };
+    super(Object.assign(config, conf));
+  }
+
+  /**
+   * The function `importProject` checks if a project path exists, creates a project if it does, and
+   * logs the result.
+   * @param {CreateProjectInput} input - The `input` parameter in the `importProject` function likely
+   * represents the data or configuration needed to create a new project. It is of type
+   * `CreateProjectInput`, which could be an object containing information such as project name,
+   * description, settings, or any other details required for creating a project.
+   */
+  async importProject(input: CreateProjectInput) {
+    const personalization = this.config.modules.personalization;
+    const { dirName, fileName } = personalization.projects;
+    const projectPath = join(this.config.data, personalization.dirName, dirName, fileName);
+
+    if (existsSync(projectPath)) {
+      await this.createProject(input).catch((error) => {
+        this.log(this.config, error, 'error');
+      });
+
+      this.log(this.config, 'Project created successfully!', 'info');
+    }
+  }
+}
--- packages/contentstack-variants/src/utils/personalization-api-adapter.ts
+++ packages/contentstack-variants/src/utils/personalization-api-adapter.ts
@@ -1,8 +1,10 @@
 import { AdapterHelper } from './adapter-helper';
-import { CreateProjectInput, GetProjectsParams, Personalization, ProjectStruct } from '../types';
+import { CreateProjectInput, GetProjectsParams, LogType, Personalization, ProjectStruct } from '../types';
 
-export class PersonalizationAdapter extends AdapterHelper implements Personalization {
+export class PersonalizationAdapter<T> extends AdapterHelper<T> implements Personalization<T> {
   async projects(options: GetProjectsParams, projects: ProjectStruct[] = []): Promise<ProjectStruct[] | void> {}
 
-  async createProject(input: CreateProjectInput): Promise<ProjectStruct | void> {}
+  async createProject(input: CreateProjectInput): Promise<ProjectStruct | void> {
+    return (await this.apiClient.get<ProjectStruct>('/projects', input)).data;
+  }
 }
