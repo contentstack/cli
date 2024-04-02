31ce46f4e2103e93fcb45440bc7ea2e27be415de
Aman Kumar <aman.kumar@contentstack.com>
2024-04-02 18:34:54 +0530

    feat: event & audience export

---
+++ packages/contentstack-variants/src/export/audiences.ts
@@ -0,0 +1,60 @@
+import omit from 'lodash/omit';
+import { resolve as pResolve } from 'node:path';
+
+import { formatError, fsUtil, PersonalizationAdapter, log } from '../utils';
+import { EclipseConfig, ExportConfig, AudiencesStruct, AudiencesConfig, LogType } from '../types';
+
+export default class ExportAudiences extends PersonalizationAdapter<ExportAudiences> {
+  private audiencesConfig: AudiencesConfig;
+  private audiencesFolderPath: string;
+  private audiences: Record<string, unknown>[];
+  public exportConfig: ExportConfig;
+  public eclipseConfig: EclipseConfig;
+
+  constructor(exportConfig: ExportConfig) {
+    super({
+      config: {
+        ...exportConfig,
+      },
+      baseURL: exportConfig.modules.eclipse.baseURL,
+      headers: {
+        organization_uid: exportConfig.org_uid,
+        authtoken: exportConfig.auth_token,
+        project_id: exportConfig.project_id,
+      },
+    });
+    this.exportConfig = exportConfig;
+    this.eclipseConfig = this.exportConfig.modules.eclipse;
+    this.audiencesConfig = this.exportConfig.modules.audiences;
+    this.audiencesFolderPath = pResolve(
+      this.exportConfig.data,
+      this.exportConfig.branchName || '',
+      this.eclipseConfig.dirName,
+      this.audiencesConfig.dirName,
+    );
+    this.audiences = [];
+  }
+
+  async start() {
+    try {
+      log(this.exportConfig, 'Starting audiences export', 'info');
+      await fsUtil.makeDirectory(this.audiencesFolderPath);
+      this.audiences = (await this.getAudiences()) as AudiencesStruct[];
+
+      if (!this.audiences?.length) {
+        log(this.exportConfig, 'No Audiences found with the given project!', 'info');
+        return;
+      } else {
+        fsUtil.writeFile(pResolve(this.audiencesFolderPath, this.audiencesConfig.fileName), this.audiences);
+        log(this.exportConfig, 'All the audiences have been exported successfully!', 'success');
+        return;
+      }
+    } catch (error) {
+      log(this.exportConfig, `Failed to export audiences! ${formatError(error)}`, 'error');
+    }
+  }
+
+  sanitizeAttribs() {
+    this.audiences = this.audiences?.map((audience) => omit(audience, this.audiencesConfig.invalidKeys)) || [];
+  }
+}
--- packages/contentstack-variants/src/utils/personalization-api-adapter.ts
+++ packages/contentstack-variants/src/utils/personalization-api-adapter.ts
@@ -1,12 +1,27 @@
 import { HttpClient } from '@contentstack/cli-utilities';
 
 import { AdapterHelper } from './adapter-helper';
-import { CreateProjectInput, GetProjectsParams, Personalization, ProjectStruct } from '../types';
+import {
+  CreateProjectInput,
+  GetProjectsParams,
+  Personalization,
+  ProjectStruct,
+  EventsStruct,
+  AudiencesStruct,
+} from '../types';
 
 export class PersonalizationAdapter<T> extends AdapterHelper<T, HttpClient> implements Personalization<T> {
   async projects(options: GetProjectsParams, projects: ProjectStruct[] = []): Promise<ProjectStruct[] | void> {}
 
   async createProject(input: CreateProjectInput): Promise<ProjectStruct | void> {
     return (await this.apiClient.get<ProjectStruct>('/projects', input)).data;
+  }
+
+  async getEvents(): Promise<EventsStruct[] | void> {
+    return (await this.apiClient.get<EventsStruct>('/events')).data;
+  }
+
+  async getAudiences(): Promise<AudiencesStruct[] | void> {
+    return (await this.apiClient.get<AudiencesStruct>('/audiences')).data;
   }
 }
