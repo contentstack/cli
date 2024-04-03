0e901b18e94507e15e0086b9a8ed427a3689aa3e
Aman Kumar <aman.kumar@contentstack.com>
2024-04-03 15:49:10 +0530

    fix: error while exporting experience & project

--- packages/contentstack-variants/src/export/projects.ts
+++ packages/contentstack-variants/src/export/projects.ts
@@ -5,24 +5,29 @@ import { ExportConfig, EclipseConfig } from '../types';
 import { PersonalizationAdapter, log, fsUtil, formatError } from '../utils';
 
 export default class ExportProjects extends PersonalizationAdapter<ExportConfig> {
+  private projectFolderPath: string;
   public exportConfig: ExportConfig;
   public eclipseConfig: EclipseConfig;
   constructor(exportConfig: ExportConfig) {
     super({
-      config: exportConfig,
-      baseURL: exportConfig.personalizationHost,
-      sharedConfig: {
-        ...exportConfig,
-        headers: { organization_uid: exportConfig.org_uid, authtoken: exportConfig.auth_token },
-      },
-      eclipseURL: exportConfig.modules.eclipse.baseURL,
+      config: {...exportConfig},
+      baseURL: exportConfig.modules.eclipse.baseURL,
+      headers: { organization_uid: exportConfig.org_uid, authtoken: exportConfig.auth_token },
     });
     this.exportConfig = exportConfig;
     this.eclipseConfig = exportConfig.modules.eclipse;
+    this.projectFolderPath = path.resolve(
+      exportConfig.data,
+      exportConfig.branchName || '',
+      this.eclipseConfig.dirName,
+      'projects',
+    );
   }
 
   async start() {
     try {
+      log(this.exportConfig, 'Starting projects export', 'info');
+      await fsUtil.makeDirectory(this.projectFolderPath);
       const project = await this.projects({ connectedStackApiKey: this.exportConfig.apiKey });
       if (!project || project?.length < 1) {
         log(this.exportConfig, 'No Personalization Project connected with the give stack', 'info');
@@ -31,7 +36,8 @@ export default class ExportProjects extends PersonalizationAdapter<ExportConfig>
       }
       this.exportConfig.project_id = project[0].uid;
       this.exportConfig.personalizationEnabled = true;
-      return fsUtil.writeFile(path.join(this.eclipseConfig.dirName, 'projects', 'projects.json'), project);
+      this.exportConfig.project_id = project[0]?.uid;
+      fsUtil.writeFile(path.resolve(this.projectFolderPath, 'projects.json'), project);
     } catch (error) {
       log(this.exportConfig, `Failed to export projects  ${formatError(error)}`, 'error');
       log(this.exportConfig, error, 'error');
