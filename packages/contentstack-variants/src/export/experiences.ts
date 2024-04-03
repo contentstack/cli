87b350940692dd81f796d691fc20d235969f240e
Aman Kumar <aman.kumar@contentstack.com>
2024-04-03 15:49:10 +0530

    fix: error while exporting experience & project

--- packages/contentstack-variants/src/export/experiences.ts
+++ packages/contentstack-variants/src/export/experiences.ts
@@ -3,20 +3,27 @@ import { EclipseConfig, ExportConfig } from '../types';
 import { formatError, fsUtil, log, PersonalizationAdapter } from '../utils';
 
 export default class ExportExperiences extends PersonalizationAdapter<ExportConfig> {
+  private experiencesFolderPath: string;
   public exportConfig: ExportConfig;
   public eclipseConfig: EclipseConfig;
   constructor(exportConfig: ExportConfig) {
     super({
-      config: exportConfig,
-      baseURL: exportConfig.personalizationHost,
-      sharedConfig: {
-        ...exportConfig,
-        headers: { organization_uid: exportConfig.org_uid, authtoken: exportConfig.auth_token },
+      config: { ...exportConfig },
+      baseURL: exportConfig.modules.eclipse.baseURL,
+      headers: {
+        organization_uid: exportConfig.org_uid,
+        authtoken: exportConfig.auth_token,
+        project_id: exportConfig.project_id,
       },
-      eclipseURL: exportConfig.modules.eclipse.baseURL,
     });
     this.exportConfig = exportConfig;
     this.eclipseConfig = exportConfig.modules.eclipse;
+    this.experiencesFolderPath = path.resolve(
+      exportConfig.data,
+      exportConfig.branchName || '',
+      this.eclipseConfig.dirName,
+      'experiences',
+    );
   }
 
   async start() {
@@ -24,12 +31,14 @@ export default class ExportExperiences extends PersonalizationAdapter<ExportConf
       // get all experiences
       // loop through experiences and get content types attached to it
       // write experiences in to a file
+      log(this.exportConfig, 'Starting experiences export', 'info');
+      await fsUtil.makeDirectory(this.experiencesFolderPath);
       const experiences = await this.getExperiences();
       if (!experiences || experiences?.length < 1) {
         log(this.exportConfig, 'No Experiences found with the give project', 'info');
         return;
       }
-      return fsUtil.writeFile(path.join(this.eclipseConfig.dirName, 'experiences', 'experiences.json'), experiences);
+      return fsUtil.writeFile(path.resolve(this.experiencesFolderPath, 'experiences.json'), experiences);
     } catch (error) {
       log(this.exportConfig, `Failed to export experiences  ${formatError(error)}`, 'error');
       log(this.exportConfig, error, 'error');
