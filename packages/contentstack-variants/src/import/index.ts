17e809c13472274667ca2e10023ae6acb2b9c9a3
Aman Kumar <aman.kumar@contentstack.com>
2024-04-05 15:45:25 +0530

    feat: implemented audience import, attribute mapper & attribute helper

--- packages/contentstack-variants/src/import/index.ts
+++ packages/contentstack-variants/src/import/index.ts
@@ -1,8 +1,10 @@
 import Project from './project';
 import Attribute from './attribute';
+import Audiences from './audiences';
 
 // NOTE Acting as namespace to avoid the same class name conflicts in other modules
 export const Import = {
   Project,
   Attribute,
+  Audiences
 };
