e28eef8d06d92b7f9e14a2df1d79471342c4bfc6
Aman Kumar <aman.kumar@contentstack.com>
2024-04-10 17:35:15 +0530

    feat: experiences import

--- packages/contentstack-variants/src/import/index.ts
+++ packages/contentstack-variants/src/import/index.ts
@@ -1,10 +1,12 @@
 import Project from './project';
 import Attribute from './attribute';
 import Audiences from './audiences';
+import Experiences from './experiences';
 
 // NOTE Acting as namespace to avoid the same class name conflicts in other modules
 export const Import = {
   Project,
   Attribute,
-  Audiences
+  Audiences,
+  Experiences
 };
