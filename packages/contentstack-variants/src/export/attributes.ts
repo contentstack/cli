20b6967f5ec70ca40a7c0a0ea6873ca498e48bbf
shafeeqd959 <shafeeq.pp@contentstack.com>
2024-05-09 17:17:24 +0530

    updated personalization url to access in multiple region

--- packages/contentstack-variants/src/export/attributes.ts
+++ packages/contentstack-variants/src/export/attributes.ts
@@ -13,7 +13,7 @@ export default class ExportAttributes extends PersonalizationAdapter<ExportConfi
   constructor(readonly exportConfig: ExportConfig) {
     super({
       config: exportConfig,
-      baseURL: exportConfig.modules.personalization.baseURL,
+      baseURL: exportConfig.modules.personalization.baseURL[exportConfig.region.name],
       headers: { authtoken: exportConfig.auth_token, 'X-Project-Uid': exportConfig.project_id },
     });
     this.personalizationConfig = exportConfig.modules.personalization;
