976f74113c4ea917e7db5aef9c0204d66c82e8ca
shafeeqd959 <shafeeq.pp@contentstack.com>
2024-05-09 17:17:24 +0530

    updated personalization url to access in multiple region

--- packages/contentstack-variants/src/import/experiences.ts
+++ packages/contentstack-variants/src/import/experiences.ts
@@ -33,7 +33,7 @@ export default class Experiences extends PersonalizationAdapter<ImportConfig> {
   constructor(public readonly config: ImportConfig, private readonly log: LogType = console.log) {
     const conf: APIConfig = {
       config,
-      baseURL: config.personalizationHost,
+      baseURL: config.modules.personalization.baseURL[config.region.name],
       headers: { 'X-Project-Uid': config.modules.personalization.project_id, authtoken: config.auth_token },
     };
     super(Object.assign(config, conf));
@@ -207,6 +207,3 @@ export default class Experiences extends PersonalizationAdapter<ImportConfig> {
     }
   }
 }
-
-
-
