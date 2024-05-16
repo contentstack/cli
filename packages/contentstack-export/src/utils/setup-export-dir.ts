c2f04c76a2cd8114a3ef98f7cce7c0870283b3af
raj pandey <raj.pandey@contentstack.com>
2024-05-16 18:26:46 +0530

    SRE fixes

--- packages/contentstack-export/src/utils/setup-export-dir.ts
+++ packages/contentstack-export/src/utils/setup-export-dir.ts
@@ -1,12 +1,13 @@
 import path from 'path';
 import { ExportConfig } from '../types';
 import { makeDirectory } from './file-helper';
+import { sanitizepath } from '@contentstack/cli-utilities';
 
 export default async function setupExportDir(exportConfig: ExportConfig) {
   makeDirectory(exportConfig.exportDir);
   if (exportConfig.branches) {
     return Promise.all(
-      exportConfig.branches.map((branch) => makeDirectory(path.join(exportConfig.exportDir, branch.uid))),
+      exportConfig.branches.map((branch) => makeDirectory(path.join(sanitizepath(exportConfig.exportDir), sanitizepath(branch.uid)))),
     );
   }
 }
