7c3c49913b99b1e729a5ee9104158d36f0613038
shafeeqd959 <shafeeq.pp@contentstack.com>
2024-06-05 20:18:44 +0530

    fixed entry variant import for file fields and fixed project name duplication error

--- packages/contentstack-variants/src/import/project.ts
+++ packages/contentstack-variants/src/import/project.ts
@@ -1,7 +1,7 @@
 import { join } from 'path';
 import { existsSync, readFileSync } from 'fs';
 
-import { PersonalizationAdapter } from '../utils';
+import { PersonalizationAdapter, askProjectName } from '../utils';
 import { APIConfig, CreateProjectInput, ImportConfig, LogType } from '../types';
 
 export default class Project extends PersonalizationAdapter<ImportConfig> {
@@ -24,29 +24,36 @@ export default class Project extends PersonalizationAdapter<ImportConfig> {
     const projectPath = join(this.config.data, personalization.dirName, dirName, fileName);
 
     if (existsSync(projectPath)) {
-      try {
-        const projects = JSON.parse(readFileSync(projectPath, 'utf8')) as CreateProjectInput[];
+      const projects = JSON.parse(readFileSync(projectPath, 'utf8')) as CreateProjectInput[];
 
-        if (!projects || projects.length < 1) {
-          this.config.modules.personalization.importData = false; // Stop personalization import if stack not connected to any project
-          this.log(this.config, 'No project found!', 'info');
-          return;
-        }
+      if (!projects || projects.length < 1) {
+        this.config.modules.personalization.importData = false; // Stop personalization import if stack not connected to any project
+        this.log(this.config, 'No project found!', 'info');
+        return;
+      }
 
-        for (const project of projects) {
-          const { name, description } = project;
-          const projectRes = await this.createProject({
-            name: `Copy of ${name}`,
-            description,
+      for (const project of projects) {
+        let projectRes: any;
+        const createProject = async (newName: void | string) => {
+          projectRes = await this.createProject({
+            name: newName || project.name,
+            description: project.description,
             connectedStackApiKey: this.config.apiKey,
+          }).catch(async (error) => {
+            if (error === 'personalization.PROJECTS.DUPLICATE_NAME') {
+              const projectName = await askProjectName('Copy Of ' + (newName || project.name));
+              return await createProject(projectName);
+            }
+            this.log(this.config, this.$t(this.messages.CREATE_FAILURE, { module: 'Projects' }), 'error');
+            throw error;
           });
-          this.config.modules.personalization.project_id = projectRes?.uid;
-        }
+        };
+
+        await createProject();
+
+        this.config.modules.personalization.project_id = projectRes.uid;
         this.config.modules.personalization.importData = true;
-        this.log(this.config, this.$t(this.messages.CREATE_SUCCESS, { module: 'Projects' }), 'info');
-      } catch (error) {
-        this.log(this.config, this.$t(this.messages.CREATE_FAILURE, { module: 'Projects' }), 'error');
-        throw error;
+        this.log(this.config, `Project Created Successfully: ${projectRes.uid}`, 'info');
       }
     } else {
       this.config.modules.personalization.importData = false; // Stop personalization import if stack not connected to any project
