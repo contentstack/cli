03896475ffba2a5daa005733b0d996cb89dad0ee
shafeeqd959 <shafeeq.pp@contentstack.com>
2024-08-28 12:07:09 +0530

    updated code to check environment support/ provided the url for personlise

--- packages/contentstack-export/src/export/modules/personalization.ts
+++ packages/contentstack-export/src/export/modules/personalization.ts
@@ -4,6 +4,7 @@ import {
   ExportEvents,
   ExportAttributes,
   ExportAudiences,
+  AnyProperty,
 } from '@contentstack/cli-variants';
 
 import { log, formatError } from '../../utils';
@@ -11,14 +12,22 @@ import { ModuleClassParams, ExportConfig } from '../../types';
 
 export default class ExportPersonalization {
   public exportConfig: ExportConfig;
+  public personalizeConfig: { dirName: string; baseURL: Record<string, string> } & AnyProperty;
   constructor({ exportConfig }: ModuleClassParams) {
     this.exportConfig = exportConfig;
+    this.personalizeConfig = exportConfig.modules.personalization;
   }
 
   async start(): Promise<void> {
     try {
+      if (!this.personalizeConfig.baseURL[this.exportConfig.region.name]) {
+        log(this.exportConfig, 'Skipping Personalize project export, personalize url is not set', 'info');
+        this.exportConfig.personalizationEnabled = false;
+        return;
+      }
       if (this.exportConfig.management_token) {
         log(this.exportConfig, 'Skipping Personalize project export when using management token', 'info');
+        this.exportConfig.personalizationEnabled = false;
         return;
       }
       await new ExportProjects(this.exportConfig).start();
