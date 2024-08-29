56de4c3800c4d1823d04a2e6a4fbeebf86090fb4
Harshitha D <harshitha.d@contentstack.com>
2024-08-29 10:59:18 +0530

    updated for personalize url in attributes

--- packages/contentstack-variants/src/export/attributes.ts
+++ packages/contentstack-variants/src/export/attributes.ts
@@ -3,7 +3,6 @@ import { resolve as pResolve } from 'node:path';
 import { sanitizePath } from '@contentstack/cli-utilities';
 import { formatError, fsUtil, PersonalizationAdapter, log } from '../utils';
 import { PersonalizationConfig, ExportConfig, AttributesConfig, AttributeStruct } from '../types';
-import { Command } from '@contentstack/cli-command';
 
 export default class ExportAttributes extends PersonalizationAdapter<ExportConfig> {
   private attributesConfig: AttributesConfig;
@@ -11,10 +10,10 @@ export default class ExportAttributes extends PersonalizationAdapter<ExportConfi
   private attributes: Record<string, unknown>[];
   public personalizationConfig: PersonalizationConfig;
 
-  constructor(readonly exportConfig: ExportConfig, readonly command: Command) {
+  constructor(readonly exportConfig: ExportConfig) {
     super({
       config: exportConfig,
-      baseURL: exportConfig.modules.personalization.baseURL[exportConfig.region.name] || command.personalizeUrl,
+      baseURL: exportConfig.modules.personalization.baseURL as unknown as string || exportConfig.modules.personalization.baseURL[exportConfig.region.name],
       headers: { authtoken: exportConfig.auth_token, 'X-Project-Uid': exportConfig.project_id },
     });
     this.personalizationConfig = exportConfig.modules.personalization;
