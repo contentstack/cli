b7eec956f7ba6cebf5c15aaf556bcfbbe89b75a5
Harshitha D <harshitha.d@contentstack.com>
2024-09-04 11:01:42 +0530

    resolved the conversations, updated structure of rate limit, removed handleError

--- packages/contentstack-config/src/commands/config/remove/rate-limit.ts
+++ packages/contentstack-config/src/commands/config/remove/rate-limit.ts
@@ -1,38 +1,37 @@
-import { cliux, configHandler, FlagInput, flags, isAuthenticated } from '@contentstack/cli-utilities';
+import { cliux, configHandler, FlagInput, flags, isAuthenticated, messageHandler } from '@contentstack/cli-utilities';
 import { Command } from '@contentstack/cli-command';
-import { askOrgID, handleErrorMsg } from '../../../utils/interactive';
+import { askOrgID } from '../../../utils/interactive';
 
 export default class RateLimitRemoveCommand extends Command {
+  static description: string = messageHandler.parse('Remove rate-limit of the organization');
 
   static flags: FlagInput = {
     org: flags.string({
       description: 'Provide the organization UID',
     }),
   };
-  static examples = [
-    '$ csdx config:remove:rate-limit --org <<org_uid>>'
-  ];
+  static examples = ['$ csdx config:remove:rate-limit --org <<org_uid>>'];
   async run() {
     try {
       if (!isAuthenticated()) {
         const err = { errorMessage: 'You are not logged in. Please login with command $ csdx auth:login' };
-        handleErrorMsg(err);
+        cliux.print(err.errorMessage, { color: 'red' });
       }
       const { flags } = await this.parse(RateLimitRemoveCommand);
       let { org } = flags;
       if (!org) {
         org = await askOrgID();
       }
-      const rateLimits = configHandler.get('rateLimits') || {};
+      const rateLimit = configHandler.get('rateLimit') || {};
 
-      if (!rateLimits.rateLimit[org]) {
+      if (!rateLimit[org]) {
         cliux.print(`No rate limit found for the organization UID: ${org}`, { color: 'red' });
         return;
       }
 
-      delete rateLimits.rateLimit[org];
+      delete rateLimit[org];
 
-      configHandler.set('rateLimits', rateLimits);
+      configHandler.set('rateLimit', rateLimit);
       cliux.print(`Rate limit entry for organization UID ${org} has been removed.`, { color: 'green' });
     } catch (error) {
       this.log('Unable to remove the rate limit entry', error instanceof Error ? error.message : error);
