f803b5695cb3a3a48e633af48f3cdcf22d2eb867
Harshitha D <harshitha.d@contentstack.com>
2024-09-09 16:02:32 +0530

    added check for limit names, utilize values and comma separated values for utilize

--- packages/contentstack-config/src/utils/common-utilities.ts
+++ packages/contentstack-config/src/utils/common-utilities.ts
@@ -1,1 +1,7 @@
 export const limitNamesConfig = ['getLimit', 'limit', 'bulkLimit'];
+
+export const defaultRalteLimitConfig = {
+  getLimit: { value: 10, utilize: 50 },
+  limit: { value: 10, utilize: 50 },
+  bulkLimit: { value: 1, utilize: 50 },
+};
