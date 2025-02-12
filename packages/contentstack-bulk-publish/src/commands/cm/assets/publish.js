3da449754f05705d82cf720935789f0d90254ab8
raj pandey <raj.pandey@contentstack.com>
2025-02-12 17:00:19 +0530

    Fix: Added use of delivery token alias in cross publish and unpublish

--- packages/contentstack-bulk-publish/src/commands/cm/assets/publish.js
+++ packages/contentstack-bulk-publish/src/commands/cm/assets/publish.js
@@ -83,9 +83,18 @@ class AssetsPublishCommand extends Command {
 
           if (updatedFlags['source-env']) {
             updatedFlags.deliveryToken = updatedFlags['delivery-token'];
+            if (updatedFlags.deliveryToken) {
+              const configToken = this.getToken(updatedFlags.deliveryToken)
+              if(configToken) {
+                updatedFlags.deliveryToken = configToken.token;
+              } else {
+                this.error(`Delivery Token alias does not exist run the csdx auth:tokens command to verify`,{ exit: 2 })
+              }
+            }
             updatedFlags.destEnv = updatedFlags.environments;
             updatedFlags.environment = updatedFlags['source-env'];
             updatedFlags.onlyAssets = true;
+            console.log(updatedFlags)
             if (updatedFlags.locales instanceof Array) {
               updatedFlags.locales.forEach((locale) => {
                 updatedFlags.locale = locale;
