2905f7caa226cbd668ebde1e4369cf94efffcc83
Vikram Kalta <vikramkalta@Vikrams-MacBook-Pro.local>
2025-02-17 20:36:10 +0000

    test: added test for nested global fields

--- packages/contentstack-audit/test/unit/modules/global-field.test.ts
+++ packages/contentstack-audit/test/unit/modules/global-field.test.ts
@@ -1,3 +1,5 @@
+import fs from 'fs';
+import sinon from 'sinon';
 import { resolve } from 'path';
 import { fancy } from 'fancy-test';
 import { expect } from 'chai';
@@ -7,21 +9,40 @@ import { ux } from '@contentstack/cli-utilities';
 import config from '../../../src/config';
 import { GlobalField } from '../../../src/modules';
 import { $t, auditMsg } from '../../../src/messages';
-
+import { CtConstructorParam, ModuleConstructorParam } from '../../../src/types';
 
 describe('Global Fields', () => {
-  describe('run method with invalid path for global_feild', () => {
-    const gfInstance = new GlobalField({
+  let constructorParam: ModuleConstructorParam & CtConstructorParam;
+
+  class AuditFixTempClass extends GlobalField {
+    constructor(public missingRefs: Record<string, any> = {}) {
+      super({ ...constructorParam, fix: true, moduleName: 'global-fields' });
+      this.currentUid = 'audit-fix';
+      this.currentTitle = 'Audit fix';
+      this.missingRefs['audit-fix'] = [];
+    }
+  }
+
+  beforeEach(() => {
+    constructorParam = {
       log: () => {},
       moduleName: 'global-fields',
       ctSchema: cloneDeep(require('../mock/contents/content_types/schema.json')),
-      gfSchema: cloneDeep(require('../mock/contents/global_fields/Global_fields_2.json')),
+      gfSchema: cloneDeep(require('../mock/contents/global_fields/globalfields.json')),
       config: Object.assign(config, { basePath: resolve(__dirname, '..', 'mock', 'contents'), flags: {} }),
-    });
+    };
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  describe('run method', () => {
     fancy
       .stdout({ print: process.env.PRINT === 'true' || false })
       .stub(ux, 'confirm', async () => true)
-      .it('Should Validate the base path for workflows', async () => {
+      .it('Should Validate the base path for global-fields', async () => {
+        const gfInstance = new GlobalField({ ...constructorParam });
         try {
           await gfInstance.run();
         } catch (error: any) {
@@ -29,26 +50,58 @@ describe('Global Fields', () => {
           expect(error.message).to.eql($t(auditMsg.NOT_VALID_PATH, { path: gfInstance.folderPath }));
         }
       });
+
+    fancy
+      .stdout({ print: process.env.PRINT === 'true' || false })
+      .stub(GlobalField.prototype, 'lookForReference', async () => {})
+      .it('should call lookForReference', async () => {
+        const gfInstance = new GlobalField(constructorParam);
+        const logSpy = sinon.spy(gfInstance, 'lookForReference');
+        await gfInstance.run();
+        expect(logSpy.callCount).to.be.equals(gfInstance.gfSchema.length);
+      });
+
+    fancy
+      .stdout({ print: process.env.PRINT === 'true' || false })
+      .stub(GlobalField.prototype, 'lookForReference', async () => {})
+      .it('should return schema', async () => {
+        const gfInstance = new GlobalField(constructorParam);
+        expect(await gfInstance.run(true)).to.deep.equals(gfInstance.gfSchema);
+      });
+
+    fancy
+      .stdout({ print: process.env.PRINT === 'true' || false })
+      .stub(GlobalField.prototype, 'lookForReference', async () => {})
+      .stub(GlobalField.prototype, 'writeFixContent', async () => {})
+      .it('should call writeFixContent', async () => {
+        const gfInstance = new GlobalField({ ...constructorParam, fix: true });
+        const logSpy = sinon.spy(gfInstance, 'writeFixContent');
+        await gfInstance.run();
+        expect(logSpy.callCount).to.be.equals(1);
+      });
   });
 
-  describe('run method with valid Path', () => {
-    const gfInstance = new GlobalField({
-      log: () => {},
-      moduleName: 'global-fields',
-      ctSchema: cloneDeep(require('../mock/contents/content_types/schema.json')),
-      gfSchema: cloneDeep(require('../mock/contents/global_fields/globalfields.json')),
-      config: Object.assign(config, { basePath: resolve(__dirname, '..', 'mock', 'contents'), flags: {} }),
-    });
+  describe('fix nested global field references', () => {
     fancy
-      .stdout({ print: process.env.PRINT === 'true' || true })
-      .stub(ux, 'confirm', async () => true)
-      .it('Should output the global feild where nested feilds are not present', async () => {
-        try {
-          const missingRefs = await gfInstance.run();
-          expect(JSON.stringify(missingRefs)).to.be.equal(
-            '{\n  nested_global_field_2: [\n    {\n      tree: [\n        {\n          uid: "nested_global_field_2",\n          name: "Nested Global Field 2",\n        },\n        {\n          uid: "global_field",\n          name: "Global",\n        },\n      ],\n      ct: "nested_global_field_2",\n      name: "Nested Global Field 2",\n      data_type: "global_field",\n      display_name: "Global",\n      missingRefs: "Referred Global Field Does not Exist",\n      treeStr: "Nested Global Field 2 ➜ Global",\n    },\n  ],\n  global_field_sample_2: [\n    {\n      tree: [\n        {\n          uid: "global_field_sample_2",\n          name: "global_field_sample_2",\n        },\n        {\n          uid: "group",\n          name: "Group",\n        },\n        {\n          uid: "group",\n          name: "Group",\n        },\n        {\n          uid: "global_field",\n          name: "Global",\n        },\n      ],\n      ct: "global_field_sample_2",\n      name: "global_field_sample_2",\n      data_type: "global_field",\n      display_name: "Global",\n      missingRefs: "Referred Global Field Does not Exist",\n      treeStr: "global_field_sample_2 ➜ Group ➜ Group ➜ Global",\n    },\n  ],\n}',
-          );
-        } catch (error) {}
+      .stub(fs, 'rmSync', () => {})
+      .stdout({ print: process.env.PRINT === 'true' || false })
+      .stub(GlobalField.prototype, 'writeFixContent', async () => {})
+      .it('perform audit operation on the given GF schema', async () => {
+        const gfInstance = new AuditFixTempClass();
+
+        await gfInstance.run();
+
+        expect(gfInstance.missingRefs).ownProperty('nested_global_field_2');
+        expect(JSON.stringify(gfInstance.missingRefs)).includes('"missingRefs":["nested_global_field_1"]');
+      });
+
+    fancy
+      .stub(fs, 'rmSync', () => {})
+      .stdout({ print: process.env.PRINT === 'true' || false })
+      .stub(GlobalField.prototype, 'writeFixContent', async () => {})
+      .it('perform audit and fix operation on the given GF schema', async () => {
+        const gfInstance = new AuditFixTempClass();
+        expect(JSON.stringify(await gfInstance.run(true))).includes('"uid":"global_field_sample_2","schema":[]');
       });
   });
 });
