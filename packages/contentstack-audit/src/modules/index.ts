e487133a4fca696d8533d9924490b6eb289add5d
Vikram Kalta <vikramkalta@Vikrams-MacBook-Pro.local>
2025-03-03 21:32:16 +0000

    test: added custom roles test cases

--- packages/contentstack-audit/src/modules/index.ts
+++ packages/contentstack-audit/src/modules/index.ts
@@ -3,4 +3,5 @@ import GlobalField from './global-fields';
 import ContentType from './content-types';
 import Workflows from './workflows';
 import Extensions from './extensions';
-export { Entries, GlobalField, ContentType, Workflows, Extensions };
+import CustomRoles from './custom-roles';
+export { Entries, GlobalField, ContentType, Workflows, Extensions, CustomRoles };
