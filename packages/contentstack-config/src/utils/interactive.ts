02c436b275df65f8c7996f98c6334ecbca02ccd7
naman-contentstack <naman.dembla@cotentstack.com>
2025-04-07 12:40:23 +0530

    [DX-2346], added support for AWS-NA and AWS-EU, fixed test cases

--- packages/contentstack-config/src/utils/interactive.ts
+++ packages/contentstack-config/src/utils/interactive.ts
@@ -9,12 +9,14 @@ export const askRegions = async (): Promise<string> => {
     choices: [
       { name: 'NA', value: 'NA' },
       { name: 'EU', value: 'EU' },
+      { name: 'AWS-NA', value: 'AWS-NA' },
+      { name: 'AWS-EU', value: 'AWS-EU' },
       { name: 'AZURE-NA', value: 'AZURE-NA' },
       { name: 'AZURE-EU', value: 'AZURE-EU' },
       { name: 'GCP-NA', value: 'GCP-NA' },
       { name: 'GCP-EU', value: 'GCP-EU' },
       { name: 'Custom', value: 'custom' },
-      { name: 'exit', value: 'exit' },
+      { name: 'Exit', value: 'exit' },
     ],
   });
 };
