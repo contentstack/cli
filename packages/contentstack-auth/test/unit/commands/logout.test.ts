1aca059d40001b9f58fe3ea13a25fd1f6dc17732
harshitha.d <harshitha.d@contentstack.com>
2025-07-23 02:04:27 +0530

    refactor: Reorganize logout command tests for clarity and consistency

--- packages/contentstack-auth/test/unit/commands/logout.test.ts
+++ packages/contentstack-auth/test/unit/commands/logout.test.ts
@@ -1,32 +1,29 @@
 import { expect } from 'chai';
 import * as sinon from 'sinon';
 import LogoutCommand from '../../../src/commands/auth/logout';
+import { cliux } from '@contentstack/cli-utilities';
 import { authHandler } from '../../../src/utils';
-import { CLIError, cliux } from '@contentstack/cli-utilities';
-
-const user = { email: '***REMOVED***', authtoken: 'testtoken' };
-const validAuthToken = 'bltadjkjdkjfd';
 
 describe('Logout Command', () => {
-  let logoutStub;
-  let inquireStub;
-  let successStub;
-  before(function () {
-    logoutStub = sinon.stub(authHandler, 'logout').callsFake(function (authToken): Promise<any> {
-      if (authToken === validAuthToken) {
-        return Promise.resolve({ user });
-      }
-      return Promise.reject(new CLIError({ message: 'invalid token' }));
-    });
+  let inquireStub: sinon.SinonStub;
+  let successStub: sinon.SinonStub;
+  let loaderStub: sinon.SinonStub;
+  let logoutStub: sinon.SinonStub;
+  let isAuthenticatedStub: sinon.SinonStub;
+  let configStub: sinon.SinonStub;
 
-    inquireStub = sinon.stub(cliux, 'inquire').resolves(true);
-    successStub = sinon.stub(cliux, 'success').resolves();
+  beforeEach(() => {
+    inquireStub = sinon.stub(cliux, 'inquire');
+    successStub = sinon.stub(cliux, 'success');
+    loaderStub = sinon.stub(cliux, 'loader');
+    logoutStub = sinon.stub(authHandler, 'logout').resolves({ user: {} });
   });
 
-  after(() => {
-    logoutStub.restore();
+  afterEach(() => {
     inquireStub.restore();
     successStub.restore();
+    loaderStub.restore();
+    logoutStub.restore();
   });
 
   it('Logout with valid token, should be successful', async function () {
@@ -34,11 +31,15 @@ describe('Logout Command', () => {
     expect(inquireStub.calledOnce).to.be.true;
   });
 
-  it('logout with force flag, should not propmt the confirm', async function () {
-    const stub1 = sinon.stub(LogoutCommand.prototype, 'run').resolves();
-    const args = ['-f'];
-    await LogoutCommand.run(args);
-    expect(stub1.calledOnce).to.be.true;
-    stub1.restore();
+  it('Logout should prompt for confirmation', async function () {
+    inquireStub.resolves(false);
+    await LogoutCommand.run([]);
+    expect(inquireStub.calledOnce).to.be.true;
+    expect(inquireStub.firstCall.args[0]).to.deep.include({
+      type: 'confirm',
+      message: 'CLI_AUTH_LOGOUT_CONFIRM',
+      name: 'confirmation'
+    });
+    expect(logoutStub.called).to.be.false; // Should not call logout if confirmation is denied
   });
 });
