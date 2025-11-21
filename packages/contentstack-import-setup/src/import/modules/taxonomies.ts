24dc096be67bef243da0268c9f7ced47e7b717e7
Netraj Patel <netraj.patel@contentstack.com>
2025-11-21 20:56:29 +0530

    Updated and fixed unit test cases and linting issues

--- packages/contentstack-import-setup/src/import/modules/taxonomies.ts
+++ packages/contentstack-import-setup/src/import/modules/taxonomies.ts
@@ -18,7 +18,7 @@ export default class TaxonomiesImportSetup {
   private taxonomiesMapperDirPath: string;
   private termsMapperDirPath: string;
   private localesFilePath: string;
-  private isLocaleBasedStructure: boolean = false;
+  private isLocaleBasedStructure = false;
   public taxonomiesMapper: Record<string, unknown> = {};
   public termsMapper: Record<string, unknown> = {};
   public masterLocaleFilePath: string;
