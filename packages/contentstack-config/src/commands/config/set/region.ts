d04c6aa2f8a96f4c8536c8bd801b27d913af046c
shafeeqd959 <78414855+shafeeqd959@users.noreply.github.com>
2025-12-04 14:39:36 +0530

    Merge pull request #2258 from contentstack/export-import/composable-studio
    added multi region support

--- packages/contentstack-config/src/commands/config/set/region.ts
+++ packages/contentstack-config/src/commands/config/set/region.ts
@@ -48,6 +48,9 @@ export default class RegionSetCommand extends BaseCommand<typeof RegionSetComman
     launch: _flags.string({
       description: 'Custom host to set for Launch API',
     }),
+    'composable-studio': _flags.string({
+      description: 'Custom host to set for Composable Studio API',
+    }),
   };
   static examples = [
     '$ csdx config:set:region',
@@ -62,7 +65,8 @@ export default class RegionSetCommand extends BaseCommand<typeof RegionSetComman
     '$ csdx config:set:region --cma <custom_cma_host_url> --cda <custom_cda_host_url> --ui-host <custom_ui_host_url> --name "India" --developer-hub <custom_developer_hub_url>',
     '$ csdx config:set:region --cma <custom_cma_host_url> --cda <custom_cda_host_url> --ui-host <custom_ui_host_url> --name "India" --personalize <custom_personalize_url>',
     '$ csdx config:set:region --cma <custom_cma_host_url> --cda <custom_cda_host_url> --ui-host <custom_ui_host_url> --name "India" --launch <custom_launch_url>',
-    '$ csdx config:set:region --cda <custom_cda_host_url> --cma <custom_cma_host_url> --ui-host <custom_ui_host_url> --name "India" --developer-hub <custom_developer_hub_url> --launch <custom_launch_url> --personalize <custom_personalize_url>',
+    '$ csdx config:set:region --cma <custom_cma_host_url> --cda <custom_cda_host_url> --ui-host <custom_ui_host_url> --name "India" --composable-studio <custom_composable_studio_url>',
+    '$ csdx config:set:region --cda <custom_cda_host_url> --cma <custom_cma_host_url> --ui-host <custom_ui_host_url> --name "India" --developer-hub <custom_developer_hub_url> --launch <custom_launch_url> --personalize <custom_personalize_url> --composable-studio <custom_composable_studio_url>',
   ];
 
   static args: ArgInput = {
@@ -78,6 +82,7 @@ export default class RegionSetCommand extends BaseCommand<typeof RegionSetComman
     let developerHubUrl = regionSetFlags['developer-hub'];
     let personalizeUrl = regionSetFlags['personalize'];
     let launchHubUrl = regionSetFlags['launch'];
+    let composableStudioUrl = regionSetFlags['composable-studio'];
     let selectedRegion = args.region;
     if (!(cda && cma && uiHost && name) && !selectedRegion) {
       selectedRegion = await interactive.askRegions();
@@ -105,7 +110,19 @@ export default class RegionSetCommand extends BaseCommand<typeof RegionSetComman
         if (!personalizeUrl) {
           personalizeUrl = this.transformUrl(cma, 'personalize-api');
         }
-        let customRegion: Region = { cda, cma, uiHost, name, developerHubUrl, personalizeUrl, launchHubUrl };
+        if (!composableStudioUrl) {
+          composableStudioUrl = this.transformUrl(cma, 'composable-studio-api');
+        }
+        let customRegion: Region = {
+          cda,
+          cma,
+          uiHost,
+          name,
+          developerHubUrl,
+          personalizeUrl,
+          launchHubUrl,
+          composableStudioUrl,
+        };
         customRegion = regionHandler.setCustomRegion(customRegion);
         await authHandler.setConfigData('logout'); //Todo: Handle this logout flow well through logout command call
         cliux.success(`Custom region has been set to ${customRegion.name}`);
@@ -115,6 +132,7 @@ export default class RegionSetCommand extends BaseCommand<typeof RegionSetComman
         cliux.success(`Developer Hub URL: ${customRegion.developerHubUrl}`);
         cliux.success(`Personalize URL: ${customRegion.personalizeUrl}`);
         cliux.success(`Launch URL: ${customRegion.launchHubUrl}`);
+        cliux.success(`Composable Studio URL: ${customRegion.composableStudioUrl}`);
       } catch (error) {
         this.logger.error('failed to set the region', error);
         cliux.error(`Failed to set region due to: ${error.message}`);
@@ -133,6 +151,7 @@ export default class RegionSetCommand extends BaseCommand<typeof RegionSetComman
       cliux.success(`Developer Hub URL: ${regionDetails.developerHubUrl}`);
       cliux.success(`Personalize URL: ${regionDetails.personalizeUrl}`);
       cliux.success(`Launch URL: ${regionDetails.launchHubUrl}`);
+      cliux.success(`Composable Studio URL: ${regionDetails.composableStudioUrl}`);
     } else {
       cliux.error(`Invalid region specified.`);
     }
