2282622ad0530811005ee6ff15e0049d0f8aa1d9
harshitha.d <harshitha.d@contentstack.com>
2026-01-02 21:26:08 +0530

    Merge pull request #2255 from contentstack/fix/DX-3792
    fix: Taxonomy Export Fails When Localization Not in Plan

--- packages/contentstack-import/src/types/index.ts
+++ packages/contentstack-import/src/types/index.ts
@@ -162,7 +162,7 @@ export interface Context {
   command: string;
   module: string;
   userId: string | undefined;
-  email?: string | undefined;
+  email: string | undefined;
   sessionId: string | undefined;
   clientId?: string | undefined;
   apiKey: string;
