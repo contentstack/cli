518fee8443bf13bbb9b6e33fdbc7bd160bb4cf0a
Netraj Patel <netraj.patel@contentstack.com>
2026-01-23 17:42:48 +0530

    Resolved failing tests

--- packages/contentstack-export-to-csv/test/unit/utils/teams-export.test.ts
+++ packages/contentstack-export-to-csv/test/unit/utils/teams-export.test.ts
@@ -1,15 +1,18 @@
 import { expect } from 'chai';
+import {
+  exportTeams,
+  getTeamsDetail,
+  exportRoleMappings,
+  mapRoleWithTeams,
+} from '../../../src/utils/teams-export';
 
 describe('teams-export', () => {
   describe('module exports', () => {
-    it('should export all team export functions', async () => {
-      // Import from barrel export with explicit index.js
-      const utils = await import('../../../dist/utils/index.js');
-
-      expect(utils.exportTeams).to.be.a('function');
-      expect(utils.getTeamsDetail).to.be.a('function');
-      expect(utils.exportRoleMappings).to.be.a('function');
-      expect(utils.mapRoleWithTeams).to.be.a('function');
+    it('should export all team export functions', () => {
+      expect(exportTeams).to.be.a('function');
+      expect(getTeamsDetail).to.be.a('function');
+      expect(exportRoleMappings).to.be.a('function');
+      expect(mapRoleWithTeams).to.be.a('function');
     });
   });
 
