4dbc83f4b6002b7409e79b6f12a3f398176e2509
naman-contentstack <naman.dembla@cotentstack.com>
2026-01-27 15:56:38 +0530

    feat: remove logic for cli-log file creation

--- packages/contentstack/src/hooks/prerun/auth-guard.ts
+++ packages/contentstack/src/hooks/prerun/auth-guard.ts
@@ -1,13 +1,13 @@
 import { 
   cliux, 
   managementSDKClient,
-  isAuthenticated
+  isAuthenticated,
+  log,
+  handleAndLogError
 } from '@contentstack/cli-utilities';
-import {LoggerService} from '@contentstack/cli-utilities'
 // TBD: run region command if region is not there
 
 export default async function (opts): Promise<void> {
-  const newLogger = new LoggerService(process.cwd(),'cli-log');
   const { context: { plugin: { config: { protectedCommands = {} } = {} } = {}, region = null } = {} } = this.config;
   if (opts.Command.id !== 'config:set:region') {
     if (!region) {
@@ -20,7 +20,7 @@ export default async function (opts): Promise<void> {
   // Auth guard
   if (protectedCommands[opts.Command.id]) {
     if (!isAuthenticated()) {
-      newLogger.error('No auth token found for command.', opts.Command.id);
+      handleAndLogError(new Error('No auth token found for command.'), { module: 'auth-guard', commandId: opts.Command.id });
       cliux.error('Please log in to execute the command');
       this.exit();
     }
@@ -28,13 +28,13 @@ export default async function (opts): Promise<void> {
     try {
       const result = await client.getUser();
       if (!result) {
-        newLogger.error('Error in auth validation');
+        handleAndLogError(new Error('Error in auth validation'), { module: 'auth-guard' });
         cliux.error('Please log in to execute the command');
         this.exit();
       }
-      newLogger.debug('Logged-in user', result.data);    
+      log.debug('Logged-in user', { module: 'auth-guard', userData: result.data });    
     } catch (error) {
-      newLogger.error('Error in auth validation', error);
+      handleAndLogError(error, { module: 'auth-guard' }, 'Error in auth validation');
       cliux.error('Please log in to execute the command');
       process.exit();
     }   
